00a104638c3bba1d34bc4a4bcb4eb996ef8eaad5
Andrew Gainer-Dewar <andrew.gainer.dewar@gmail.com>
2013-12-31 09:45:15 -0600

    Remove bogus functorial composition code

--- src/sage/combinat/species/group_cycle_index_series.py
+++ src/sage/combinat/species/group_cycle_index_series.py
@@ -189,37 +189,6 @@ class GroupCycleIndexSeries(CombinatorialFreeModuleElement):
         """
 
         return 1/self.parent()._group.cardinality() * sum(self.coefficients())
-    
-    def functorial_composition(self, y):
-        """
-        Return the functorial composition of ``self`` with ``y``.
-
-        Functorial composition of group cycle index series is defined componentwise:
-
-        .. MATH::
-          (F \square G) [\gamma] = (F [\gamma]) \square (G [\gamma])
-
-        This operation on $\Gamma$-cycle indices corresponds to the functorial composition
-        operation on $\Gamma$-species.
-
-        EXAMPLES::
-
-            sage: S4 = SymmetricGroup(4)
-            sage: GCISR = gci.GroupCycleIndexSeriesRing(S4)
-            sage: E = species.SetSpecies().cycle_index_series()
-            sage: Eg = GCISR(E)
-            sage: Egplus = GCISR(E-1)
-            sage: test = Eg.functorial_composition(Egplus)
-            sage: test[SymmetricGroup(4).an_element()].coefficients(4)
-            [p[], p[1], 1/2*p[1, 1] + 1/2*p[2], 1/6*p[1, 1, 1] + 1/2*p[2, 1] + 1/3*p[3]]
-        """
-
-        parent = self.parent()
-        group = parent._group
-
-        components_dict = { g: self[g].functorial_composition(y[g]) for g in group }
-        res = parent._from_dict(components_dict)
-        return res
 
     def composition(self, y, test_constant_term_is_zero = True):
         """Return the group-cycle-index plethysm of ``self`` with ``y``.
