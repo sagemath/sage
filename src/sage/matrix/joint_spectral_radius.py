72fbff4166d926456b259a1876ce8df8cb59085e
Daniel Krenn <git@danielkrenn.at>
2016-09-08 17:19:13 +0200

    Trac #21205: replace .str(...) --> .endpoints()

--- src/sage/matrix/joint_spectral_radius.py
+++ src/sage/matrix/joint_spectral_radius.py
@@ -100,8 +100,8 @@ def joint_spectral_radius(S, delta=None, norm=None,
         ....:      Matrix([[3, -3], [0, -1]]) / 5),
         ....:     delta=RIF(0.01))
         DEBUG:...:m=2, alpha=0.6000000000000000?, beta=0.70827625302982189?, len(T)=4
-        DEBUG:...:m=3, alpha=0.6000000000000000?, beta=0.706734263510838?, len(T)=6
-        DEBUG:...:m=4, alpha=0.6000000000000000?, beta=0.704390998023197?, len(T)=8
+        DEBUG:...:m=3, alpha=0.600000000000000?, beta=0.706734263510838?, len(T)=6
+        DEBUG:...:m=4, alpha=0.600000000000000?, beta=0.704390998023197?, len(T)=8
         ...
         DEBUG:...:m=16, alpha=0.659678908955284?, beta=0.6707696905811751?, len(T)=1
         DEBUG:...:m=17, alpha=0.659678908955284?, beta=0.669678908955284?, len(T)=0
@@ -122,22 +122,22 @@ def joint_spectral_radius(S, delta=None, norm=None,
         sage: A2 = Matrix([[0, 0, 0], [1, 1, 0],  [0, 0, 1]])
         sage: A3 = Matrix([[0, 0, 0], [0, 0, 0],  [1, -1, 1]])
         sage: joint_spectral_radius((A0, A1, A2, A3),  # long time
-        ....:     delta=RIF(0.1)).str(style='brackets')
-        '[1.0000000000000000 .. 1.1000000000000001]'
+        ....:     delta=RIF(0.1)).endpoints()
+        (1.00000000000000, 1.10000000000001)
 
     [Dumas2013]_, Example 4 (result is `1`)::
 
         sage: A0 = Matrix([[1, 1/2, 0], [0, 1/2, 0], [0, 1/2, 1]])
         sage: A1 = Matrix([[1/2, 0, 0], [1/2, 1, 0], [1/2, 0, 1]])
-        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).str(style='brackets')
-        '[1.0000000000000000 .. 1.2000000000000002]'
+        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).endpoints()
+        (1.00000000000000, 1.20000000000001)
 
     [Dumas2013]_, Example 5 (result is `3\sqrt{2}=4.24\dots`)::
 
         sage: A0 = Matrix([[1, 0], [0, 1]])
         sage: A1 = Matrix([[3, -3], [3, 3]])
-        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).str(style='brackets')
-        '[4.2426406871192847 .. 4.2426406871192848]'
+        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).endpoints()
+        (4.24264068711928, 4.24264068711929)
 
     [Dumas2013]_, Example 6 (result is `1`)::
 
@@ -146,8 +146,8 @@ def joint_spectral_radius(S, delta=None, norm=None,
         sage: Z = zero_matrix(2, 2)
         sage: A0 = block_matrix([[B1, Z], [Z, B0]])
         sage: A1 = block_matrix([[Z, Z], [B0, B1]])
-        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).str(style='brackets')
-        '[1.0000000000000000 .. 1.2000000000000002]'
+        sage: joint_spectral_radius((A0, A1), delta=RIF(0.2)).endpoints()
+        (1.00000000000000, 1.20000000000001)
 
     TESTS::
 
@@ -156,10 +156,10 @@ def joint_spectral_radius(S, delta=None, norm=None,
         sage: A2 = Matrix([[0, 0, 0], [1, 1, 0],  [0, 0, 1]])
         sage: A3 = Matrix([[0, 0, 0], [0, 0, 0],  [1, -1, 1]])
         sage: joint_spectral_radius((A0, A1, A2, A3),
-        ....:     delta=RIF(0.1), max_iterations=5).str(style='brackets')
+        ....:     delta=RIF(0.1), max_iterations=5).endpoints()
         WARNING:sage.matrix.joint_spectral_radius:Stopping since
         maximum number of iterations reached.
-        '[1.0000000000000000 .. 1.1472026904398774]'
+        (1.00000000000000, 1.14720269043988)
     """
     from sage.arith.srange import srange
     from sage.misc.misc_c import prod
