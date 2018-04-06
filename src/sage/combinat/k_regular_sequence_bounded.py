ec533d8da200599f702390f5cd6a7a3c18184b4a
Gabriel Lipnik <galipnik@edu.aau.at>
2018-04-07 01:48:00 +0200

    modify doc-tests

--- src/sage/combinat/k_regular_sequence_bounded.py
+++ src/sage/combinat/k_regular_sequence_bounded.py
@@ -504,15 +504,6 @@ def k_regular_sequence_is_bounded(S):
         ....: Matrix([[0, 0, 1], [0, 0, 2], [0, 0, 1]])],
         ....: left=vector([1, 0, 0]), right=vector([0, 0, 1]))
         sage: k_regular_sequence_is_bounded(S)
-        Traceback (most recent call last):
-        ...
-        RuntimeError: It is not decidable with this implementation whether the
-        sequence is bounded or not.
-
-    Find another Representation or a Sequence which is equivalent in Boundedness::
-
-        sage: S1 = S.subsequence(a = 1, b = 1)
-        sage: k_regular_sequence_is_bounded(S1)
         False
 
     TESTS::
