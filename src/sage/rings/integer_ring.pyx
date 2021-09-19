5372b70d9b2d214223586a298819cc583a4006bf
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-09-19 00:16:08 -0700

    src/sage/rings/integer_ring.pyx: Remove unused import of sage.libs.pari.all

--- src/sage/rings/integer_ring.pyx
+++ src/sage/rings/integer_ring.pyx
@@ -54,8 +54,6 @@ import sage.rings.infinity
 import sage.rings.rational
 import sage.rings.rational_field
 import sage.rings.ideal
-import sage.libs.pari.all
-import sage.rings.ideal
 from sage.categories.basic import EuclideanDomains
 from sage.categories.infinite_enumerated_sets import InfiniteEnumeratedSets
 from sage.structure.coerce cimport is_numpy_type
