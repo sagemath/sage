19d4d38bc129595e545ac2cc4bf9b119d54c0546
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-10-03 18:15:04 -0700

    src/sage/rings/abc.pyx: Remove duplicate def of ComplexBallField

--- src/sage/rings/abc.pyx
+++ src/sage/rings/abc.pyx
@@ -66,14 +66,6 @@ cdef class ComplexDoubleField(Field):
     pass
 
 
-cdef class ComplexBallField(Field):
-    r"""
-    Abstract base class for :class:`sage.rings.complex_arb.ComplexBallField`.
-    """
-
-    pass
-
-
 class IntegerModRing:
     r"""
     Abstract base class for :class:`~sage.rings.finite_rings.integer_mod_ring.IntegerModRing_generic`.
