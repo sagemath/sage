6188e3f27409f0f6118a604e6b0696489bf57aed
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2021-10-04 19:49:40 -0700

    src/sage/all__sagemath_polyhedra.py: from sage.matrix.all import *

--- src/sage/all__sagemath_polyhedra.py
+++ src/sage/all__sagemath_polyhedra.py
@@ -2,4 +2,5 @@ from .all__sagemath_categories import *
 
 from sage.rings.all__sagemath_polyhedra import *
 from sage.modules.all import *
+from sage.matrix.all import *
 from sage.geometry.all__sagemath_polyhedra import *
