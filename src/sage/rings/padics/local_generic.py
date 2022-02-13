60b4f780d0a391a6ba83cd0ed609493cdcada2bd
Julian Rüth <julian.rueth@fsfe.org>
2022-02-12 19:29:48 -0500

    Fix change() when resetting q

--- src/sage/rings/padics/local_generic.py
+++ src/sage/rings/padics/local_generic.py
@@ -373,6 +373,12 @@ class LocalGeneric(CommutativeRing):
             37-adic Ring with lattice-cap precision (label: change)
             sage: S.change(label = "new")
             37-adic Ring with lattice-cap precision (label: new)
+
+        Changing from a base field to an unramified extension::
+
+            sage: K = Qp(2)
+            sage: L.<a> = K.change(q=4); L
+
         """
         # We support both print_* and * for *=mode, pos, sep, alphabet
         for atr in ('print_mode', 'print_pos', 'print_sep', 'print_alphabet'):
@@ -386,9 +392,12 @@ class LocalGeneric(CommutativeRing):
         from .padic_base_generic import pAdicBaseGeneric
         if 'q' in kwds and isinstance(self.base_ring(), pAdicBaseGeneric):
             q = kwds.pop('q')
-            if not isinstance(q, Integer):
-                raise TypeError("q must be an integer")
-            p, n = q.is_prime_power(get_data=True)
+            if isinstance(q, (tuple, list)):
+                p, n = q
+            else:
+                if not isinstance(q, (Integer, int)):
+                    raise TypeError("q must be an integer or a tuple (p, n)")
+                p, n = ZZ(q).is_prime_power(get_data=True)
             if n == 0:
                 raise ValueError("q must be a prime power")
             if 'p' in kwds and kwds['p'] != p:
@@ -446,6 +455,8 @@ class LocalGeneric(CommutativeRing):
                     names = kwds.pop('unram_name')
                 else:
                     raise TypeError("You must specify the name of the generator")
+                if isinstance(names, (tuple, list)):
+                    names = names[0]
                 res_name = kwds.pop('res_name', names + '0')
                 modulus = kwds.pop('modulus', get_unramified_modulus(q, res_name))
                 implementation = kwds.pop('implementation', 'FLINT')
@@ -476,7 +487,7 @@ class LocalGeneric(CommutativeRing):
                 base = functor(ring)
                 from .factory import ExtensionFactory
                 modulus = modulus.change_ring(base)
-                return ExtensionFactory(base=base, premodulus=modulus, names=names, res_name=res_name, unram=True, implementation=implementation)
+                return ExtensionFactory(base=base, modulus=modulus, names=names, res_name=res_name, implementation=implementation)
         else:
             functor.kwds = copy(functor.kwds)
             functor.kwds['print_mode'] = copy(functor.kwds['print_mode'])
