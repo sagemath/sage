3609c915438fd89da5686cc310be848b587f672a
Antoine Leudière <antoine.leudiere@inria.fr>
2022-05-10 14:38:36 +0200

    Move FiniteDrinfeldModule to sage.rings.function_field

--- src/sage/rings/function_field/all.py
+++ src/sage/rings/function_field/all.py
@@ -1,1 +1,6 @@
 from .constructor import FunctionField
+
+from sage.misc.lazy_import import lazy_import
+
+lazy_import('sage.rings.function_field.finite_drinfeld_module', 'FiniteDrinfeldModule')
+lazy_import('sage.rings.function_field.finite_drinfeld_module', 'FiniteDrinfeldModuleAction')
