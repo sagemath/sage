45be70180e9cc59760943399dd9758b81ac47217
Antoine Leudière <antoine.leudiere@inria.fr>
2022-06-22 13:37:39 +0200

    Clean DrinfeldModules category
    - Comment constructor
    - Check that codomain of input morphism is a field
    - Delete various useless docstrings (they will be written later, at a
      more final stage of development)
    - Change gamma to morphism
    - Various small enhancements

--- src/sage/categories/drinfeld_modules.py
+++ src/sage/categories/drinfeld_modules.py
@@ -18,42 +18,31 @@ from sage.rings.polynomial.ore_polynomial_ring import OrePolynomialRing
 # from sage.categories.basic import Fields
 
 class DrinfeldModules(CategoryWithParameters):
-    r"""
-    The category of Drinfeld modules.
 
-    EXAMPLES:
-
-    We create the category of function fields::
-
-        sage: C = FunctionFields()
-        sage: C
-        Category of function fields
-
-    TESTS::
-
-        sage: TestSuite(FunctionFields()).run()
-    """
-
-    def __init__(self, gamma, name='t'):
-        r"""
-        """
-        self._gamma = gamma
-        self._domain = FqX = gamma.domain()
-        K = gamma.codomain()
+    def __init__(self, morphism, name='t'):
+        self._morphism = morphism
+        self._domain = morphism.domain()
+        # Check domain is Fq[X]
+        FqX = self._domain
         if not isinstance(FqX, PolynomialRing_general):
             raise NotImplementedError('domain must be a polynomial ring')
         Fq = FqX.base_ring()
         if not Fq.is_field() or not Fq.is_finite() :
             raise TypeError('the base ring of the domain must be a finite field')
+        # Check domain is field
+        K = morphism.codomain()
+        if not K.is_field():
+            raise TypeError('the codomain must be a field')
+        # Build K{t}
         d = log(Fq.cardinality(), Fq.characteristic())
         tau = K.frobenius_endomorphism(d)
         self._codomain = OrePolynomialRing(K, tau, names=name)
         # Create characteristic
         self._characteristic = None
         if K.is_finite():
-            f = gamma * FqX.coerce_map_from(Fq)
+            f = morphism * FqX.coerce_map_from(Fq)
             E = K.over(f)
-            self._characteristic = E(gamma(FqX.gen())).minpoly()
+            self._characteristic = E(morphism(FqX.gen())).minpoly()
 
     def characteristic(self):
         if self._characteristic is None:
@@ -61,21 +50,19 @@ class DrinfeldModules(CategoryWithParameters):
         return self._characteristic 
 
     def _call_(self, gen):
-        r"""
-        Constructs an object in this category from the data in ``x``,
-        or throws a TypeError.
-        """
-        from sage.rings.function_field.finite_drinfeld_module import FiniteDrinfeldModule
+        # Avoid circular import
+        from sage.rings.function_field.drinfeld_module import DrinfeldModule
+        # If gen is not in the codomain, an exception is raised
         gen = self._codomain(gen)
         if self.characteristic()(gen[0]) != 0:
             raise ValueError('incorrect characteristic')
-        return FiniteDrinfeldModule(self._domain, gen)
+        return DrinfeldModule(self._domain, gen)
 
     def super_categories(self):
         return []
 
     def _repr_(self):
-        return f'Category of Drinfeld modules defined by {self._gamma}'
+        return f'Category of Drinfeld modules defined by {self._morphism}'
 
     def _make_named_class_key(self, name):
         return self._domain.category()
@@ -86,8 +73,8 @@ class DrinfeldModules(CategoryWithParameters):
     def codomain(self):
         return self._codomain
 
-    def gamma(self):
-        return self._gamma
+    def morphism(self):
+        return self._morphism
 
     class ParentMethods:
         def characteristic(self):
