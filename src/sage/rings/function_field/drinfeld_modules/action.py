942a9c8d95c0c59115ef55e533cd3f6ac999590c
Antoine Leudière <antoine.leudiere@inria.fr>
2022-08-18 17:17:21 +0200

    Fix DrinfeldModuleAction
    This class was all broken since it had not been updated since the
    begining of the project, which itself had evolved a lot in the meantime.

--- src/sage/rings/function_field/drinfeld_modules/action.py
+++ src/sage/rings/function_field/drinfeld_modules/action.py
@@ -4,34 +4,29 @@ from sage.rings.function_field.drinfeld_modules.drinfeld_module import DrinfeldM
 
 
 class DrinfeldModuleAction(Action):
-    def __init__(self, finite_drinfeld_module):
-        # Verifications
-        if not isinstance(finite_drinfeld_module, DrinfeldModule):
+
+    def __init__(self, drinfeld_module):
+        if not isinstance(drinfeld_module, DrinfeldModule):
             raise TypeError('input must be a DrinfeldModule')
-        # Work
-        self.__finite_drinfeld_module = finite_drinfeld_module
-        super().__init__(finite_drinfeld_module.polring(),
-                finite_drinfeld_module.ore_polring().base_ring())
+        self._drinfeld_module = drinfeld_module
+        super().__init__(drinfeld_module.function_ring(),
+                drinfeld_module.base_ring())
 
-    ###########
-    # Methods #
-    ###########
-
-    def finite_drinfeld_module(self):
-        return self.__finite_drinfeld_module
-
-    ##########################
-    # Special Sage functions #
-    ##########################
-
-    def _act_(self, g, x):
-        return self.finite_drinfeld_module()(g)(x)
+    def _act_(self, pol, x):
+        if pol not in self._drinfeld_module.function_ring():
+            raise TypeError('first input must be in the function ring')
+        if x not in self._drinfeld_module.base_ring():
+            raise TypeError('second input must be in the base ring')
+        return self._drinfeld_module(pol)(x)
 
     def _latex_(self):
         return f'\\text{{Action{{ }}on{{ }}}}' \
-                f'{latex(self.extension())}\\text{{{{ }}' \
-                f'induced{{ }}by{{ }}}}{self.finite_drinfeld_module()}'
+                f'{latex(self._drinfeld_module.base_ring())}\\text{{{{ }}' \
+                f'induced{{ }}by{{ }}}}{self._drinfeld_module}'
 
     def _repr_(self):
-        return f'Action on {self.domain()} induced by ' \
-                f'{self.finite_drinfeld_module()}'
+        return f'Action on {self._drinfeld_module.base_ring()} induced by ' \
+                f'{self._drinfeld_module}'
+
+    def drinfeld_module(self):
+        return self._drinfeld_module
