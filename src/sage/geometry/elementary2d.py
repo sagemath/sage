baf9ca47b98af8edb727a3a512ad4646bbdc14db
Kwankyu Lee <ekwankyu@gmail.com>
2022-09-04 20:49:52 +0900

    Fixes

--- src/sage/geometry/elementary2d.py
+++ src/sage/geometry/elementary2d.py
@@ -1,43 +1,155 @@
-class GeometricFigure:
+import math
+
+from sage.misc.lazy_attribute import lazy_attribute
+
+from sage.structure.sage_object import SageObject
+
+from sage.plot.graphics import Graphics
+
+from sage.modules.free_module_element import vector
+
+
+class GeometricFigure(SageObject):
     pass
+
+
+class Plane2D(GeometricFigure):
+    def __init__(self):
+        self._figures = []
+
+    def _repr_(self):
+        num = len(self._figures)
+        return f"Euclidean Plane containing {num} figures"
+
+    def add(self, figure):
+        self._figures.append(figure)
+
+    def clear(self):
+        self._figures = []
+
+    def show(self, *args, **kwargs):
+        g = Graphics()
+        for figure in self._figures:
+            g += figure.show()
+        if 'aspect_ratio' not in kwargs:
+            kwargs['aspect_ratio'] = True
+        g.show(*args, **kwargs)
+
+
+default_plane = Plane2D()
 
 
 class Figure2D(GeometricFigure):
-    pass
+    def __init__(self, plane=None):
+        if plane is None:
+            plane = default_plane
+        self._plane = plane
+        plane.add(self)
+
+    def plane(self):
+        return self._plane
+
+    @staticmethod
+    def _turn(vec, angle):
+        theta = math.atan(vec[1] / vec[0]) + angle
+        return vector([math.cos(theta), math.sin(theta)])
+
+    def point(self, t=0):
+        return Point(self._pvec(t))
+
+    def near_point(self, t=0, angle=None, distance=1):
+        if angle is None:
+            angle = math.pi / 2
+        return Point(self._pvec(t) + distance * self._turn(self._tvec(t), angle))
 
 
-class Point(Figure2D):
-    def __init__(self, pos):
-        self.pos = pos
+class Point2D(Figure2D):
+    def __init__(self, pos, plane=None):
+        super().__init__(plane=plane)
+        self.pos = tuple(pos)
+
+    def __iter__(self):
+        return iter(self.pos)
+
+    def _repr_(self):
+        return f"Point at {self.pos}"
+
+    def show(self, start=0, end=1, *args, **kwargs):
+        from sage.plot.point import point2d
+        return point2d(self.pos, *args, **kwargs)
 
 
-class Line(Figure2D):
-    def __init__(self, start, end):
-        self.start_point = start
-        self.end_point = end
+class Line2D(Figure2D):
+    """
+    EXAMPLES::
 
-    def intersect(fig):
-        pass
+        sage: from sage.geometry.elementary2d import Line
+        sage: l = Line((1,2), (4,7))
+        sage: p1 = l.near_point(t=0, distance=2)
+        sage: p2 = l.near_point(t=1, distance=2)
+        sage: l2 = Line(p1, p2)
+        sage: l3 = Line(l.start_point(), p1)
+        sage: l4 = Line(l.end_point(), p2)
+        sage: l.plane()
+        Euclidean Plane containing 8 figures
+        sage: l.plane().show()
+    """
+    def __init__(self, start, end, plane=None):
+        super().__init__(plane=plane)
+        self._start = tuple(start)
+        self._end = tuple(end)
 
-    def render(self, *args, **kwargs):
-        pass
+    def _repr_(self):
+        return f"Line from {self._start} to {self._end}"
+
+    @lazy_attribute
+    def _svec(self):
+        return vector(self._start)
+
+    @lazy_attribute
+    def _evec(self):
+        return vector(self._end)
+
+    def _pvec(self, t=0):
+        v = self._evec - self._svec
+        return self._svec + t*v
+
+    def _tvec(self, t=0):
+        v = self._evec - self._svec
+        v = ~(v.norm()) * v
+        if t >= 0:
+            return v
+        return -v
+
+    def start_point(self):
+        return Point(self._start)
+
+    def end_point(self):
+        return Point(self._end)
+
+    def show(self, start=0, end=1, *args, **kwargs):
+        from sage.plot.line import line2d
+        return line2d([self._start, self._end], *args, **kwargs)
 
 
-class Circle(Figure2D):
-    def __init__(self, center, radius):
+class Circle2D(Figure2D):
+    def __init__(self, center, radius, plane=None):
+        super().__init__(plane=plane)
         self.center = center
         self.length = length
 
 
-class Ellipse(Figure2D):
-    def __init__(self, focus1, focus2, semimajor):
+class Ellipse2D(Figure2D):
+    def __init__(self, focus1, focus2, semimajor, plane=None):
+        super().__init__(plane=plane)
         self.focus1 = focus1
         self.focus2 = focus2
         self.semimajor = semimajor
 
 
-class Parabola(Figure2D):
-    def __init__(self, focus, directrix: Line):
+class Parabola2D(Figure2D):
+    def __init__(self, focus, directrix: Line2D, plane=None):
+        super().__init__(plane=plane)
         self.focus: focus
         slef.directrix: directrix
 
--- src/sage/geometry/elementary3d.py
+++ src/sage/geometry/elementary3d.py
@@ -1,20 +1,152 @@
-class GeometricFigure:
-    pass
+import math
+
+from sage.misc.lazy_attribute import lazy_attribute
+
+from sage.structure.sage_object import SageObject
+
+from sage.plot.graphics import Graphics
+
+from sage.modules.free_module_element import vector
+
+from .elementary2d import GeometricFigure
+
+
+class Space3D(GeometricFigure):
+    def __init__(self):
+        self._figures = []
+
+    def _repr_(self):
+        num = len(self._figures)
+        return f"Euclidean Space containing {num} figures"
+
+    def add(self, figure):
+        self._figures.append(figure)
+
+    def clear(self):
+        self._figures = []
+
+    def show(self, *args, **kwargs):
+        g = Graphics()
+        for figure in self._figures:
+            g += figure.show()
+        if 'aspect_ratio' not in kwargs:
+            kwargs['aspect_ratio'] = True
+        g.show(*args, **kwargs)
+
+
+default_space = Space3D()
 
 
 class Figure3D(GeometricFigure):
-    pass
+    def __init__(self, space=None):
+        if space is None:
+            space = default_space
+        self._space = space
+        space.add(self)
+
+    def space(self):
+        return self._space
 
 
-class Point(Figure3D):
+class Point3D(Figure3D):
     def __init__(self, pos):
-        self.pos = pos
+        self._pos = pos
+
+    def __iter__(self):
+        return iter(self._pos)
+
+    def _repr_(self):
+        return f"Point at {self._pos}"
+
+    def show(self, start=0, end=1, *args, **kwargs):
+        from sage.plot3d.point import point3d
+        return point3d(self.pos, *args, **kwargs)
 
 
-class Line(Figure3D):
-    def __init__(self, start, end):
+class Line3D(Figure3D):
+    def __init__(self, start, end, space=None):
+        super().__init__(space=space)
         self.pos_start = start
         self.pos_end = end
 
+    def __init__(self, start, end, plane=None):
+        super().__init__(plane=plane)
+        self._start = tuple(start)
+        self._end = tuple(end)
+
+    def _repr_(self):
+        return f"Line from {self._start} to {self._end}"
+
+    @lazy_attribute
+    def _svec(self):
+        return vector(self._start)
+
+    @lazy_attribute
+    def _evec(self):
+        return vector(self._end)
+
+    def _pvec(self, t=0):
+        v = self._evec - self._svec
+        return self._svec + t*v
+
+    def _tvec(self, t=0):
+        v = self._evec - self._svec
+        v = ~(v.norm()) * v
+        if t >= 0:
+            return v
+        return -v
+
+    def start_point(self):
+        return Point(self._start)
+
+    def end_point(self):
+        return Point(self._end)
+
+    def show(self, start=0, end=1, *args, **kwargs):
+        from sage.plot3d.line import line3d
+        return line3d([self._start, self._end], *args, **kwargs)
 
 
+class Plane3D(Figure3D):
+    def __init__(self, pt1, pt2, pt3):
+        self._pos1 = tuple(pt1)
+        self._pos2 = tuple(pt2)
+        self._pos3 = tuple(pt3)
+
+    @lazy_attribute
+    def _ovec(self):
+        return vector(self._pos1)
+
+    @lazy_attribute
+    def _xvec(self):
+        v = vector(self._pos2) - self._ovec
+        v = ~(v.norm()) * v
+        return v
+
+    @lazy_attribute
+    def _yvec(self):
+        v1 = self._xvec
+        v2 = vector(self._pos3) - self._ovec
+        yvec = -(v1.inner_product(v2))*v1 + v2
+        if not yvec:
+            raise(f'points {self._pos1}, {self._pos2}, and {self._pos3} are colinear')
+        return yvec
+
+
+class PlanarPoint3D(Figure3D):
+    def __init__(self, point2d, plane, space=None):
+        super().__init__(space=space)
+        self._point2d = point2d
+        self._plane = plane
+
+class PlanarLine3D(Figure3D):
+    def __init__(self, line2d, plane, space=None):
+        super().__init__(space=space)
+        self._line2d = line2d
+        self._plane = plane
+
+class PlanarCircle3D(Figure3D):
+    def __init__(self, circle2d, plane, space=None):
+        super().__init__(space=space)
+        self._circle2d = circle2d
+        self._plane = plane
