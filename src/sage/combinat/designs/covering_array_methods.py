bb509f2a8f4f0fabb305241252af7bc845834b86
Aaron Dwyer <aarondwyer@cmail.carleton.ca>
2022-10-11 14:45:37 -0400

    finished the strength 2 construction

--- src/sage/combinat/designs/covering_array_methods.py
+++ src/sage/combinat/designs/covering_array_methods.py
@@ -1,1 +1,31 @@
-test
+import itertools
+import math
+
+def tuple_weight(t):
+    '''
+    given a tuple t returns the integer sum of all of its values
+    '''
+    ctr=0
+    for i in t:
+        ctr+=i
+    return ctr
+
+def Strength2(n):
+    '''
+    Given an integer n, returns a binary covering array with strength 2
+    To do this we form a matrix represented as a tuple of tuples where the 
+    columns are all binary n-tuples of weight ceiling(n/2) that have 0 
+    in 1st position
+    
+    Reference is Combinatorial aspects...
+    '''
+    resulttupleT=()
+    x = [0,1]
+    for p in itertools.product(x, repeat=n-1):
+        if tuple_weight(p)==math.ceil(n/2):
+            column=(0,)+p
+            resulttupleT+=(column,)
+    M=Matrix(resulttupleT)
+    M1=M.transpose()
+    return(tuple(M1))
+
