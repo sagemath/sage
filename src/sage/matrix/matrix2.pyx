7175d2f75c73a8db254e38b201991cb4deb0017e
Sandstorm831 <gargrohan138@gmail.com>
2023-01-06 22:33:39 +0530

    Revert "minor documentation addition"
    This reverts commit f0f4fc65c4aed652b2d4d7189247b8258d55fb94.

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -5213,11 +5213,6 @@ cdef class Matrix(Matrix1):
 
             sage: image(B) == B.row_module()
             True
-
-        ::
-
-           sage: image(B) = B.column_module()
-           True
         """
         return self.row_module()
 
