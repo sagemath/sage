81bec94c3616153279d2fc926d8d80c8080b8b6e
Frédéric Chapoton <chapoton@unistra.fr>
2023-01-22 09:57:25 +0100

    fix and activate W391 in cython files

--- src/sage/numerical/backends/generic_backend.pyx
+++ src/sage/numerical/backends/generic_backend.pyx
@@ -1819,4 +1819,3 @@ cpdef GenericBackend get_solver(constraint_generation = False, solver = None, ba
 
     else:
         raise ValueError("'solver' should be set to 'GLPK', 'GLPK/exact', 'Coin', 'CPLEX', 'CVXOPT', 'Gurobi', 'PPL', 'SCIP', 'InteractiveLP', None (in which case the default one is used), or a callable.")
-
