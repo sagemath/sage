0fcb6fbbe723ba9c591f10766843c53918f3430c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-01-22 17:22:25 -0800

    src/sage/rings/polynomial/commutative_polynomial.pyx: Add doctests

--- src/sage/rings/polynomial/commutative_polynomial.pyx
+++ src/sage/rings/polynomial/commutative_polynomial.pyx
@@ -5,6 +5,19 @@ cdef class CommutativePolynomial(CommutativeAlgebraElement):
     It is a common base for :class:`~sage.rings.polynomial.polynomial_element.Polynomial`,
     :class:`~sage.rings.polynomial.multi_polynomial.MPolynomial`, and
     :class:`~sage.rings.polynomial.infinite_polynomial_element.InfinitePolynomial`.
+
+    EXAMPLES::
+
+        sage: from sage.rings.polynomial.commutative_polynomial import CommutativePolynomial
+        sage: K.<x> = PolynomialRing(QQ)
+        sage: isinstance(x, CommutativePolynomial)
+        True
+        sage: K.<x,y> = PolynomialRing(QQ)
+        sage: isinstance(x, CommutativePolynomial)
+        True
+        sage: X.<x,y> = InfinitePolynomialRing(ZZ, implementation='sparse')
+        sage: isinstance(x[2], CommutativePolynomial)
+        True
     """
 
     pass
