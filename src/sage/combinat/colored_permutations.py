ef68b895ca130c3b500bd86813dc9d0a513d1790
Sandstorm831 <gargrohan138@gmail.com>
2023-02-12 23:29:48 +0530

    allowing iterables as input

--- src/sage/combinat/colored_permutations.py
+++ src/sage/combinat/colored_permutations.py
@@ -1361,7 +1361,7 @@ class SignedPermutations(ColoredPermutations):
             True
 
         """
-        if isinstance(x, list):
+        if isinstance(x, list) or isinstance(x, range):
             if x and isinstance(x[0], tuple):
                 c = []
                 p = []
