40db71da2dd06f7793d78e0d68ca171ec752a574
Sandstorm831 <gargrohan138@gmail.com>
2023-02-14 18:54:25 +0530

    adding reference

--- src/sage/combinat/posets/linear_extensions.py
+++ src/sage/combinat/posets/linear_extensions.py
@@ -268,7 +268,7 @@ class LinearExtensionOfPoset(ClonableArray,
         Informally, a linear extension is supergreedy if it "always
         goes up and receedes the least"; in other words, supergreedy
         linear extensions are depth-first linear extensions.
-        `Reference <https://link.springer.com/article/10.1007/BF00337892>`_
+        For more details see [KTZ1987]_.
 
         EXAMPLES::
         
