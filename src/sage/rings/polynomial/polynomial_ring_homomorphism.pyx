99a86dbb71fdb761da7a35dc6e28edec01a74dda
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-02-20 14:38:05 -0800

    More # optional

--- src/sage/rings/polynomial/polynomial_ring_homomorphism.pyx
+++ src/sage/rings/polynomial/polynomial_ring_homomorphism.pyx
@@ -73,18 +73,18 @@ cdef class PolynomialRingHomomorphism_from_base(RingHomomorphism_from_base):
 
             sage: from sage.rings.polynomial.polynomial_ring_homomorphism import PolynomialRingHomomorphism_from_base
             sage: R.<x> = ZZ[]
-            sage: S = GF(5)['x']
-            sage: f = ZZ.hom(GF(5))
-            sage: F = PolynomialRingHomomorphism_from_base(R.Hom(S), f)
-            sage: F(2*x, check=True)
+            sage: S = GF(5)['x']                                                # optional - sage.libs.pari
+            sage: f = ZZ.hom(GF(5))                                             # optional - sage.libs.pari
+            sage: F = PolynomialRingHomomorphism_from_base(R.Hom(S), f)         # optional - sage.libs.pari
+            sage: F(2 * x, check=True)                                          # optional - sage.libs.pari
             2*x
 
-            sage: k = GF(49, 'z')
-            sage: A = PolynomialRing(GF(7), 'x', sparse=True)
-            sage: B = PolynomialRing(k, 'x', sparse=True)
-            sage: g = GF(7).hom(k)
-            sage: G = PolynomialRingHomomorphism_from_base(A.Hom(B), g)
-            sage: G(A.gen()^1000000, True, construct=False)
+            sage: k = GF(49, 'z')                                               # optional - sage.libs.pari
+            sage: A = PolynomialRing(GF(7), 'x', sparse=True)                   # optional - sage.libs.pari
+            sage: B = PolynomialRing(k, 'x', sparse=True)                       # optional - sage.libs.pari
+            sage: g = GF(7).hom(k)                                              # optional - sage.libs.pari
+            sage: G = PolynomialRingHomomorphism_from_base(A.Hom(B), g)         # optional - sage.libs.pari
+            sage: G(A.gen()^1000000, True, construct=False)                     # optional - sage.libs.pari
             x^1000000
 
         """
