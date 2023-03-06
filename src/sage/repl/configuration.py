179e58aa55bad71da8deda20bfd31a55538a5c02
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-03-05 21:33:25 -0800

    sage.repl: Add # optional - pexpect

--- src/sage/repl/configuration.py
+++ src/sage/repl/configuration.py
@@ -7,11 +7,11 @@ We check that Sage stdin can be piped in even if stdout is a tty; In that case
 the IPython simple prompt is being used::
 
     sage: cmd = 'print([sys.stdin.isatty(), sys.stdout.isatty()])'
-    sage: import pexpect
-    sage: output = pexpect.run(
+    sage: import pexpect                                                                # optional - pexpect
+    sage: output = pexpect.run(                                                         # optional - pexpect
     ....:     'bash -c \'echo "{0}" | sage\''.format(cmd),
     ....: ).decode('utf-8', 'surrogateescape')
-    sage: 'sage: [False, True]' in output
+    sage: 'sage: [False, True]' in output                                               # optional - pexpect
     True
 """
 
