21e0ba0ca27bf4325a656bec2c8b0bb600be2be2
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-03-05 21:32:45 -0800

    sage.calculus: Add # optional - numpy, sympy

--- src/sage/calculus/test_sympy.py
+++ src/sage/calculus/test_sympy.py
@@ -102,98 +102,98 @@ instead of SymPy.
 
 And here are some actual tests of sympy::
 
-    sage: from sympy import Symbol, cos, sympify, pprint
-    sage: from sympy.abc import x
+    sage: from sympy import Symbol, cos, sympify, pprint                                # optional - sympy
+    sage: from sympy.abc import x                                                       # optional - sympy
 
 ::
 
-    sage: e = (1/cos(x)^3)._sympy_(); e
+    sage: e = (1/cos(x)^3)._sympy_(); e                                                 # optional - sympy
     cos(x)**(-3)
-    sage: f = e.series(x, 0, int(10)); f
+    sage: f = e.series(x, 0, int(10)); f                                                # optional - sympy
     1 + 3*x**2/2 + 11*x**4/8 + 241*x**6/240 + 8651*x**8/13440 + O(x**10)
 
 And the pretty-printer.  Since unicode characters are not working on
 some architectures, we disable it::
 
-    sage: from sympy.printing import pprint_use_unicode
-    sage: prev_use = pprint_use_unicode(False)
-    sage: pprint(e)
+    sage: from sympy.printing import pprint_use_unicode                                 # optional - sympy
+    sage: prev_use = pprint_use_unicode(False)                                          # optional - sympy
+    sage: pprint(e)                                                                     # optional - sympy
        1
     -------
        3
     cos (x)
 
-    sage: pprint(f)
+    sage: pprint(f)                                                                     # optional - sympy
            2       4        6         8
         3*x    11*x    241*x    8651*x     / 10\
     1 + ---- + ----- + ------ + ------- + O\x  /
          2       8      240      13440
-    sage: pprint_use_unicode(prev_use)
+    sage: pprint_use_unicode(prev_use)                                                  # optional - sympy
     False
 
 And the functionality to convert from sympy format to Sage format::
 
-    sage: e._sage_()
+    sage: e._sage_()                                                                    # optional - sympy
     cos(x)^(-3)
-    sage: e._sage_().taylor(x._sage_(), 0, 8)
+    sage: e._sage_().taylor(x._sage_(), 0, 8)                                           # optional - sympy
     8651/13440*x^8 + 241/240*x^6 + 11/8*x^4 + 3/2*x^2 + 1
-    sage: f._sage_()
+    sage: f._sage_()                                                                    # optional - sympy
     8651/13440*x^8 + 241/240*x^6 + 11/8*x^4 + 3/2*x^2 + Order(x^10) + 1
 
 Mixing SymPy with Sage::
 
-    sage: import sympy
-    sage: var("x")._sympy_() + var("y")._sympy_()
+    sage: import sympy                                                                  # optional - sympy
+    sage: var("x")._sympy_() + var("y")._sympy_()                                       # optional - sympy
     x + y
-    sage: o = var("omega")
-    sage: s = sympy.Symbol("x")
-    sage: t1 = s + o
-    sage: t2 = o + s
-    sage: type(t1)
+    sage: o = var("omega")                                                              # optional - sympy
+    sage: s = sympy.Symbol("x")                                                         # optional - sympy
+    sage: t1 = s + o                                                                    # optional - sympy
+    sage: t2 = o + s                                                                    # optional - sympy
+    sage: type(t1)                                                                      # optional - sympy
     <class 'sympy.core.add.Add'>
-    sage: type(t2)
+    sage: type(t2)                                                                      # optional - sympy
     <class 'sage.symbolic.expression.Expression'>
-    sage: t1, t2
+    sage: t1, t2                                                                        # optional - sympy
     (omega + x, omega + x)
-    sage: e=sympy.sin(var("y"))+sage.all.cos(sympy.Symbol("x"))
-    sage: type(e)
+    sage: e = sympy.sin(var("y"))+sage.all.cos(sympy.Symbol("x"))                       # optional - sympy
+    sage: type(e)                                                                       # optional - sympy
     <class 'sympy.core.add.Add'>
-    sage: e
+    sage: e                                                                             # optional - sympy
     sin(y) + cos(x)
-    sage: e=e._sage_()
-    sage: type(e)
+    sage: e=e._sage_()                                                                  # optional - sympy
+    sage: type(e)                                                                       # optional - sympy
     <class 'sage.symbolic.expression.Expression'>
-    sage: e
+    sage: e                                                                             # optional - sympy
     cos(x) + sin(y)
-    sage: e = sage.all.cos(var("y")**3)**4+var("x")**2
-    sage: e = e._sympy_()
-    sage: e
+    sage: e = sage.all.cos(var("y")**3)**4+var("x")**2                                  # optional - sympy
+    sage: e = e._sympy_()                                                               # optional - sympy
+    sage: e                                                                             # optional - sympy
     x**2 + cos(y**3)**4
 
 ::
 
-    sage: a = sympy.Matrix([1, 2, 3])
-    sage: a[1]
+    sage: a = sympy.Matrix([1, 2, 3])                                                   # optional - sympy
+    sage: a[1]                                                                          # optional - sympy
     2
 
 ::
 
-    sage: sympify(1.5)
+    sage: sympify(1.5)                                                                  # optional - sympy
     1.50000000000000
-    sage: sympify(2)
+    sage: sympify(2)                                                                    # optional - sympy
     2
-    sage: sympify(-2)
+    sage: sympify(-2)                                                                   # optional - sympy
     -2
 
 TESTS:
 
 This was fixed in Sympy, see :trac:`14437`::
 
-    sage: from sympy import Function, Symbol, rsolve
-    sage: u = Function('u')
-    sage: n = Symbol('n', integer=True)
-    sage: f = u(n+2) - u(n+1) + u(n)/4
-    sage: expand(2**n * rsolve(f,u(n)))
+    sage: from sympy import Function, Symbol, rsolve                                    # optional - sympy
+    sage: u = Function('u')                                                             # optional - sympy
+    sage: n = Symbol('n', integer=True)                                                 # optional - sympy
+    sage: f = u(n+2) - u(n+1) + u(n)/4                                                  # optional - sympy
+    sage: expand(2**n * rsolve(f,u(n)))                                                 # optional - sympy
     2*C1*n + C0
 
 """
