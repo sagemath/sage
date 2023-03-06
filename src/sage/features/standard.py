a9fc02ed3f3af963d18100a610cf484cb34ff44b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-03-05 21:44:26 -0800

    src/sage/features/standard.py: Fix up, rename features to match spkgs

--- src/sage/features/standard.py
+++ src/sage/features/standard.py
@@ -4,20 +4,20 @@ Check for various standard packages (for modularized distributions)
 These features are provided by standard packages in the Sage distribution.
 """
 from . import PythonModule
-from . import JoinFeature
+from .join_feature import JoinFeature
 
 
 def all_features():
     return [PythonModule('cvxopt', spkg='cvxopt'),
             PythonModule('fpylll', spkg='fpylll'),
-            PythonModule('IPython', spkg='ipython'),
-            PythonModule('lrcalc', spkg='lrcalc_python'),
+            JoinFeature('ipython', [PythonModule('IPython')], spkg='ipython'),
+            JoinFeature('lrcalc_python', [PythonModule('lrcalc')], spkg='lrcalc_python'),
             PythonModule('mpmath', spkg='mpmath'),
             PythonModule('networkx', spkg='networkx'),
             PythonModule('numpy', spkg='numpy'),
             PythonModule('pexpect', spkg='pexpect'),
-            PythonModule('PIL', spkg='pillow'),
-            PythonModule('ppl', spkg='pplpy'),
+            JoinFeature('pillow', [PythonModule('PIL')], spkg='pillow'),
+            JoinFeature('pplpy', [PythonModule('ppl')], spkg='pplpy'),
             PythonModule('primecountpy', spkg='primecountpy'),
             PythonModule('ptyprocess', spkg='ptyprocess'),
             PythonModule('requests', spkg='requests'),
