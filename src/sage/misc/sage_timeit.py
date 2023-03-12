a84fc77082dca79209d1e0a0634a3e8871fcdafa
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-03-12 13:36:51 -0700

    sage.misc: More # optional

--- src/sage/misc/sage_timeit.py
+++ src/sage/misc/sage_timeit.py
@@ -37,7 +37,7 @@ class SageTimeitResult():
     EXAMPLES::
 
         sage: from sage.misc.sage_timeit import SageTimeitResult
-        sage: SageTimeitResult( (3, 5, int(8), pi, 'ms') )
+        sage: SageTimeitResult( (3, 5, int(8), pi, 'ms') )                              # optional - sage.symbolic
         3 loops, best of 5: 3.1415927 ms per loop
 
     ::
@@ -69,10 +69,10 @@ class SageTimeitResult():
         EXAMPLES::
 
             sage: from sage.misc.sage_timeit import SageTimeitResult
-            sage: SageTimeitResult( (3, 5, int(8), pi, 'ms') )
+            sage: SageTimeitResult( (3, 5, int(8), pi, 'ms') )                          # optional - sage.symbolic
             3 loops, best of 5: 3.1415927 ms per loop
-            sage: s = SageTimeitResult( (3, 5, int(8), pi, 'ms'), [1.0,1.1,0.5])
-            sage: s.series
+            sage: s = SageTimeitResult( (3, 5, int(8), pi, 'ms'), [1.0,1.1,0.5])        # optional - sage.symbolic
+            sage: s.series                                                              # optional - sage.symbolic
             [1.00000000000000, 1.10000000000000, 0.500000000000000]
         """
         self.stats = stats
@@ -85,8 +85,8 @@ class SageTimeitResult():
         EXAMPLES::
 
             sage: from sage.misc.sage_timeit import SageTimeitResult
-            sage: stats = (1, 2, int(3), pi, 'ns')
-            sage: SageTimeitResult(stats)           #indirect doctest
+            sage: stats = (1, 2, int(3), pi, 'ns')                                      # optional - sage.symbolic
+            sage: SageTimeitResult(stats)           #indirect doctest                   # optional - sage.symbolic
             1 loop, best of 2: 3.14 ns per loop
         """
         if self.stats[0] > 1:
