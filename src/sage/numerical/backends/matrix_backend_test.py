ce437757cfaf072d43e7fbe58581d1401477fa82
alexandre <alexandremr01@gmail.com>
2023-03-26 22:01:40 +0200

    add test file

---
+++ src/sage/numerical/backends/matrix_backend_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from sage.numerical.backends.generic_backend_test import GenericBackendTests
+from sage.numerical.backends.generic_backend import GenericBackend
+from sage.numerical.mip import MixedIntegerLinearProgram
+
+
+class TestMatrixBackend(GenericBackendTests):
+
+    @pytest.fixture
+    def backend(self) -> GenericBackend:
+        return MixedIntegerLinearProgram(solver="Matrix").get_backend()
