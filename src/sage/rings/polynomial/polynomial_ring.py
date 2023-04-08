cfc6b42d7a2504515b309f39e4076ce48f6b3ef8
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-04-07 19:54:15 -0700

    src/sage/rings/polynomial/polynomial_ring.py: Fix up import

--- src/sage/rings/polynomial/polynomial_ring.py
+++ src/sage/rings/polynomial/polynomial_ring.py
@@ -172,6 +172,7 @@ from sage.rings.fraction_field_element import FractionFieldElement
 from sage.rings.finite_rings.element_base import FiniteRingElement
 from sage.rings.polynomial.polynomial_singular_interface import PolynomialRing_singular_repr
 from sage.rings.polynomial.polynomial_singular_interface import can_convert_to_singular
+from sage.rings.power_series_ring_element import PowerSeries
 
 _CommutativeRings = categories.commutative_rings.CommutativeRings()
 
