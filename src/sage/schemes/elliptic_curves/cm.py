89957348ede25ef59514e5a2d1f82deaf67d46d5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-04-24 20:28:45 -0700

    src/sage/schemes/elliptic_curves: Restore to upstream

--- src/sage/schemes/elliptic_curves/cm.py
+++ src/sage/schemes/elliptic_curves/cm.py
@@ -35,12 +35,12 @@ AUTHORS:
 #                  https://www.gnu.org/licenses/
 # ****************************************************************************
 
-from sage.arith.misc import is_fundamental_discriminant
 from sage.interfaces.magma import magma
 from sage.rings.integer import Integer
 from sage.rings.rational_field import QQ
 from sage.rings.integer_ring import ZZ
 from sage.rings.integer_ring import IntegerRing
+from sage.rings.number_field.number_field import is_fundamental_discriminant
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 
 from sage.misc.cachefunc import cached_function
