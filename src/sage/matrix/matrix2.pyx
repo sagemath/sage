1854f52fe99c817f70df4cd03a1adedb8a444b2d
Dima Pasechnik <dima@pasechnik.info>
2023-05-10 14:29:49 +0100

    Update src/sage/matrix/matrix2.pyx
    Co-authored-by: Gonzalo Tornaría <tornaria@gmail.com>

--- src/sage/matrix/matrix2.pyx
+++ src/sage/matrix/matrix2.pyx
@@ -15428,7 +15428,7 @@ cdef class Matrix(Matrix1):
         Sparse matrices are handled correctly (:trac:`28935`), but may
         require a patched version of maxima (:trac:`32898`) for now::
 
-            sage: matrix.diagonal([0], sparse=True).exp() # not tested, requires patched maxima
+            sage: matrix.diagonal([0], sparse=True).exp()  # not tested, requires patched maxima
             [1]
             sage: matrix.zero(CBF, 2, sparse=True).exp()
             [1.000000000000000                 0]
