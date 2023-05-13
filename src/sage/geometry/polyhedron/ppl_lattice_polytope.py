293c532969ceb79b0877c5d951cdd18fa4f75f7e
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-05-13 11:49:55 -0700

    sage.geometry: Fix # optional, reformat doctests

--- src/sage/geometry/polyhedron/ppl_lattice_polytope.py
+++ src/sage/geometry/polyhedron/ppl_lattice_polytope.py
@@ -989,7 +989,6 @@ class LatticePolytope_PPL_class(C_Polyhedron):
             True
             sage: (set(tuple(x) for x in G.orbit(Z3square.vertices()[0]))               # optional - sage.groups sage.graphs
             ....:   == set([(0, 0), (1, 2), (3, 3), (2, 1)]))
-
             True
             sage: cell24 = LatticePolytope_PPL(
             ....:     (1,0,0,0), (0,1,0,0), (0,0,1,0), (0,0,0,1), (1,-1,-1,1), (0,0,-1,1),
