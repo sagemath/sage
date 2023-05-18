a68115e166522d5f0e0ae3655409867f23c5d019
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-05-17 19:53:44 -0700

    src/sage/categories/finite_coxeter_groups.py: Fixup

--- src/sage/categories/finite_coxeter_groups.py
+++ src/sage/categories/finite_coxeter_groups.py
@@ -750,8 +750,8 @@ class FiniteCoxeterGroups(CategoryWithAxiom):
             v = sum(point[i-1] * weights[i] for i in weights.keys())
             vertices = [v*w for w in self]
             if base_ring is None:
-                if isinstance(v.base_ring(), [sage.rings.abc.UniversalCyclotomicField,
-                                              sage.rings.abc.AlgebraicField_common]):
+                if isinstance(v.base_ring(), (sage.rings.abc.UniversalCyclotomicField,
+                                              sage.rings.abc.AlgebraicField_common)):
                     from sage.rings.qqbar import AA
                     vertices = [v.change_ring(AA) for v in vertices]
                     base_ring = AA
