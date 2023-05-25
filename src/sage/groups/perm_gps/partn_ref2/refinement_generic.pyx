737876c3760c09d635c8b4f0cf168f12b807f6b6
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-05-24 20:17:15 -0700

    sage.groups.perm_gps.partn_ref*: Remove dependency of everything on GAP

--- src/sage/groups/perm_gps/partn_ref2/refinement_generic.pyx
+++ src/sage/groups/perm_gps/partn_ref2/refinement_generic.pyx
@@ -941,3 +941,40 @@ cdef class PartitionRefinement_generic:
         """
         if BACKTRACK_WITHLATEX_DEBUG:
             self._latex_debug_string += "]\n"
+
+
+cdef int PS_first_smallest_PR(PartitionStack *PS, bitset_t b, int *second_pos=NULL,
+                              PartitionRefinement_generic partn_ref_alg=None):
+    """
+    Find the first occurrence of the smallest cell of size greater than one,
+    which is admissible (checked by the function ``test_allowance``).
+    Its entries are stored to b and its minimum element is returned.
+
+    This generalizes :func:`sage.groups.perm_gps.partn_ref.data_structures.PS_first_smallest`.
+    """
+    cdef int i = 0, j = 0, location = 0, n = PS.degree
+    bitset_zero(b)
+    while 1:
+        if PS.levels[i] <= PS.depth:
+            if i != j and n > i - j + 1 and (partn_ref_alg is None or
+                                partn_ref_alg._minimization_allowed_on_col(PS.entries[j])):
+                n = i - j + 1
+                location = j
+            j = i + 1
+        if PS.levels[i] == -1: break
+        i += 1
+    # location now points to the beginning of the first, smallest,
+    # nontrivial cell
+    i = location
+    while 1:
+        bitset_flip(b, PS.entries[i])
+        if PS.levels[i] <= PS.depth: break
+        i += 1
+
+    if second_pos != NULL:
+        if n==2:
+            second_pos[0] = PS.entries[location+1]
+        else:
+            second_pos[0] = -1
+
+    return PS.entries[location]
