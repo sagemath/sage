f50376209c5ef888f2721aa1a7e1db65a519649a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-06-09 21:42:38 -0700

    Fix markup

--- src/sage/features/csdp.py
+++ src/sage/features/csdp.py
@@ -25,7 +25,7 @@ from . import Executable, FeatureTestResult
 class CSDP(Executable):
     r"""
     A :class:`~sage.features.Feature` which checks for the ``theta`` binary
-    of :ref:`CSDP <spkg_sdp>`.
+    of :ref:`CSDP <spkg_csdp>`.
 
     EXAMPLES::
 
