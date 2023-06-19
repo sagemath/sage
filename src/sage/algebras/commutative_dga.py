5fd9c29cc841c296fcd9e80b8c2fdbd0b2465dca
miguelmarco <mmarco@unizar.es>
2023-06-19 23:49:57 +0200

    Update src/sage/algebras/commutative_dga.py
    Co-authored-by: Travis Scrimshaw <clfrngrown@aol.com>

--- src/sage/algebras/commutative_dga.py
+++ src/sage/algebras/commutative_dga.py
@@ -2605,8 +2605,6 @@ class DifferentialGCAlgebra(GCAlgebra):
                b --> 0
                c --> 0
               Defn: (x1_0, x1_1, x1_2, y1_0, y1_1, y1_2) --> (a, b, c, 0, 0, 0)
-
-
         REFERENCES:
 
         - [Fel2001]_
