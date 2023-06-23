d4240ed7603a8980d8ca8bb73fbf8dac16c61690
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-06-22 20:20:58 -0700

    src/sage/libs/mpmath/__init__.py: import all from vendored mpmath

--- src/sage/libs/mpmath/__init__.py
+++ src/sage/libs/mpmath/__init__.py
@@ -1,24 +1,8 @@
 # sage_setup: distribution = sagemath-mpmath
-import os
-import sys
 
-import sage
-breakpoint()
+from ._vendor.mpmath import *
+from ._vendor.mpmath.libmp.backend import BACKEND as _BACKEND
 
-os.environ['MPMATH_SAGE'] = '1'
+assert _BACKEND == 'sage'
 
-# https://github.com/mpmath/mpmath/blob/master/mpmath/libmp/backend.py tries to load sage.all
-if 'sage.all' in sys.modules:
-    sage_toplevel = None
-else:
-    import sage.all__sagemath_categories as sage_toplevel
-    sys.modules['sage.all'] = sage.all = sage_toplevel
-try:
-    import mpmath as mpmath  # This
-    import mpmath.libmp.backend
-finally:
-    if sage_toplevel:
-        del sage.all
-        del sys.modules['sage.all']
-
-assert mpmath.libmp.backend.BACKEND == 'sage'
+del _BACKEND
