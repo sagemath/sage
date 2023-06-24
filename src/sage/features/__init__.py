ad202baceeabac24a1170a24c8de9b8dbef52ca4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-06-24 09:39:34 -0700

    Add 'type=standard' in more places

--- src/sage/features/__init__.py
+++ src/sage/features/__init__.py
@@ -870,7 +870,7 @@ class CythonFeature(Feature):
         ....:
         ....: assert fabs(-1) == 1
         ....: '''
-        sage: fabs = CythonFeature("fabs", test_code=fabs_test_code, spkg="gcc", url="https://gnu.org")
+        sage: fabs = CythonFeature("fabs", test_code=fabs_test_code, spkg="gcc", url="https://gnu.org", type="standard")
         sage: fabs.is_present()
         FeatureTestResult('fabs', True)
 
