72040ed832186bfeb72cc659d5cfc2d38d89154d
OP5642 <ognjenpetrov@yahoo.com>
2023-06-27 18:53:24 +0200

    Started working on __init__ and construct()

--- src/sage/topology/moment_angle_complex.py
+++ src/sage/topology/moment_angle_complex.py
@@ -1,14 +1,58 @@
 from sage.homology.homology_group import HomologyGroup
+from sage.structure.sage_object import SageObject
+from .simplicial_complex import SimplicialComplex
+from .simplicial_complex_examples import Sphere, Simplex
+from itertools import combinations
 
-class MomentAngleComplex:
-    def __init__(self, delta):
-        self.delta = delta
-        self.z_delta = None
-        self.z_delta_topological = None
+#TODO's : 
+# - Documentation
+# - __eq__, __ne__
+# - and a lot more ...
+
+class MomentAngleComplex(SimplicialComplex):
+    def __init__(self, X, construct=False):
+        # check whether X is an instance of SimplicialComplex
+        self._simplicial_complex = X
+        self._constructed = construct
+
+        n = X.dimension()
+        vertices = X.vertices()
+        k = len(vertices);
+        self._components = []
+        self._symbolic_components = []
+
+        for face in X.faces().values():
+            for subcomplex in face:
+                Y = []
+                Ys = []
+                for j in vertices:
+                    if j in subcomplex:
+                        Y.append(SimplicialComplex(Simplex(2)))
+                        Ys.append("D2")
+                    else:
+                        Y.append(SimplicialComplex(Sphere(1)))
+                        Ys.append("S1")
+
+                self._components.append(Y)
+                self._symbolic_components.append(Ys)
+
+        print(self._components)
+        print(self._symbolic_components)
+
+        if construct:
+            self.construct()
+
+        Z = SimplicialComplex()
 
     def construct(self):
-        self.z_delta = self.delta.moment_angle_complex()
-        self.z_delta_topological = CellComplex(self.z_delta).topological_space()
+        for component in self._components:
+            x = component[0]
+            for y in component:
+                # this is terribly slow, look for solutions
+                # x = x.product(y, rename_vertices=False)
+                #print(x)
+
+        self._constructed = True
 
     def vertices(self):
         return self.z_delta.vertices()
