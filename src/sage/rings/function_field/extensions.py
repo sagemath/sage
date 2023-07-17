8fb435f5bc759e84149a54cb55441d047648741b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-07-16 20:10:30 -0700

    sage.rings.function_field: Update # needs

--- src/sage/rings/function_field/extensions.py
+++ src/sage/rings/function_field/extensions.py
@@ -16,11 +16,11 @@ Constant field extension of the rational function field over rational numbers::
     sage: L                                                                             # needs sage.rings.number_field
     Rational function field in x over Number Field in a with defining
     polynomial x^2 - 2 with a = 1.4142... over its base
-    sage: d = (x^2 - 2).divisor()                                                       # needs sage.libs.pari
-    sage: d                                                                             # needs sage.libs.pari
+    sage: d = (x^2 - 2).divisor()                                                       # needs sage.libs.pari sage.modules
+    sage: d                                                                             # needs sage.libs.pari sage.modules
     -2*Place (1/x)
      + Place (x^2 - 2)
-    sage: L.conorm_divisor(d)                                                           # needs sage.libs.pari sage.rings.number_field
+    sage: L.conorm_divisor(d)                                                           # needs sage.libs.pari sage.modules sage.rings.number_field
     -2*Place (1/x)
      + Place (x - a)
      + Place (x + a)
