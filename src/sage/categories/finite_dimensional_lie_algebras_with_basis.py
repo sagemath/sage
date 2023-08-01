df10a80d911218a3c320173768b58bd7ff38fdcb
adityadwarkesh <121101774+adityadwarkesh@users.noreply.github.com>
2023-08-01 16:49:03 -0600

    Update finite_dimensional_lie_algebras_with_basis.py

--- src/sage/categories/finite_dimensional_lie_algebras_with_basis.py
+++ src/sage/categories/finite_dimensional_lie_algebras_with_basis.py
@@ -1067,7 +1067,7 @@ class FiniteDimensionalLieAlgebrasWithBasis(CategoryWithAxiom_over_base_ring):
             """
             return not self.killing_form_matrix().is_singular()
 
-        #@cached_method(key=lambda self,M,d,s,n: (M,d,s))
+        @cached_method(key=lambda self,M,d,s,n: (M,d,s))
         def chevalley_eilenberg_complex(self, M, h, dual=False, sparse=True, ncpus=None):
             r"""
             Return the Chevalley-Eilenberg complex of ``self``.
