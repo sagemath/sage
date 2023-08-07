beada5d7c111de91c33124c89d3dc8e94423365d
Volker Braun <vbraun.name@gmail.com>
2023-08-08 00:57:26 +0200

    Fix memory leak in ginac numeric.cpp

---
+++ src/sage/tests/memcheck/symbolic_expression.py
@@ -0,0 +1,12 @@
+from sage.tests.memcheck.verify_no_leak import verify_no_leak
+
+
+def test_sqrt_sqrt_2() -> None:
+    from sage.all import sqrt
+    T2 = sqrt(2)
+    
+    def sqrt_T2() -> None:
+        sqrt(T2)
+        
+    verify_no_leak(sqrt_T2)
+
