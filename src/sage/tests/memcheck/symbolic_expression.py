758ac88a37fe8f48771b0cb74bbd7c33b46dce6c
Frédéric Chapoton <fchapoton2@gmail.com>
2023-08-09 08:29:41 +0200

    Update symbolic_expression.py
    pep8 and import cleanup

--- src/sage/tests/memcheck/symbolic_expression.py
+++ src/sage/tests/memcheck/symbolic_expression.py
@@ -2,11 +2,10 @@ from sage.tests.memcheck.verify_no_leak import verify_no_leak
 
 
 def test_sqrt_sqrt_2() -> None:
-    from sage.all import sqrt
+    from sage.misc.functional import sqrt
     T2 = sqrt(2)
-    
+
     def sqrt_T2() -> None:
         sqrt(T2)
-        
-    verify_no_leak(sqrt_T2)
 
+    verify_no_leak(sqrt_T2)
