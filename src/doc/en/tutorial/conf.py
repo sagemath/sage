b71b39733ba0ee7c9a4e9ec8db7531ad3299429f
John H. Palmieri <jhpalmieri64@gmail.com>
2023-08-10 11:48:34 -0700

    Fix link to tutorial in PDF docs. This fixes #35736.

--- src/doc/en/tutorial/conf.py
+++ src/doc/en/tutorial/conf.py
@@ -33,6 +33,6 @@ htmlhelp_basename = 'sage_tutorial'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'sage_tutorial.tex', 'Tutorial',
+  ('index', 'SageTutorial.tex', project,
    'The Sage Development Team', 'manual'),
 ]
