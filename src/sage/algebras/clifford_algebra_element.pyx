28588b78a4bb66f4786a59235443a853c4e53e79
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-08-12 21:49:25 -0700

    Add # needs

--- src/sage/algebras/clifford_algebra_element.pyx
+++ src/sage/algebras/clifford_algebra_element.pyx
@@ -945,7 +945,7 @@ cdef class CohomologyRAAGElement(CliffordAlgebraElement):
 
         EXAMPLES::
 
-            sage: # needs sage.graphs
+            sage: # needs sage.graphs sage.groups
             sage: C4 = graphs.CycleGraph(4)
             sage: A = groups.misc.RightAngledArtin(C4)
             sage: H = A.cohomology()
