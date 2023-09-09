2a84d764aa25727305cc43eaae5208ba381cfc6a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-09-08 22:59:40 -0700

    sage.tests: Update # needs

--- src/sage/tests/book_stein_modform.py
+++ src/sage/tests/book_stein_modform.py
@@ -1,4 +1,5 @@
-# sage.doctest: needs sage.modular
+# sage_setup: distribution = sagemath-repl
+# sage.doctest: needs sage.libs.flint sage.modular
 """
 This file contains a bunch of tests extracted from the published book
 'Modular Forms: a Computational Approach' by William Stein, AMS 2007.
