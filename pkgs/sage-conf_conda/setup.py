870ce5d32b19946e21bdf26f455dd1a962cba7aa
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-09-30 12:16:08 -0700

    pkgs/sage-conf_conda/setup.py: In editable mode, do not make a copy of SAGE_ROOT

--- pkgs/sage-conf_conda/setup.py
+++ pkgs/sage-conf_conda/setup.py
@@ -10,15 +10,51 @@ from pathlib import Path
 from setuptools import setup
 from distutils.command.build_scripts import build_scripts as distutils_build_scripts
 from setuptools.command.build_py import build_py as setuptools_build_py
-from setuptools.command.egg_info import egg_info as setuptools_egg_info
-from distutils.errors import (DistutilsSetupError, DistutilsModuleError,
-                              DistutilsOptionError)
+from setuptools.errors import SetupError
+
 
 class build_py(setuptools_build_py):
 
     def run(self):
-        DOT_SAGE = os.environ.get('DOT_SAGE', os.path.join(os.environ.get('HOME'), '.sage'))
+
         HERE = os.path.dirname(__file__)
+        if self.editable_mode:
+            SAGE_ROOT = os.path.join(HERE, 'sage_root')
+        else:
+            SAGE_ROOT = self._create_writable_sage_root()
+
+        if os.path.exists(os.path.join(SAGE_ROOT, 'config.status')):
+            print(f'Reusing configured SAGE_ROOT={SAGE_ROOT}')
+        else:
+            cmd = f"cd {SAGE_ROOT} && ./configure --enable-build-as-root --with-system-python3=force --disable-notebook --disable-sagelib --disable-sage_conf --disable-doc"
+            cmd += ' --with-python=$CONDA_PREFIX/bin/python --prefix="$CONDA_PREFIX" --enable-system-site-packages'
+            cmd += ' $(for pkg in $(PATH="build/bin:$PATH" build/bin/sage-package list :standard: --exclude rpy2 --has-file spkg-configure.m4 --has-file distros/conda.txt); do echo --with-system-$pkg=force; done)'
+            print(f"Running {cmd}")
+            sys.stdout.flush()
+            if os.system(cmd) != 0:
+                if os.path.exists(os.path.join(SAGE_ROOT, 'config.status')):
+                    print("Warning: A configuration has been written, but the configure script has exited with an error. "
+                          "Carefully check any error messages before continuing.")
+                else:
+                    print(f"Error: The configure script has failed; this may be caused by missing build prerequisites.")
+                    sys.stdout.flush()
+                    PREREQ_SPKG = "_prereq bzip2 xz libffi"  # includes python3 SPKG_DEPCHECK packages
+                    os.system(f'cd {SAGE_ROOT} && export SYSTEM=$(build/bin/sage-guess-package-system 2>/dev/null) && export PACKAGES="$(build/bin/sage-get-system-packages $SYSTEM {PREREQ_SPKG})" && [ -n "$PACKAGES" ] && echo "You can install the required build prerequisites using the following shell command" && echo "" && build/bin/sage-print-system-package-command $SYSTEM --verbose --sudo install $PACKAGES && echo ""')
+                    raise SetupError("configure failed")
+
+        # In this mode, we never run "make".
+
+        if not self.editable_mode:
+            # Install configuration
+            shutil.copyfile(os.path.join(SAGE_ROOT, 'pkgs', 'sage-conf', '_sage_conf', '_conf.py'),
+                            os.path.join(HERE, '_sage_conf', '_conf.py'))
+        shutil.copyfile(os.path.join(SAGE_ROOT, 'src', 'bin', 'sage-env-config'),
+                        os.path.join(HERE, 'bin', 'sage-env-config'))
+
+        setuptools_build_py.run(self)
+
+    def _create_writable_sage_root(self):
+        DOT_SAGE = os.environ.get('DOT_SAGE', os.path.join(os.environ.get('HOME'), '.sage'))
         with open(os.path.join(HERE, 'VERSION.txt')) as f:
             sage_version = f.read().strip()
         # After #30534, SAGE_LOCAL no longer contains any Python.  So we key the SAGE_ROOT only to Sage version
@@ -26,7 +62,7 @@ class build_py(setuptools_build_py):
         system = platform.system()
         machine = platform.machine()
         arch_tag = f'{system}-{machine}'
-        # TODO: These two should be user-configurable with options passed to "setup.py install"
+        # TODO: Should be user-configurable with config settings
         SAGE_ROOT = os.path.join(DOT_SAGE, f'sage-{sage_version}-{arch_tag}-conda')
 
         def ignore(path, names):
@@ -35,38 +71,20 @@ class build_py(setuptools_build_py):
                 return ['src']
             ### ignore more stuff --- .tox etc.
             return [name for name in names
-                    if name in ('.tox',)]
+                    if name in ('.tox', '.git', '__pycache__')]
 
-        if os.path.exists(os.path.join(SAGE_ROOT, 'config.status')):
-            print(f'Reusing SAGE_ROOT={SAGE_ROOT}')
-        else:
+        if not os.path.exists(os.path.join(SAGE_ROOT, 'config.status')):
             # config.status and other configure output has to be writable.
             # So (until the Sage distribution supports VPATH builds - #21469), we have to make a copy of sage_root.
             try:
                 shutil.copytree('sage_root', SAGE_ROOT,
                                 ignore=ignore)  # will fail if already exists
             except Exception as e:
-                raise DistutilsSetupError(f"the directory SAGE_ROOT={SAGE_ROOT} already exists but it is not configured.  Please remove it and try again. (Exception: {e})")
-            cmd = f"cd {SAGE_ROOT} && ./configure --enable-build-as-root --with-system-python3=force --disable-notebook --disable-sagelib --disable-sage_conf --disable-doc"
-            cmd += ' --with-python=$CONDA_PREFIX/bin/python --prefix="$CONDA_PREFIX" --enable-system-site-packages'
-            cmd += ' $(for pkg in $(build/bin/sage-package list :standard: --exclude rpy2 --has-file spkg-configure.m4 --has-file distros/conda.txt); do echo --with-system-$pkg=force; done)'
-            print(f"Running {cmd}")
-            sys.stdout.flush()
-            if os.system(cmd) != 0:
-                print(f"configure failed; this may be caused by missing build prerequisites.")
-                sys.stdout.flush()
-                PREREQ_SPKG = "_prereq bzip2 xz libffi"  # includes python3 SPKG_DEPCHECK packages
-                os.system(f'cd {SAGE_ROOT} && export SYSTEM=$(build/bin/sage-guess-package-system 2>/dev/null) && export PACKAGES="$(build/bin/sage-get-system-packages $SYSTEM {PREREQ_SPKG})" && [ -n "$PACKAGES" ] && echo "You can install the required build prerequisites using the following shell command" && echo "" && build/bin/sage-print-system-package-command $SYSTEM --verbose --sudo install $PACKAGES && echo ""')
-                raise DistutilsSetupError("configure failed")
+                raise SetupError(f"the directory SAGE_ROOT={SAGE_ROOT} already exists but it is not configured ({e}). "
+                                 "Please either remove it and try again, or install in editable mode (pip install -e).")
 
-        # In this mode, we never run "make".
+        return SAGE_ROOT
 
-        # Install configuration
-        shutil.copyfile(os.path.join(SAGE_ROOT, 'pkgs', 'sage-conf', '_sage_conf', '_conf.py'),
-                        os.path.join(HERE, '_sage_conf', '_conf.py'))
-        shutil.copyfile(os.path.join(SAGE_ROOT, 'src', 'bin', 'sage-env-config'),
-                        os.path.join(HERE, 'bin', 'sage-env-config'))
-        setuptools_build_py.run(self)
 
 class build_scripts(distutils_build_scripts):
 
@@ -75,6 +93,7 @@ class build_scripts(distutils_build_scripts):
         if not self.distribution.entry_points:
             self.entry_points = self.distribution.entry_points = dict()
         distutils_build_scripts.run(self)
+
 
 setup(
     cmdclass=dict(build_py=build_py, build_scripts=build_scripts)
