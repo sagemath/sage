063896851ce74540bb0279baf6c58c92170c541a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-10-04 13:58:57 -0700

    src/sage/misc/cython.py: Suppress a warning caused by setuptools_scm 8

--- src/sage/misc/cython.py
+++ src/sage/misc/cython.py
@@ -415,6 +415,14 @@ def cython(filename, verbose=0, compile_message=False,
 
     # This emulates running "setup.py build" with the correct options
     dist = Distribution()
+    # setuptools plugins considered harmful:
+    # If build isolation is not in use and setuptools_scm is installed,
+    # then its file_finders entry point is invoked, which we don't need.
+    # And with setuptools_scm 8, we get more trouble:
+    # LookupError: pyproject.toml does not contain a tool.setuptools_scm section
+    # LookupError: setuptools-scm was unable to detect version ...
+    # We just remove all handling of "setuptools.finalize_distribution_options" entry points.
+    dist._removed = staticmethod(lambda ep: True)
     dist.ext_modules = [ext]
     dist.include_dirs = includes
     buildcmd = dist.get_command_obj("build")
