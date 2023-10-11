d90340e6634905beb1a1fee2136a9b9ac9463832
David Ayotte <34245930+DavidAyotte@users.noreply.github.com>
2023-10-11 09:54:52 -0400

    Update src/sage/rings/function_field/drinfeld_modules/charzero_drinfeld_module.py
    Co-authored-by: Xavier Caruso <xcaruso@users.noreply.github.com>

--- src/sage/rings/function_field/drinfeld_modules/charzero_drinfeld_module.py
+++ src/sage/rings/function_field/drinfeld_modules/charzero_drinfeld_module.py
@@ -368,9 +368,13 @@ class DrinfeldModule_charzero(DrinfeldModule):
         if n % q == 0:
             return self.goss_polynomial(n // q)**q
         # General case
-        pol = sum(self._compute_coefficient_exp(i+1)
-                  *self._compute_goss_polynomial(n - q**(i+1), q, poly_ring, X)
-                  for i in range(0, (n.log(q).n()).floor()))
+        pol = poly_ring.zero()
+        m = q
+        i = 1
+        while m < n:
+            pol += self._compute_coefficient_exp(i) * self._compute_goss_polynomial(n - m, q, poly_ring, X)
+            m *= q
+            i += 1
         return X*(self._compute_goss_polynomial(n - 1, q, poly_ring, X) + pol)
 
     def goss_polynomial(self, n, var='X'):
