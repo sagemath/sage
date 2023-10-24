26cca827eba76cdaeea195f1af7d0511839462ab
Tobias Diez <code@tobiasdiez.com>
2023-10-24 02:45:00 +0000

    Restore atexit

--- src/sage/cpython/atexit.pyx
+++ src/sage/cpython/atexit.pyx
@@ -148,15 +148,44 @@ from cpython.ref cimport PyObject
 # Implement "_atexit_callbacks()" for each supported python version
 cdef extern from *:
     """
+    #if PY_VERSION_HEX >= 0x030a0000
+    /********** Python 3.10 **********/
     #define Py_BUILD_CORE
     #undef _PyGC_FINALIZED
     #include "internal/pycore_interp.h"
     #include "internal/pycore_pystate.h"
+    #if PY_VERSION_HEX >= 0x030c0000
+    // struct atexit_callback was renamed in 3.12 to atexit_py_callback
+    #define atexit_callback atexit_py_callback
+    #endif
     static atexit_callback ** _atexit_callbacks(PyObject *self) {
         PyInterpreterState *interp = _PyInterpreterState_GET();
         struct atexit_state state = interp->atexit;
         return state.callbacks;
     }
+    #else
+    /********** Python < 3.10 **********/
+    /* Internal structures defined in the CPython source in
+     * Modules/atexitmodule.c and subject to (but unlikely to) change.  Watch
+     * https://bugs.python.org/issue32082 for a request to (eventually)
+     * re-expose more of the atexit module's internals to Python
+     * typedef struct
+     */
+    typedef struct {
+        PyObject *func;
+        PyObject *args;
+        PyObject *kwargs;
+    } atexit_callback;
+    typedef struct {
+        atexit_callback **atexit_callbacks;
+        int ncallbacks;
+        int callback_len;
+    } atexitmodule_state;
+    static atexit_callback ** _atexit_callbacks(PyObject *self) {
+        atexitmodule_state *state = PyModule_GetState(self);
+        return state->atexit_callbacks;
+    }
+    #endif
     """
     ctypedef struct atexit_callback:
         PyObject* func
