3ff66b8cd730e3d0192fce81efeb6ea1a6947534
Tobias Diez <code@tobiasdiez.com>
2023-10-28 01:48:28 +0000

    fix conf package

---
+++ pkgs/sage-conf_meson/sage_conf/__init__.py
@@ -0,0 +1,4 @@
+from .builddir._conf import *
+from .builddir.build_info import CONDA_PREFIX
+
+SAGE_LOCAL = CONDA_PREFIX
--- pkgs/sage-conf_meson/setup.py
+++ pkgs/sage-conf_meson/setup.py
@@ -1,13 +1,12 @@
 import os
 import sys
-from distutils.command.build_scripts import \
-    build_scripts as distutils_build_scripts
 from pathlib import Path
 
 from setuptools import setup
 from setuptools.command.build_py import build_py as setuptools_build_py
-from setuptools.command.editable_wheel import \
-    editable_wheel as setuptools_editable_wheel
+from setuptools.command.editable_wheel import (
+    editable_wheel as setuptools_editable_wheel,
+)
 from setuptools.errors import SetupError
 
 
@@ -26,7 +25,7 @@ class build_py(setuptools_build_py):
                 'See https://doc.sagemath.org/html/en/installation/conda.html on how to get started.'
             )
 
-        builddir = here / "builddir"
+        builddir = here / "sage_conf" / "builddir"
         cmd = f"cd {root} && meson setup {builddir} --wipe"
         print(f"Running {cmd}")
         sys.stdout.flush()
@@ -39,14 +38,6 @@ class build_py(setuptools_build_py):
             build_info.write(f'CONDA_PREFIX = "{conda_prefix}"\n')
 
 
-class build_scripts(distutils_build_scripts):
-    def run(self):
-        self.distribution.scripts.append(os.path.join('bin', 'sage-env-config'))
-        if not self.distribution.entry_points:
-            self.entry_points = self.distribution.entry_points = dict()
-        distutils_build_scripts.run(self)
-
-
 class editable_wheel(setuptools_editable_wheel):
     r"""
     Customized so that exceptions raised by our build_py
@@ -57,6 +48,6 @@ class editable_wheel(setuptools_editable_wheel):
 
 setup(
     cmdclass=dict(
-        build_py=build_py, build_scripts=build_scripts, editable_wheel=editable_wheel
+        build_py=build_py, editable_wheel=editable_wheel
     )
 )
