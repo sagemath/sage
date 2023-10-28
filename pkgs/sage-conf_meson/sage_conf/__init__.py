d9e0d49bf4798a1e1ac904cb988b2aaab02882f3
Tobias Diez <code@tobiasdiez.com>
2023-10-28 04:43:08 +0000

    move config stuff to meson file in pkgs

--- pkgs/sage-conf_meson/sage_conf/__init__.py
+++ pkgs/sage-conf_meson/sage_conf/__init__.py
@@ -1,4 +1,9 @@
-from .builddir._conf import *
+import importlib
+
 from .builddir.build_info import CONDA_PREFIX
+
+conf = importlib.import_module('.builddir.pkgs.sage-conf_meson._conf', 'sage_conf')
+conf_options = [x for x in conf.__dict__ if not x.startswith("_")]
+globals().update({k: getattr(conf, k) for k in conf_options})
 
 SAGE_LOCAL = CONDA_PREFIX
