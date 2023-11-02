3c9c4c9812d86ccdbff782cd980a4daa10379236
Tobias Diez <code@tobiasdiez.com>
2023-11-02 10:56:47 +0000

    Allow to specify output directory for generated interpreters

--- src/sage_setup/autogen/__init__.py
+++ src/sage_setup/autogen/__init__.py
@@ -1,6 +1,7 @@
 import os
+
 from . import interpreters
-from sage.env import SAGE_SRC
+
 
 def autogen_all():
     """
@@ -9,6 +10,7 @@ def autogen_all():
     Return a list of sub-packages that should be appended to the list
     of packages built/installed by setup.py.
     """
+    from sage.env import SAGE_SRC
     interpreters.rebuild(os.path.join(SAGE_SRC, "sage", "ext", "interpreters"))
 
     return ['sage.ext.interpreters']
--- src/sage_setup/autogen/interpreters/__main__.py
+++ src/sage_setup/autogen/interpreters/__main__.py
@@ -1,9 +1,18 @@
-from __future__ import print_function, absolute_import
+from __future__ import absolute_import, print_function
 
+import argparse
 import os
-from sage.env import SAGE_SRC
 
 from . import rebuild
 
+parser = argparse.ArgumentParser()
+parser.add_argument("output_dir")
+args = parser.parse_args()
 
-rebuild(os.path.join(SAGE_SRC, "sage", "ext", "interpreters"))
+output_dir = args.output_dir
+if not output_dir:
+    from sage.env import SAGE_SRC
+    output_dir = os.path.join(SAGE_SRC, "sage", "ext", "interpreters")
+
+
+rebuild(output_dir)
