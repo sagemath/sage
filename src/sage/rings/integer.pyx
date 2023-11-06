3453deaec6049dfb508673760d68a6ed21d3cbc7
Frédéric Chapoton <fchapoton2@gmail.com>
2023-11-06 17:55:59 +0100

    Update src/sage/rings/integer.pyx
    Co-authored-by: Matthias Köppe <mkoeppe@math.ucdavis.edu>

--- src/sage/rings/integer.pyx
+++ src/sage/rings/integer.pyx
@@ -2870,7 +2870,7 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
                 return elog
 
         from sage.functions.log import function_log
-        return function_log(self,dont_call_method_on_arg=True)/\
+        return function_log(self, dont_call_method_on_arg=True)/\
             function_log(m, dont_call_method_on_arg=True)
 
     def exp(self, prec=None):
