c791f781a8364e025d3d6af2d50b034868b7571f
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-11-08 12:12:23 -0800

    Restores

--- src/sage/misc/all.py
+++ src/sage/misc/all.py
@@ -3,6 +3,16 @@ from sage.misc.all__sagemath_environment import *
 from sage.misc.all__sagemath_modules import *
 from sage.misc.all__sagemath_repl import *
 
+from sage.misc.misc import (BackslashOperator,
+                            exists, forall, is_iterator,
+                            random_sublist,
+                            pad_zeros,
+                            SAGE_DB,
+                            newton_method_sizes, compose,
+                            nest)
+lazy_import('sage.misc.misc', 'union',
+            deprecation=32096)
+
 from sage.misc.remote_file import get_remote_file
 
 lazy_import('sage.misc.dist', 'install_scripts', deprecation=34259)
