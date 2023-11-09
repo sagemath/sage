c2000d80518f14e7ebdf18ee52fb98a9f844e13a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-11-09 13:09:16 -0800

    src/sage/misc/sageinspect.py: Update doctests for increased use of lazy imports

--- src/sage/misc/sageinspect.py
+++ src/sage/misc/sageinspect.py
@@ -1361,6 +1361,7 @@ def sage_getfile(obj):
         sage: from sage.misc.sageinspect import sage_getfile
         sage: sage_getfile(sage.rings.rational)
         '...sage/rings/rational.pyx'
+        sage: from sage.algebras.steenrod.steenrod_algebra import Sq                    # needs sage.combinat sage.modules
         sage: sage_getfile(Sq)                                                          # needs sage.combinat sage.modules
         '...sage/algebras/steenrod/steenrod_algebra.py'
         sage: sage_getfile(x)                                                           # needs sage.symbolic
@@ -1441,6 +1442,7 @@ def sage_getfile_relative(obj):
         sage: from sage.misc.sageinspect import sage_getfile_relative
         sage: sage_getfile_relative(sage.rings.rational)
         'sage/rings/rational.pyx'
+        sage: from sage.algebras.steenrod.steenrod_algebra import Sq                    # needs sage.combinat sage.modules
         sage: sage_getfile_relative(Sq)                                                 # needs sage.combinat sage.modules
         'sage/algebras/steenrod/steenrod_algebra.py'
         sage: sage_getfile_relative(x)                                                  # needs sage.symbolic
@@ -2345,6 +2347,7 @@ def sage_getsourcelines(obj):
     EXAMPLES::
 
         sage: from sage.misc.sageinspect import sage_getsourcelines
+        sage: from sage.matrix.constructor import matrix                                # needs sage.modules
         sage: sage_getsourcelines(matrix)[1]                                            # needs sage.modules
         21
         sage: sage_getsourcelines(matrix)[0][0]                                         # needs sage.modules
