b6310d9eb30b11d8694344212563164a36749ec8
Tobias Diez <code@tobiasdiez.com>
2023-11-16 10:32:40 +0800

    Fix codestyle

--- src/sage/misc/__init__.py
+++ src/sage/misc/__init__.py
@@ -1,4 +1,4 @@
-try: 
+try:
     import sage.structure.element  # to break a cyclic import (misc.constant_function > structure.element > ... > misc.constant_function)
 except ModuleNotFoundError:
     # Ignore, the import above may fail while collecting metadata info (at which point sage.structure is not yet installed)
