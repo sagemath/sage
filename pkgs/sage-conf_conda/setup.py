5af6f07ea2061870ad021e20bff06cae4628859e
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-11-25 15:03:53 -0800

    pkgs/sage-conf_conda/setup.py: Do not use force for packages that may be eliminated by the bootstrap-conda environment minimizer

--- pkgs/sage-conf_conda/setup.py
+++ pkgs/sage-conf_conda/setup.py
@@ -30,7 +30,7 @@ class build_py(setuptools_build_py):
 
         cmd = f"cd {SAGE_ROOT} && ./configure --enable-build-as-root --with-system-python3=force --disable-notebook --disable-sagelib --disable-sage_conf --disable-doc"
         cmd += ' --with-python=$CONDA_PREFIX/bin/python --prefix="$CONDA_PREFIX"'
-        cmd += ' $(for pkg in $(PATH="build/bin:$PATH" build/bin/sage-package list :standard: --exclude rpy2 --has-file spkg-configure.m4 --has-file distros/conda.txt); do echo --with-system-$pkg=force; done)'
+        cmd += ' $(for pkg in $(PATH="build/bin:$PATH" build/bin/sage-package list :standard: --exclude rpy2 --has-file spkg-configure.m4 --has-file distros/conda.txt --exclude-dependencies); do echo --with-system-$pkg=force; done)'
         print(f"Running {cmd}")
         sys.stdout.flush()
         if os.system(cmd) != 0:
