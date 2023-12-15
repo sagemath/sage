26ffdaaa5ad01da3b5156d3d7e50cb77562ce24e
Kwankyu Lee <ekwankyu@gmail.com>
2023-12-15 15:14:05 +0900

    Treat the case when file is removed

--- .ci/create-changes-html.sh
+++ .ci/create-changes-html.sh
@@ -64,8 +64,11 @@ for block in diff_blocks:
         doc = match.group(1)
         path = 'html/' + doc
         file_path = os.path.join('$DOC_REPOSITORY', doc)
-        with open(file_path, 'r') as file:
-            content = file.readlines()
+        try:
+            with open(file_path, 'r') as file:
+                content = file.readlines()
+        except FileNotFoundError:
+            content = []
         count = 0
         for line in block.splitlines():
             if line.startswith('@@ -'):
@@ -77,8 +80,9 @@ for block in diff_blocks:
                             count += 1
                             content[i] = f'<span id="hunk{count}" style="visibility: hidden;"></span>' + content[i]
                             break
-        with open(file_path, 'w') as file:
-            file.writelines(content)
+        if content:
+            with open(file_path, 'w') as file:
+                file.writelines(content)
         hunks = '&nbsp;'.join(f'<a href="{path}#hunk{i+1}" class="hunk" target="_blank">#{i + 1}</a>' for i in range(count))
         out_blocks.append(f'<p class="diff"><a href="{path}">{doc}</a>&nbsp;' + hunks + '&emsp;</p>'
                             + '\n<pre><code class="language-diff">'
