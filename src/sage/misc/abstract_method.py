126a6009f4da6f0bd33cf6e1dd8b232e4bda0dc7
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2023-12-26 07:29:14 -0800

    Update output of self-referential doctests

--- src/sage/misc/abstract_method.py
+++ src/sage/misc/abstract_method.py
@@ -193,7 +193,7 @@ class AbstractMethod():
             sage: src[0]
             'def version():\n'
             sage: lines
-            18
+            19
         """
         from sage.misc.sageinspect import sage_getsourcelines
         return sage_getsourcelines(self._f)
