d67b011836609cd661337f321542e1a4781cdb8a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-14 13:50:53 -0800

    .devcontainer/onCreate-conda.sh: Add comments

--- .devcontainer/onCreate-conda.sh
+++ .devcontainer/onCreate-conda.sh
@@ -4,8 +4,17 @@ set -e
 set -x
 
 # Create conda environment
+# A space bottleneck occurs at build time of the container
+# when the smallest (2-core, 32 GB) configuration is used.
 mkdir /tmp/conda-pkgs && mkdir -p /opt/conda && ln -s /tmp/conda-pkgs /opt/conda/pkgs
 conda install --quiet mamba -n base -c conda-forge -y
+# The following line shows the available space.
+# At the time of writing:
+# Filesystem      Size  Used Avail Use% Mounted on
+# overlay          32G   23G  7.5G  76% /
+# /dev/sda1        44G   52K   42G   1% /tmp
+# The 7.5G (also shared with /workspaces) are not enough for our
+# conda packages, which is why we redirect them into /tmp
 df -h / /tmp
 mamba env create --file src/environment-dev-3.11-linux.yml || mamba env update --file src/environment-dev-3.11-linux.yml
 conda init bash
