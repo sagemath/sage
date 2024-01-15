9ffd3328fbea68180998bba5412cd58324fe8bef
Peter Bruin <P.J.Bruin@math.leidenuniv.nl>
2024-01-15 15:19:23 +0100

    suppress spurious warning in QuotientFunctor

--- src/sage/rings/finite_rings/residue_field_ntl_gf2e.pyx
+++ src/sage/rings/finite_rings/residue_field_ntl_gf2e.pyx
@@ -45,7 +45,6 @@ class ResidueFiniteField_ntl_gf2e(ResidueField_generic, FiniteField_ntl_gf2e):
         sage: c = OK(a)
         sage: b = k(c)
         sage: b*c^2
-        doctest:warning ...
         7
         sage: b*c
         13*abar + 5
