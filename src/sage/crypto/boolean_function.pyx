9a955cd8ac9c3f3aed096734fdd6213d5ef0c8c1
RuchitJagodara <ruchit.jagodara@iitgn.ac.in>
2024-01-29 00:39:52 +0530

    Corrected Correlation Immunity of Boolean Functions

--- src/sage/crypto/boolean_function.pyx
+++ src/sage/crypto/boolean_function.pyx
@@ -852,7 +852,7 @@ cdef class BooleanFunction(SageObject):
         if self._correlation_immunity is None:
             c = self._nvariables
             W = self.walsh_hadamard_transform()
-            for i in range(len(W)):
+            for i in range(1, len(W)):
                 sig_check()
                 if W[i]:
                     c = min(c, hamming_weight(i))
