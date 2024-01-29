9f68b47429b169260da887f2eba3371f04de8328
adrinospy <amanmoon099@gmail.com>
2024-01-29 21:25:52 +0530

    made ordering of coefficient in coefficient_matrix decreasing for univariate polynomial ring

--- src/sage/rings/polynomial/toy_variety.py
+++ src/sage/rings/polynomial/toy_variety.py
@@ -120,7 +120,10 @@ def coefficient_matrix(polys):
     M = matrix(R, len(polys), len(mons))
     for i in range(len(polys)):
         imons = polys[i].monomials()
-        icoeffs = polys[i].coefficients()
+        if polys[0].parent().ngens() == 1:
+            icoeffs = polys[i].coefficients()[::-1]
+        else:
+            icoeffs = polys[i].coefficients()
         for j in range(len(imons)):
             M[i, mons.index(imons[j])] = icoeffs[j]
     return M
