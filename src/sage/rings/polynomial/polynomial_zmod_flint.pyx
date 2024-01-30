5671eab3b9eba1bcd28799b7c0151033b4a08663
Vincent Neiger <vneiger@users.noreply.github.com>
2024-01-30 23:20:59 +0100

    fix in other similar places

--- src/sage/rings/polynomial/polynomial_zmod_flint.pyx
+++ src/sage/rings/polynomial/polynomial_zmod_flint.pyx
@@ -815,9 +815,12 @@ cdef class Polynomial_zmod_flint(Polynomial_template):
         cdef Polynomial_zmod_flint res = self._new()
         cdef unsigned long d
         if degree is not None:
-            d = degree
-            if d != degree:
-                raise ValueError("degree argument must be a non-negative integer, got %s"%(degree))
+            if degree <= 0:
+                raise ValueError("degree argument must be a non-negative integer, got %s" % (degree))
+            try:
+                d = degree
+            except ValueError:
+                raise ValueError("degree argument must be a non-negative inte ger, got %s" % (degree))
             nmod_poly_reverse(&res.x, &self.x, d+1) # FLINT expects length
         else:
             nmod_poly_reverse(&res.x, &self.x, nmod_poly_length(&self.x))
