ded09755a1faaee4bcdd0567df0bcb86b34ed127
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-30 13:53:36 -0800

    Translate A Tour Of Sage to Persian

---
+++ src/doc/fa/a_tour_of_sage/conf.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Numerical Sage documentation build configuration file, created by
+# sphinx-quickstart on Sat Dec  6 11:08:04 2008.
+#
+# This file is execfile()d with the current directory set to its containing dir.
+#
+# The contents of this file are pickled, so don't put values in the namespace
+# that aren't pickleable (module imports are okay, they're removed automatically).
+#
+# All configuration values have a default; values that are commented out
+# serve to show the default.
+
+from sage.docs.conf import release
+from sage.docs.conf import *  # NOQA
+
+# General information about the project.
+project = u'تور Sage'
+name = 'a_tour_of_sage'
+language = 'fa'
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = project + " v"+release
+html_short_title = u"تور Sage v" + release
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = name
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, document class [howto/manual]).
+latex_documents = [
+  ('index', name+'.tex', u'تور Sage',
+   u'The Sage Development Team', 'manual'),
+]
+
