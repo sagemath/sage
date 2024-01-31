224faea914cd4d49c7cd05568d72c8b38bb88815
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-01-30 16:58:49 -0800

    src/sage/misc/timing.py (cputime): If module 'resource' is not available, fall back to walltime

--- src/sage/misc/timing.py
+++ src/sage/misc/timing.py
@@ -72,11 +72,16 @@ def cputime(t=0, subprocesses=False):
         CPU time is reported correctly because subprocesses can be
         started and terminated at any given time.
     """
+    try:
+        import resource
+    except ImportError:
+        # The module 'resource' is removed in Pyodide to browser limitations.
+        return walltime(t)
+
     if isinstance(t, GlobalCputime):
         subprocesses = True
 
     if not subprocesses:
-        import resource
         try:
             t = float(t)
         except TypeError:
