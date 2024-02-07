6617f7963fcaf0572abaf184ee7e41604a2f2f05
Frédéric Chapoton <chapoton@unistra.fr>
2024-02-07 11:32:37 +0100

    tentative fixes

--- src/sage/rings/function_field/order_polymod.py
+++ src/sage/rings/function_field/order_polymod.py
@@ -17,6 +17,7 @@ Orders of function fields: extension
 # ****************************************************************************
 
 from sage.arith.functions import lcm
+from sage.categories.algebras import Algebras
 from sage.misc.cachefunc import cached_method
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 
@@ -648,9 +649,9 @@ class FunctionFieldMaximalOrder_polymod(FunctionFieldMaximalOrder):
         # Let O denote the maximal order self. When reduced modulo p,
         # matrices_reduced give the multiplication matrices used to form the
         # algebra O mod pO.
-        matrices_reduced = list(map(lambda M: M.mod(p), matrices))
-        A = FiniteDimensionalAlgebra(k, matrices_reduced,
-                                     assume_associative=True)
+        matrices_reduced = [M.mod(p) for M in matrices]
+        cat = Algebras(k).FiniteDimensional().WithBasis()
+        A = FiniteDimensionalAlgebra(k, matrices_reduced, category=cat)
 
         # Each prime ideal of the algebra A corresponds to a prime ideal of O,
         # and since the algebra is an Artinian ring, all of its prime ideals
