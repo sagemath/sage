848fbabf073df72635f9d4ebcb7928e63089efc3
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-02-07 23:19:23 -0800

    sage.feature: Use spkg='pypi:DISTRIBUTION-NAME'

--- src/sage/features/polymake.py
+++ src/sage/features/polymake.py
@@ -35,7 +35,7 @@ class JuPyMake(JoinFeature):
             True
         """
         JoinFeature.__init__(self, "jupymake",
-                             [PythonModule("JuPyMake", spkg="jupymake")])
+                             [PythonModule("JuPyMake", spkg="pypi:jupymake")])
 
 
 def all_features():
