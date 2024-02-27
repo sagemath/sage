88fb325002c112284652b39064deb06a44b440eb
Saatvik Rao <saatvikrao@Saatviks-MacBook-Air.local>
2024-02-27 12:09:50 +0530

    exposed acyclic orientations to graph

--- src/sage/graphs/graph.py
+++ src/sage/graphs/graph.py
@@ -10134,7 +10134,7 @@ class Graph(GenericGraph):
     from sage.graphs.tutte_polynomial import tutte_polynomial
     from sage.graphs.lovasz_theta import lovasz_theta
     from sage.graphs.partial_cube import is_partial_cube
-    from sage.graphs.orientations import strong_orientations_iterator, random_orientation
+    from sage.graphs.orientations import strong_orientations_iterator, random_orientation, acyclic_orientations
     from sage.graphs.connectivity import bridges, cleave, spqr_tree
     from sage.graphs.connectivity import is_triconnected
     from sage.graphs.comparability import is_comparability
