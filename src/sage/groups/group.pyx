53d71685ff62f2ad18175454148d4456a4aa2004
RuchitJagodara <ruchit.jagodara@iitgn.ac.in>
2024-02-27 12:46:05 +0530

    Fixed a linting error in group.pyx

--- src/sage/groups/group.pyx
+++ src/sage/groups/group.pyx
@@ -274,7 +274,7 @@ cdef class Group(Parent):
 
             sage: sage: G = PermutationGroup([(1,2,3), (2,3), (4,5)])
             sage: sage: G.minimum_generating_set()
-            {[1, 3, 2], [3, 1, 2, 5, 4]}            
+            {[1, 3, 2], [3, 1, 2, 5, 4]}
         """
         from sage.groups.libgap_mixin import minimum_generating_set
         return minimum_generating_set(self, gap_based=gap_based)
