30306ec715c458304b65aca269464fe154325b60
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-03-02 19:51:40 -0800

    BruhatTitsHarmonicCocycleElement.monomial_coefficients: Remove pickling workaround, delegate to .element()

--- src/sage/modular/btquotients/pautomorphicform.py
+++ src/sage/modular/btquotients/pautomorphicform.py
@@ -200,9 +200,6 @@ class BruhatTitsHarmonicCocycleElement(HeckeModuleElement):
             sage: H = X.harmonic_cocycles(2,prec=10)
             sage: v = H.basis()[0] # indirect doctest
             sage: TestSuite(v).run()
-            Traceback (most recent call last):
-            ...
-            The following tests failed: _test_monomial_coefficients
         """
         HeckeModuleElement.__init__(self, _parent, None)
         self._parent = _parent
@@ -323,17 +320,19 @@ class BruhatTitsHarmonicCocycleElement(HeckeModuleElement):
         """
         return 'Harmonic cocycle with values in %s' % self.parent()._U
 
-    def monomial_coefficients(self):
+    def monomial_coefficients(self, copy=True):
         r"""
-        Void method to comply with pickling.
+        Return a dictionary whose keys are indices of basis elements
+        in the support of ``self`` and whose values are the
+        corresponding coefficients.
 
         EXAMPLES::
 
-            sage: M = BruhatTitsQuotient(3,5).harmonic_cocycles(2,prec=10)
+            sage: M = BruhatTitsQuotient(3,5).harmonic_cocycles(2, prec=10)
             sage: M.monomial_coefficients()
             {}
         """
-        return {}
+        return self.element().monomial_coefficients(copy=copy)
 
     def print_values(self):
         r"""
@@ -718,9 +717,6 @@ class BruhatTitsHarmonicCocycles(AmbientHeckeModule, UniqueRepresentation):
             sage: X = BruhatTitsQuotient(3,37)
             sage: H = X.harmonic_cocycles(4,prec=10)
             sage: TestSuite(H).run()
-            Traceback (most recent call last):
-            ...
-            The following tests failed: _test_elements
         """
         self._k = k
         self._X = X
