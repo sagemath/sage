c5773fd7d3c0d8ea13b23c60e3735d3a8fa9345e
Enrique Artal <artal@unizar.es>
2024-03-13 16:33:01 +0100

    minor change in one test

--- src/sage/groups/finitely_presented.py
+++ src/sage/groups/finitely_presented.py
@@ -769,9 +769,8 @@ class FinitelyPresentedGroup(GroupMixinLibGAP, CachedRepresentation, Group, Pare
             sage: G = FreeGroup(2) / [2 * (1, 2, -1, -2)]
             sage: loads(dumps(G))
             Finitely presented group < x0, x1 | (x0*x1*x0^-1*x1^-1)^2 >
-            sage: G.__reduce__()[1]
-            (<class 'sage.groups.finitely_presented.FinitelyPresentedGroup'>,
-            (Free Group on generators {x0, x1}, ((x0*x1*x0^-1*x1^-1)^2,)),{})
+            sage: G.__reduce__()[1][1]
+            (Free Group on generators {x0, x1}, ((x0*x1*x0^-1*x1^-1)^2,))
 
             sage: TestSuite(H).run()
             sage: TestSuite(J).run()
