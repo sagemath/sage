f9ddc324b8dc9488d85ba7cbd05a671a0272f4b4
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-03-25 21:20:50 -0700

    src/sage/modular/quasimodform/ring.py: Fix pycodestyle warning

--- src/sage/modular/quasimodform/ring.py
+++ src/sage/modular/quasimodform/ring.py
@@ -822,6 +822,6 @@ class QuasiModularForms(Parent, UniqueRepresentation):
             basis += [f*E2_pow for f
                       in M.modular_forms_of_weight(weight - 2*j).basis()]
             E2_pow *= E2
-        if not weight%2:
+        if not weight % 2:
             basis.append(E2_pow)
         return basis
