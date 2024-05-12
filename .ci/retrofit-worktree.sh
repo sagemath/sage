fe470b554495bf224280e51ee777f28f5d252180
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-05-12 15:31:39 -0700

    .ci/retrofit-worktree.sh: Exit earlier if git is not present

--- .ci/retrofit-worktree.sh
+++ .ci/retrofit-worktree.sh
@@ -12,11 +12,13 @@ export GIT_AUTHOR_EMAIL="ci-sage@example.com"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 
+set -e
+
 # Set globally for other parts of the workflow
 git config --global user.name "$GIT_AUTHOR_NAME"
 git config --global user.email "$GIT_AUTHOR_EMAIL"
 
-set -ex
+set -x
 
 # If actions/checkout downloaded our source tree using the GitHub REST API
 # instead of with git (because do not have git installed in our image),
