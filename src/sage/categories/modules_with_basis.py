80cb440147a848a3adcdf1846ea09e62130312b5
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-05-21 22:19:54 -0700

    src/sage/categories/modules_with_basis.py (ModulesWithBasis.Subobjects): Add construction

--- src/sage/categories/modules_with_basis.py
+++ src/sage/categories/modules_with_basis.py
@@ -29,6 +29,7 @@ from sage.categories.fields import Fields
 from sage.categories.modules import Modules
 from sage.categories.poor_man_map import PoorManMap
 from sage.categories.map import Map
+from sage.categories.subobjects import SubobjectsCategory
 from sage.structure.element import Element, parent
 
 
@@ -2392,6 +2393,29 @@ class ModulesWithBasis(CategoryWithAxiom_over_base_ring):
                 2*B[3]
             """
             return self(self.domain().monomial(i))
+
+    class Subobjects(SubobjectsCategory):
+        """
+        A category for submodules of modules with basis.
+
+        EXAMPLES::
+
+            sage: Modules(ZZ).Subobjects()
+            Category of subobjects of sets
+            sage: Modules(ZZ).Subobjects().all_super_categories()
+            [Category of subobjects of sets,
+             Category of subquotients of sets,
+             Category of sets,
+             Category of sets with partial maps,
+             Category of objects]
+        """
+
+        class ParentMethods:
+
+            def construction(self):
+                from sage.categories.pushout import SubspaceFunctor
+                return (SubspaceFunctor(self.basis()),
+                        self.ambient())
 
     class CartesianProducts(CartesianProductsCategory):
         """
