1a7f300cf439319b4c46d3a77496b7ea5d16ebfa
Jing Guo <dev.guoj@gmail.com>
2024-05-23 11:26:21 +0000

    helper_functions.py: Complete tests and documentations

--- src/sage/graphs/homomorphisms/helper_functions.py
+++ src/sage/graphs/homomorphisms/helper_functions.py
@@ -1,19 +1,21 @@
-from sage.graphs.graph import Graph
-
-from collections import Counter
-
-
 ### General helper functions
 
 def get_node_index(node):
+    r"""
+    Return the index in the host graph of `node` in a (nice) tree decomposition
+    """
     return node[0]
 
 def get_node_content(node):
+    r"""
+    Return the content of `node` in a (nice) tree decomposition
+    """
     return node[1]
 
 def node_changes(labelled_TD):
     r"""
-    Record introduced and forgotten nodes in a directed labelled nice tree decomposition.
+    Record introduced and forgotten nodes in a directed labelled nice tree
+    decomposition.
 
     INPUT:
 
@@ -28,13 +30,13 @@ def node_changes(labelled_TD):
     EXAMPLES::
 
         sage: from sage.graphs.graph_decompositions.tree_decomposition import label_nice_tree_decomposition
-        sage: from sage.graphs.hom_count import node_changes
+        sage: from sage.graphs.homomorphisms.helper_functions import node_changes
         sage: bip_one_four = graphs.CompleteBipartiteGraph(1, 4)
         sage: nice_tree_decomp = bip_one_four.treewidth(certificate=True, nice=True)
         sage: root = sorted(nice_tree_decomp)[0]
         sage: dir_labelled_TD = label_nice_tree_decomposition(nice_tree_decomp, root, directed=True)
         sage: node_changes(dir_labelled_TD)
-        {1: 1, 2: 1, 3: 4, 5: 4, 6: 4, 7: 3, 8: 2, 9: 0, 10: 0, 11: 3, 12: 2}
+        {0: 0, 1: 1, 2: 1, 3: 4, 5: 4, 6: 4, 7: 3, 8: 2, 9: 0, 10: 0, 11: 3, 12: 2}
     """
     node_changes_dict = {}
 
@@ -43,12 +45,7 @@ def node_changes(labelled_TD):
 
         node_type = labelled_TD.get_vertex(node)
         match node_type:
-            case 'intro':
-                child_vertex_set = labelled_TD.neighbors_out(node)[0][1]
-                # Get one element from the one-element set
-                (extra_vertex,) = node_vertex_set.symmetric_difference(child_vertex_set)
-                node_changes_dict[node_index] = extra_vertex
-            case 'forget':
+            case 'intro' | 'forget':
                 child_vertex_set = labelled_TD.neighbors_out(node)[0][1]
                 # Get one element from the one-element set
                 (extra_vertex,) = node_vertex_set.symmetric_difference(child_vertex_set)
@@ -60,26 +57,16 @@ def is_valid_mapping(mapped_vtx, mapped_nbhrs, target_graph):
     r"""
     Check if the mapping is valid.
     """
+    from sage.graphs.graph import Graph
+
     if isinstance(target_graph, Graph):
         return all(target_graph.has_edge(mapped_vtx, vtx) for vtx in mapped_nbhrs)
     else:
-        # Assume that `target_graph` is the adjacency matrix
+        # Assume that `target_graph` is an adjacency matrix
         return all(target_graph[mapped_vtx, vtx] for vtx in mapped_nbhrs)
 
-def count_occurrences(lst):
-    r"""
-    Count the occurrences of each element in `lst`
-    """
-    return Counter(lst)
 
-def list_intersection(lst1, lst2):
-    r"""
-    Return the intersection of `lst1` and `lst2`
-    """
-    return list(set(lst1) & set(lst2))
-
-
-### For integer representation
+### For integer representation of mappings
 
 def extract_bag_vertex(mapping, index, graph_size):
     r"""
@@ -106,19 +93,3 @@ def remove_vertex_from_mapping(mapping, index, graph_size):
     right_digits = mapping % (graph_size ** index)
 
     return left_digits // graph_size + right_digits
-
-
-### For integer representation in colourful case
-
-def encode_clr_list(clr_list, base):
-    r"""
-    Converts a list of integers to an integer in base-k representation.
-    """
-    return sum(val * base**idx for idx, val in enumerate(clr_list))
-
-def decode_clr_int(num, base, nth):
-    r"""
-    Retrieve the nth element from the base-k representation.
-    """
-    num //= base ** nth
-    return num % base
