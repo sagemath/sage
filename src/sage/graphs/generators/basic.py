80cc1663373a6c0bd1ddad300f8467a559f95ef4
Omegaconstant <janmenjaya.panda.22@gmail.com>
2024-05-26 01:44:58 +0530

    added MoebiusLadderGraph

--- src/sage/graphs/generators/basic.py
+++ src/sage/graphs/generators/basic.py
@@ -1149,6 +1149,67 @@ def LadderGraph(n):
     return G
 
 
+def MoebiusLadderGraph(n):
+    r"""
+    Return a Möbius ladder graph with `2 * n` nodes.
+
+    A Möbius ladder graph is a ladder graph that is connected at the ends with
+    a single twist, i.e.: a ladder bent around so that top meets bottom with a
+    single twist. Alternatively, it can be described as a single cycle graph (of
+    order `2 * n`) with the addition of edges (called 'rungs') connecting the
+    antipodal pairs of vertices. See the :wikipedia:`Möbius_ladder` for more
+    information.
+
+    PLOTTING: Upon construction, the position dictionary is filled to override
+    the spring-layout algorithm. By convention, each Möbius ladder graph will be
+    displayed with the first (0) node at the top, with the rest following in a
+    counterclockwise manner.
+
+    INPUT:
+
+    - ``n`` -- a non negative integer; number of nodes is `2 * n`
+
+    EXAMPLES:
+
+    Construct and show a Moebius ladder graph with 26 nodes::
+
+        sage: g = graphs.MoebiusLadderGraph(13)
+        sage: g.show()                          # long time                             # needs sage.plot
+
+    Create several Moebius ladder graphs in a Sage graphics array::
+
+        sage: g = []
+        sage: j = []
+        sage: for i in range(9):
+        ....:    k = graphs.MoebiusLadderGraph(i+3)
+        ....:    g.append(k)
+        sage: for i in range(3):                                                        # needs sage.plot
+        ....:    n = []
+        ....:    for m in range(3):
+        ....:        n.append(g[3*i + m].plot(vertex_size=50, vertex_labels=False))
+        ....:    j.append(n)
+        sage: G = graphics_array(j)                                                     # needs sage.plot
+        sage: G.show()                          # long time                             # needs sage.plot
+
+    TESTS:
+
+    The input parameter must be a non negative integer::
+
+        sage: G = graphs.MoebiusLadderGraph(-1)
+        Traceback (most recent call last):
+        ...
+        ValueError: parameter n must be a non negative integer
+    """
+    if n < 0:
+        raise ValueError("parameter n must be a non negative integer")
+
+    G = Graph(2 * n, name="Moebius Ladder graph")
+    G._circle_embedding(list(range(2 * n)), angle=pi/2)
+    G.add_cycle(list(range(2 * n)))
+    G.add_edges((i, i + n) for i in range(n))
+    return G
+
+
 def PathGraph(n, pos=None):
     r"""
     Return a path graph with `n` nodes.
