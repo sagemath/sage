5eb8d68170a87829d16b613857cacac375177b69
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-05-27 21:56:57 -0700

    for a in "CLASS='Morphism';SUFFIX=" ; do eval "$a"; git --no-pager grep -l -E 'is_('$CLASS')' src/sage | xargs sed -E -i.bak 's/import is_('$CLASS')/import \1'$SUFFIX'/;/^ +/s/is_('$CLASS')[(]([^()]*([(][^()]*[)])?[^()]*)[)]/isinstance(\2, \1'$SUFFIX')/g'; done

--- src/sage/modules/fg_pid/fgp_morphism.py
+++ src/sage/modules/fg_pid/fgp_morphism.py
@@ -101,7 +101,7 @@ class FGP_Morphism(Morphism):
         # input: phi is a morphism from MO = M.optimized().V() to N.V()
         # that sends MO.W() to N.W()
         if check:
-            if not is_Morphism(phi) and M == N:
+            if not isinstance(phi, Morphism) and M == N:
                 A = M.optimized()[0].V()
                 B = N.V()
                 s = M.base_ring()(phi) * B.coordinate_module(A).basis_matrix()
