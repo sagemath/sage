5b15a7d841b1b896951180e019d946c55ef53c6a
Frédéric Chapoton <chapoton@unistra.fr>
2024-06-02 11:03:32 +0200

    fix

--- src/sage/typeset/character_art.py
+++ src/sage/typeset/character_art.py
@@ -690,8 +690,8 @@ class CharacterArt(SageObject):
 
         if self._baseline is not None and Nelt._baseline is not None:
             # left treatement
-            new_matrix.extend(line + " " * (self._l - len(line))
-                              for line in self._matrix)
+            for line in self._matrix:
+                new_matrix.append(line + " " * (self._l - len(line)))
 
             if new_h > self._h:
                 # |                 new_h > self._h
@@ -700,9 +700,8 @@ class CharacterArt(SageObject):
                 #  | }               :: Nelt._baseline - self._baseline
                 #  | }
                 if new_baseline > self._baseline:
-                    new_matrix.append(" " * self._l
-                        for k in range(new_baseline - self._baseline))
-
+                    for k in range(new_baseline - self._baseline):
+                        new_matrix.append(" " * self._l)
                 #  | }              new_h > self._h
                 #  | }              new_h - new_baseline > self._h - self._baseline
                 # ||<-- baseline    number of white lines at the top
@@ -728,8 +727,8 @@ class CharacterArt(SageObject):
             for j in range(Nelt._h):
                 new_matrix[i + j] += Nelt._matrix[j]
         else:
-            new_matrix.extend(line + " " * (self._l - len(line))
-                              for line in self._matrix)
+            for line in self._matrix:
+                new_matrix.append(line + " " * (self._l - len(line)))
             for i, line_i in enumerate(Nelt._matrix):
                 if i == len(new_matrix):
                     new_matrix.append(" " * self._l + line_i)
