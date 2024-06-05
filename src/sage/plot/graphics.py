9f3627e71c0820dafad9026729591370b93415ca
Shriya M <25shriya@gmail.com>
2024-06-05 12:44:28 +0100

    Solved second set of linting errors

--- src/sage/plot/graphics.py
+++ src/sage/plot/graphics.py
@@ -310,7 +310,7 @@ class Graphics(WithEqualityById, SageObject):
             False
             sage: P.legend(True)
             sage: P  # show with the legend
-            Graphics object consisting of 1 graphics primitive    
+            Graphics object consisting of 1 graphics primitive   
         """
         if show is None:
             return self._show_legend
@@ -430,10 +430,10 @@ class Graphics(WithEqualityById, SageObject):
 
             sage: p.set_legend_options(loc=(0.5,0.5)); p  # aligns the bottom of the box to the center                # needs sage.symbolic
             Graphics object consisting of 1 graphics primitive
-    
+
         The parameters ``loc`` and ``borderaxespad`` can be altered in order to place the legend below the x-axis label or to the left of the y-axis label.
         The following example sets ``loc=3`` and ``borderaxespad = -7.5-0.01*p.fontsize()`` so as to obtain the legend below the x-axis label.
-        
+
             sage: p = line([(0, 0), (1, 1)], legend_label='test')
             sage: p.axes_labels(['X-Label', 'Y-Label'])                                  # adding labels for axes
             sage: p.set_legend_options(loc=8, borderaxespad=-7.5-0.01*p.fontsize())
