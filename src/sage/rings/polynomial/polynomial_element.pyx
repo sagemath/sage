65946f4db6d0bdcd292173592aad6df67c045b84
Matthias Köppe <mkoeppe@math.ucdavis.edu>
2024-06-06 11:02:09 -0700

    src/sage/rings/polynomial/polynomial_element.pyx: Remove unused import
    Co-authored-by: gmou3 <32706872+gmou3@users.noreply.github.com>

--- src/sage/rings/polynomial/polynomial_element.pyx
+++ src/sage/rings/polynomial/polynomial_element.pyx
@@ -107,7 +107,7 @@ from sage.rings.rational_field import QQ, RationalField
 from sage.rings.integer_ring import ZZ, IntegerRing_class
 from sage.rings.integer cimport Integer, smallInteger
 from sage.libs.gmp.mpz cimport *
-from sage.rings.fraction_field import FractionField, is_FractionField
+from sage.rings.fraction_field import FractionField
 from sage.rings.padics.padic_generic import pAdicGeneric
 
 from sage.structure.category_object cimport normalize_names
