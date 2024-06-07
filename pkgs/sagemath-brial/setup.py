2f3ac967814577103e1d03642154b6e59c56fb3e
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-06-07 14:16:13 -0700

    pkgs/sagemath-brial/setup.py: Set up sys.path for now

--- pkgs/sagemath-brial/setup.py
+++ pkgs/sagemath-brial/setup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# PEP 517 builds do not have . in sys.path
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
 from sage_setup import sage_setup
 
 sage_setup(['sagemath-brial'],
