3cc461fd162c8535eb225cde36438151ae5c97bf
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-06-10 19:05:37 -0700

    Update 'all' files

--- src/sage/rings/padics/all.py
+++ src/sage/rings/padics/all.py
@@ -1,6 +1,7 @@
+from sage.rings.padics.all__sagemath_categories import *
+
 from sage.rings.padics.factory import Zp, Zq, Zp as pAdicRing, ZpCR, ZpCA, ZpFM, ZpFP, ZpLC, ZpLF, ZqCR, ZqCA, ZqFM, ZqFP, ZpER
 from sage.rings.padics.factory import Qp, Qq, Qp as pAdicField, QpCR, QpFP, QpLC, QpLF, QqCR, QqFP, QpER
 from sage.rings.padics.factory import pAdicExtension
-from sage.rings.padics.padic_generic import local_print_mode
 from sage.rings.padics.pow_computer import PowComputer
 from sage.rings.padics.pow_computer_ext import PowComputer_ext_maker
