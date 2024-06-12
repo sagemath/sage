73cf5823bc27008c5163b8d4f7d5d41293ef1b6d
Verrel Rievaldo Wijaya <verrelrievaldo@gmail.com>
2024-06-12 23:39:30 +0700

    Modify `PolynomialRing` to allows construction of tropical polynomials

--- src/sage/rings/polynomial/polynomial_ring_constructor.py
+++ src/sage/rings/polynomial/polynomial_ring_constructor.py
@@ -35,6 +35,8 @@ except ImportError:
 import sage.rings.abc
 from sage.rings.integer import Integer
 from sage.rings.finite_rings.finite_field_base import FiniteField
+from sage.rings.semirings.tropical_semiring import TropicalSemiring
+from sage.rings.semirings.tropical_polynomial import TropicalPolynomialSemiring
 
 from sage.misc.cachefunc import weak_cached_function
 import sage.misc.weak_dict
@@ -622,7 +624,10 @@ def PolynomialRing(base_ring, *args, **kwds):
         sage: R.<x,y> = PolynomialRing(RIF,2)
         sage: TestSuite(R).run(skip=['_test_elements', '_test_elements_eq_transitive'])
     """
-    if base_ring not in Rings():
+    is_tropical = False
+    if isinstance(base_ring, TropicalSemiring):
+        is_tropical = True
+    elif base_ring not in Rings():
         raise TypeError("base_ring {!r} must be a ring".format(base_ring))
 
     n = -1  # Unknown number of variables
@@ -716,9 +721,15 @@ def PolynomialRing(base_ring, *args, **kwds):
             raise TypeError("variable names specified twice inconsistently: %r and %r" % (names, kwnames))
 
     if multivariate or len(names) != 1:
-        return _multi_variate(base_ring, names, **kwds)
+        if is_tropical:
+            pass # will add MTropicalPolynomialSemiring
+        else:
+            return _multi_variate(base_ring, names, **kwds)
     else:
-        return _single_variate(base_ring, names, **kwds)
+        if is_tropical:
+            return TropicalPolynomialSemiring(base_ring, names)
+        else:
+            return _single_variate(base_ring, names, **kwds)
 
 
 def unpickle_PolynomialRing(base_ring, arg1=None, arg2=None, sparse=False):
