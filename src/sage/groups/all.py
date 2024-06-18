ab610a3195d90c11bb101532290d2049c79ffc96
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-06-18 16:01:30 -0700

    src/sage/groups/all.py: Deprecate import of GroupExp_Class, GroupExpElement

--- src/sage/groups/all.py
+++ src/sage/groups/all.py
@@ -32,6 +32,7 @@ lazy_import('sage.groups', 'groups_catalog', 'groups')
 
 lazy_import('sage.groups.semimonomial_transformations.semimonomial_transformation_group', 'SemimonomialTransformationGroup')
 
-lazy_import('sage.groups.group_exp', ['GroupExp', 'GroupExp_Class', 'GroupExpElement'])
+lazy_import('sage.groups.group_exp', ['GroupExp'])
+lazy_import('sage.groups.group_exp', ['GroupExp_Class', 'GroupExpElement'], deprecation=99999)
 
 lazy_import('sage.groups.group_semidirect_product', ['GroupSemidirectProduct', 'GroupSemidirectProductElement'])
