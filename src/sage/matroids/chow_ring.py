462ce6c0c25d916a85dba2e54963c4bb0dc30ca6
Shriya M <25shriya@gmail.com>
2024-06-23 14:47:36 +0530

    Created Chow Ring class

---
+++ src/sage/matroids/chow_ring.py
@@ -0,0 +1,18 @@
+from sage.matroids.chow_ring_ideal import *
+from sage.rings.quotient_ring import QuotientRing_nc
+from sage.rings.quotient_ring_element import QuotientRingElement
+from sage.categories.graded_algebras_with_basis import GradedAlgebrasWithBasis
+import sage.misc.latex as latex
+
+class ChowRing(QuotientRing_nc, category=GradedAlgebrasWithBasis):
+    def __init__(self, R, M):
+        self._matroid = M
+        self._ideal = ChowRingIdeal(M, R)
+        self.poly_ring = self._ideal.poly_ring
+        QuotientRing_nc.__init__(R, self._ideal, names=self.poly_ring.variable_names, category=GradedAlgebrasWithBasis)
+
+    def _repr_(self):
+        return "Chow ring of {}".format(self._matroid)    
+
+    def _latex_(self):
+        return "%s/%s" % (latex.latex(self.poly_ring), latex.latex(self._ideal))     
