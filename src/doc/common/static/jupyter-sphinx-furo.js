0ef948c269570cc87832da6ca2c140835dc53961
Kwankyu Lee <ekwankyu@gmail.com>
2024-07-01 15:02:49 +0900

    Small fix

--- src/doc/common/static/jupyter-sphinx-furo.js
+++ src/doc/common/static/jupyter-sphinx-furo.js
@@ -67,7 +67,7 @@ var versionMap = {};
 async function fetchVersions() {
     try {
         // For the origin of this site, see .github/workflows/doc-publish.yml
-        let versions_file = "https://doc-release--sagemath.netlify.app/html/en/versions.txt"
+        let versions_file = "https://doc-release--sagemath.netlify.app/html/en/versions.txt";
         let response = await fetch(versions_file);
         if (!response.ok) {
             throw new Error('Network response was not ok');
