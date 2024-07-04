eb1cc24d2ff8b22c054b12b8ae6f9073918f0753
Aram Dermenjian <aram.dermenjian.math@gmail.com>
2024-07-04 04:55:18 -0700

    Fix lint issue

--- src/sage/matroids/oriented_matroids/abstract_oriented_matroid.py
+++ src/sage/matroids/oriented_matroids/abstract_oriented_matroid.py
@@ -399,7 +399,7 @@ class AbstractOrientedMatroid(UniqueRepresentation, Parent):
         .. MATH::
 
             \mathcal{C} / A = \left\{ X\mid_{E \backslash A} : X \in \mathcal{C} \text{ and} A \subseteq X^0 \right\}
-            
+
         EXAMPLES::
 
             sage: from sage.matroids.oriented_matroids.oriented_matroid import OrientedMatroid
