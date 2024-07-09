79538064adfdcabea73b25b378d88e3566503d9f
Shriya M <25shriya@gmail.com>
2024-07-09 18:59:14 +0530

    Split Augmented Chow ring ideal class into 3 classes

--- src/sage/matroids/chow_ring_ideal.py
+++ src/sage/matroids/chow_ring_ideal.py
@@ -1,16 +1,8 @@
-from sage.rings.polynomial.multi_polynomial_ideal import MPolynomialIdeal
-from sage.matroids.matroid import Matroid
-from sage.matroids.utilities import cmp_elements_key
-from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
-from sage.sets.set import Set
-from sage.rings.polynomial.multi_polynomial_sequence import PolynomialSequence
-
 r"""
 Chow ring ideals of matroids
 
 AUTHORS:
 
-- Travis Scrimshaw 
 - Shriya M
 
 These are the classes of Chow ring ideals for matroids. There are two classes 
@@ -25,21 +17,14 @@ REFERENCES
 - :arxiv:`2309.14312`
 - :arxiv:`2111.00393`
 """
-#*****************************************************************************
-#       Copyright (C) 2024 Travis Scrimshaw
-#                     2024 Shriya M
-#
-#  Distributed under the terms of the GNU General Public License (GPL)
-#
-#    This code is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-#    General Public License for more details.
-#
-#  The full text of the GPL is available at:
-#
-#                  http://www.gnu.org/licenses/
-#*****************************************************************************
+
+from sage.rings.polynomial.multi_polynomial_ideal import MPolynomialIdeal
+from sage.matroids.utilities import cmp_elements_key
+from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
+from sage.sets.set import Set
+from sage.rings.polynomial.multi_polynomial_sequence import PolynomialSequence
+from abc import ABC, abstractmethod, ABCMeta
+from sage.structure.sage_object import SageObject
 
 class ChowRingIdeal(MPolynomialIdeal):
     r"""
@@ -128,17 +113,14 @@ class ChowRingIdeal(MPolynomialIdeal):
             for G in flats: 
                 if not (F < G or G < F):
                     gb.append(self.flats_generator[F]*self.flats_generator[G])
-                elif Set(F).is_empty():
+                else:
                     term = self.poly_ring.zero()
                     for H in flats:
                         if H < F:
                             term += self.flats_generator[H]
-                    gb.append(term**self._matroid.rank(Set(G)))
-                elif F < G:
-                    term = self.poly_ring.zero()
-                    for H in flats:
-                        if H < F:
-                            term += self.flats_generator[H]
+                    if Set(F).is_empty():
+                        gb.append(term**self._matroid.rank(Set(G)))
+                    else:
                         gb.append(term**(self._matroid.rank(Set(G))-self._matroid.rank(Set(F))))
 
         g_basis = PolynomialSequence(self.poly_ring, [gb])
@@ -150,12 +132,28 @@ class ChowRingIdeal(MPolynomialIdeal):
     
     def flats_generator(self):
         return dict(self.flats_generator)
+    
+class AugmentedChowRingIdeal(SageObject):
 
+    @abstractmethod
+    def gens_constructor():
+        pass
 
+    @abstractmethod
+    def groebner_basis():
+        pass
 
-class AugmentedChowRingIdeal(MPolynomialIdeal):
+    def matroid(self):
+        M = self._matroid
+        return M
+    
+    def flats_generator(self):
+        return dict(self.flats_generator)
+
+class AugmentedChowRingIdeal_fy(AugmentedChowRingIdeal, MPolynomialIdeal):
     r"""
-    The class of Chow ring ideal, a multi-polynomial ideal. 
+    The class of augmented Chow ring ideal of Feitchner-Yuzvinsky
+    presentation, a multi-polynomial ideal.
     Base class - ``MPolynomialIdeal``.
 
     INPUT:
@@ -163,96 +161,60 @@ class AugmentedChowRingIdeal(MPolynomialIdeal):
 
     - `M` -- a matroid.
     - `R` -- a ring.
-    - ``atom_free`` -- a Boolean value, default value set to ``False``. When
-    ``True``, it returns the atom-free presentation of the augmented Chow
-    ring. If ``False``, it returns the Feitchner-Yuzvinsky presentation of the
-    augmented Chow ring.
 
-    OUTPUT: augmented Chow ring ideal of matroid `M`.
+    OUTPUT: augmented Chow ring ideal of matroid `M` of Feitchner-Yuzvinsky
+    presentation.
 
     EXAMPLES::
 
     Augmented Chow ring ideal of Wheel matroid of rank 3::
 
-        sage: ch = AugumentedChowRingIdeal(M=matroids.Wheel(3), R=QQ)
+        sage: ch = AugumentedChowRingIdeal_fy(M=matroids.Wheel(3), R=QQ)
         sage: ch
         Augmented Chow ring ideal of Wheel(3): Regular matroid of rank 3 on 
         6 elements with 16 bases of Feitchner-Yuzvinsky presentation
     """
-    def __init__(self, M, R, atom_free=False):
-        self.atom_free = atom_free
+    def __init__(self, M, R):
         self._matroid = M
-        flats = [X for i in range(1, self._matroid.rank())
+        self.flats = [X for i in range(1, self._matroid.rank())
                  for X in self._matroid.flats(i)]
-        
         E = list(self._matroid.groundset())
-        flats_containing = {x: [] for x in E}
-        for F in flats:
-            for x in F:
-                flats_containing[x].append(F)
         self.flats_generator = dict()
         try:
             names_groundset = ['A{}'.format(''.join(str(x))) for x in E]
-            names_flats = ['B{}'.format(''.join(str(x) for x in sorted(F, key=cmp_elements_key))) for F in flats]
+            names_flats = ['B{}'.format(''.join(str(x) for x in sorted(F, key=cmp_elements_key))) for F in self.flats]
             self.poly_ring = PolynomialRing(R, names_groundset + names_flats)
         except ValueError:
-            self.poly_ring = PolynomialRing(R, 'A', len(E) + len(flats))
-        gens = self.poly_ring.gens()
+            self.poly_ring = PolynomialRing(R, 'A', len(E) + len(self.flats))
         for i,x in enumerate(E):
-            self.flats_generator[x] = gens[i]
-        for i,F in enumerate(flats):
-            self.flats_generator[F] = gens[len(E) + i]
+            self.flats_generator[x] = self.poly_ring.gens()[i]
+        for i,F in enumerate(self.flats):
+            self.flats_generator[F] = self.poly_ring.gens()[len(E) + i]
+        MPolynomialIdeal.__init__(self, self.poly_ring, self.gens_constructor())
+    
+    def gens_constructor(self):
+        E = list(self._matroid.groundset())
+        flats_containing = {x: [] for x in E}
+        for F in self.flats:
+            for x in F:
+                flats_containing[x].append(F)
 
-
-        le = len(E)
         Q = list()
-        for i,F in enumerate(flats):
-            for j,G in enumerate(flats):
+        for F in self.flats:
+            for G in self.flats:
                     if not (F < G or G < F):
-                        Q.append(gens[le+i] * gens[le+j])
-        
-        for j,F in enumerate(flats):
-            for k,x in enumerate(E):
+                        Q.append(self.flats_generator[F] * self.flats_generator[G])
+        L = list()
+        for x in E:
+            term = self.poly_ring.zero()
+            for F in self.flats:
                 if F not in flats_containing[x]:
-                    if self.atom_free:
-                        term = self.poly_ring.zero()
-                        for G in flats_containing[x]:
-                            term += self.flats_generator[G]
-                        Q.append(self.flats_generator[F]*term)
-                    else:
-                        Q.append(gens[k]*gens[le+j])
-                   
-
-        if self.atom_free:
-            for i in E:
-                term = self.poly_ring.zero()
-                for F in flats_containing[i]:
                     term += self.flats_generator[F]
-                Q.append(term**2)
-            
-            MPolynomialIdeal.__init__(self, self.poly_ring, Q)
-        else:
-            L = list()
-            for i,x in enumerate(E):
-                term = self.poly_ring.zero()
-                for j,F in enumerate(flats):
-                    if F not in flats_containing[x]:
-                        term += gens[le+j]
-                L.append(gens[i] - term)
-            
-            MPolynomialIdeal.__init__(self, self.poly_ring, Q + L)
-    
-    def __repr__(self):
-        if self.atom_free:
-            return "Augmented Chow ring ideal of {} of atom-free presentation".format(self._matroid)
-        else:
-            return "Augmented Chow ring ideal of {} of Feitchner-Yuzvinsky presentation".format(self._matroid)
+            L.append(self.flats_generator[x] - term)
+        return Q + L
 
-    def matroid(self):
-        return self._matroid
-    
-    def flat_generator(self):
-        return dict(self.flats_generator)
+    def __repr__(self):
+        return "Augmented Chow ring ideal of {} of Feitchner-Yuzvinsky presentation".format(self._matroid)
     
     def groebner_basis(self):
         r"""
@@ -261,16 +223,119 @@ class AugmentedChowRingIdeal(MPolynomialIdeal):
 
         EXAMPLES::
 
-            sage: ch = AugmentedChowRingIdeal(M=matroids.catalog.Fano(), R=QQ)
+            sage: ch = AugmentedChowRingIdeal_fy(M=matroids.catalog.Fano(), R=QQ)
             sage: ch.groebner_basis()
             Polynomial Sequence with 2744 Polynomials in 21 Variables
-        
-        Another example would be the Groebner basis of the augmented Chow ring
-        ideal (atom-free presentation) of the Graphic matroid of cycle graph of
-        3 vertices::
+        """
+        gb = []
+        E = list(self._matroid.groundset())
+        for i in E:
+            for F in self.flats:
+                for G in self.flats:
+                    term = self.poly_ring.zero()
+                    term1 = self.poly_ring.zero()
+                    for H in self.flats:
+                        if i in Set(H):
+                            term += self.flats_generator[H]
+                        if H > F:
+                            term1 += self.flats_generator[H]
+
+                    gb.append(self.flats_generator[i] + term)
+                    gb.append(term1**(self._matroid.rank(Set(F))) + 1)
+
+                    if i in Set(F):
+                        gb.append(self.flats_generator[i]*((term1)**self._matroid.rank(Set(F))))
+            
+                    elif not i in Set(F):
+                        gb.append(self.flats_generator[i]*self.flats_generator[F])
+                    
+                    elif not (F < G or G < F):
+                        gb.append(self.flats_generator[F]*self.flats_generator[G])
+                    
+                    elif G < F:
+                        gb.append(self.flats_generator[G]*term1**(self._matroid.rank(Set(F))-self._matroid.rank(Set(G))))
+
+        g_basis = PolynomialSequence(self.poly_ring, [gb])
+        return g_basis
+
+class AugmentedChowRingIdeal_atom_free(AugmentedChowRingIdeal, MPolynomialIdeal):
+    r"""
+    The class of augmented Chow ring ideal of atom-free
+    presentation, a multi-polynomial ideal.
+    Base class - ``MPolynomialIdeal``.
+
+    INPUT:
+
+
+    - `M` -- a matroid.
+    - `R` -- a ring.
+
+    OUTPUT: augmented Chow ring ideal of matroid `M` of atom-free
+    presentation.
+
+    EXAMPLES::
+
+    Augmented Chow ring ideal of Wheel matroid of rank 3::
+
+        sage: ch = AugumentedChowRingIdeal_atom_free(M=matroids.Wheel(3), R=QQ)
+        sage: ch
+        Augmented Chow ring ideal of Wheel(3): Regular matroid of rank 3 on 
+        6 elements with 16 bases of atom-free presentation
+    """
+    def __init__(self, M, R):
+        self._matroid = M
+        self.flats = [X for i in range(1, self._matroid.rank())
+                 for X in self._matroid.flats(i)]
+        E = list(self._matroid.groundset())
+        self.flats_generator = dict()
+        try:
+            names_groundset = ['A{}'.format(''.join(str(x))) for x in E]
+            names_flats = ['B{}'.format(''.join(str(x) for x in sorted(F, key=cmp_elements_key))) for F in self.flats]
+            self.poly_ring = PolynomialRing(R, names_groundset + names_flats)
+        except ValueError:
+            self.poly_ring = PolynomialRing(R, 'A', len(E) + len(self.flats))
+        for i,x in enumerate(E):
+            self.flats_generator[x] = self.poly_ring.gens()[i]
+        for i,F in enumerate(self.flats):
+            self.flats_generator[F] = self.poly_ring.gens()[len(E) + i]
+        MPolynomialIdeal.__init__(self, self.poly_ring, self.gens_constructor())
+
+    def gens_constructor(self): 
+        E = list(self._matroid.groundset())
+        Q = []
+        flats_containing = {x: [] for x in E}
+        for F in self.flats:
+            for x in F:
+                flats_containing[x].append(F)
+        for F in self.flats:
+            for x in E:
+                if F not in flats_containing[x]:
+                    Q.append(self.flats_generator[x]*self.flats_generator[F])
+                    term = self.poly_ring.zero()
+                    for G in flats_containing[x]:
+                        term += self.flats_generator[G]
+                        Q.append(self.flats_generator[F]*term)
+                   
+        for i in E:
+            term = self.poly_ring.zero()
+            for F in flats_containing[i]:
+                term += self.flats_generator[F]
+            Q.append(term**2)
+
+        return Q
+            
+    def __repr__(self):
+        return "Augmented Chow ring ideal of {} of atom-free presentation".format(self._matroid)
+    
+    def groebner_basis(self):
+        r"""
+        Returns the Groebner basis of the augmented Chow ring ideal.
+        Return type - ``PolynomialSequence``.
+
+        EXAMPLES::
 
             sage: M1 = GraphicMatroid(graphs.CycleGraph(3))
-            sage: ch = AugmentedChowRingIdeal(M=M1, R=QQ, atom_free=True)
+            sage: ch = AugmentedChowRingIdeal_atom_free(M=M1, R=QQ)
             sage: ch.groebner_basis()
             [B0^2, B0*B1, B0*B2, B0*B1, B1^2, B1*B2, B0*B2, B1*B2, B2^2]
         """
@@ -279,54 +344,20 @@ class AugmentedChowRingIdeal(MPolynomialIdeal):
                  for X in self._matroid.flats(i)]
         if Set([]) in flats:
             flats.remove(Set([]))
-        if self.atom_free:
-            for F in flats:
-                for G in flats:
-                    if not (F > G or G > F):
-                        gb.append(self.flats_generator[F]*self.flats_generator[G])
-                    elif F < G:
-                        term = self.poly_ring.zero()
-                        for H in flats:
-                            if H < F:
-                                term += self.flats_generator[H]
-                        gb.append(self.flats_generator[F]*(term**self._matroid.rank(Set(G)))*
-                                (term**(self._matroid.rank(Set(G))-self._matroid.rank(Set(F)))))
-        
-        else:
-            E = list(self._matroid.groundset())
-            for i in E:
-                for F in flats:
-                    for G in flats:
-                        term = self.poly_ring.zero()
-                        for H in flats:
-                            if i in Set(H):
-                                term += self.flats_generator[H]
-                        gb.append(self.flats_generator[i] + term)
-
-                        if i in Set(F):
-                            term = self.poly_ring.zero()
-                            for H in flats:
-                                if H < F:
-                                    term += self.flats_generator[H]
-                            gb.append(self.flats_generator[i]*(term**self._matroid.rank(Set(G)))*
-                                (term**(self._matroid.rank(Set(G))-self._matroid.rank(Set(F)))))
-                        
-                        elif not i in Set(F):
-                            gb.append(self.flats_generator[i]*self.flats_generator[F])
-                        
-                        elif not (F < G or G < F):
-                            gb.append(self.flats_generator[F]*self.flats_generator[G])
-                        
-                        elif F < G:
-                            term = self.poly_ring.zero()
-                            for H in flats:
-                                if H < F:
-                                    term += self.flats_generator[H]
-                            gb.append(term**(self._matroid.rank(Set(G))-self._matroid.rank(Set(F))))
+        for F in flats:
+            for G in flats:
+                if not (F > G or G > F):
+                    gb.append(self.flats_generator[F]*self.flats_generator[G])
+                elif F < G:
+                    term = self.poly_ring.zero()
+                    for H in flats:
+                        if H < F:
+                            term += self.flats_generator[H]
+                    gb.append(self.flats_generator[F]*(term**self._matroid.rank(Set(G)))*
+                            (term**(self._matroid.rank(Set(G))-self._matroid.rank(Set(F)))))
 
         g_basis = PolynomialSequence(self.poly_ring, [gb])
         return g_basis
-
 
 
         
