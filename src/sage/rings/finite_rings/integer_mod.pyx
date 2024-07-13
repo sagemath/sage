fe1923d3280456cc6df1868f410c63a614b477a5
AZ-0 <35401882+AZ-0@users.noreply.github.com>
2024-07-14 00:00:35 +0200

    add optional check= argument to .log() method for modn finite-field elements

--- src/sage/rings/finite_rings/integer_mod.pyx
+++ src/sage/rings/finite_rings/integer_mod.pyx
@@ -638,7 +638,7 @@ cdef class IntegerMod_abstract(FiniteRingElement):
         else:
             return sib(self.parent())(v)
 
-    def log(self, b=None, order=None):
+    def log(self, b=None, order=None, check=False):
         r"""
         Compute the discrete logarithm of this element to base `b`,
         that is,
@@ -655,8 +655,11 @@ cdef class IntegerMod_abstract(FiniteRingElement):
            ``R`` is the parent of ``self``.
         
         -  ``order`` -- integer (unused), the order of ``b``.
-           This argument is unused, only there for coherence of
-           api with finite field elements.
+           This argument is normally unused, only there for
+           coherence of apis with finite field elements.
+        
+        - ``check`` -- boolean (default: ``False``). If set,
+           test whether the given ``order`` is correct.
 
 
         OUTPUT:
@@ -784,6 +787,11 @@ cdef class IntegerMod_abstract(FiniteRingElement):
             b = self._parent(b)
             if not b.is_unit():
                 raise ValueError(f"logarithm with base {b} is not defined since it is not a unit modulo {b.modulus()}")
+
+        if check:
+            from sage.groups.generic import has_order
+            if not has_order(b, order, '*'):
+                raise ValueError('b does not have the provided order')
 
         cdef Integer n = Integer()
         cdef Integer m = one_Z
