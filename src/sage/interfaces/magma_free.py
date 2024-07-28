391639237f5bcb4dfaafaa77d224e38bd9d67dc7
Siddharth Baleja <siddharthbaleja83@gmail.com>
2024-07-28 09:49:42 +0530

    Fix Style Issues

--- src/sage/interfaces/magma_free.py
+++ src/sage/interfaces/magma_free.py
@@ -1,4 +1,6 @@
-"Interface to the free online MAGMA calculator"
+"""
+Interface to the free online MAGMA calculator.
+"""
 
 # ****************************************************************************
 # Copyright (C) 2007 William Stein <wstein@gmail.com>
@@ -20,9 +22,10 @@ class MagmaExpr(str):
     def __repr__(self):
         return str(self)
 
+
 def magma_free_eval(code, strip=True, columns=0):
     """
-    Use the free online MAGMA calculator to evaluate the given input code 
+    Use the free online MAGMA calculator to evaluate the given input code
     and return the answer as a string.
 
     INPUT:
@@ -33,7 +36,7 @@ def magma_free_eval(code, strip=True, columns=0):
     OUTPUT:
     - A string containing the output from the MAGMA online calculator.
 
-    LIMITATIONS: 
+    LIMITATIONS:
     - The code must evaluate in at most 120 seconds.
     - There is a limitation on the amount of RAM available.
 
@@ -42,16 +45,15 @@ def magma_free_eval(code, strip=True, columns=0):
         sage: magma_free_eval("Factorization(9290348092384)") # optional - internet
         [ <2, 5>, <290323377887, 1> ]
     """
-
     from urllib.parse import urlencode
     from http import client as httplib
     from xml.dom.minidom import parseString
-    
+
     server = "magma.maths.usyd.edu.au"
     processPath = "/xml/calculator.xml"
     refererPath = "/calc/"
     refererUrl = f"http://{server}{refererPath}"
-    
+
     code = f"SetColumns({columns});\n{code}"
     params = urlencode({'input': code})
     headers = {
@@ -59,13 +61,13 @@ def magma_free_eval(code, strip=True, columns=0):
         "Accept": "text/html, application/xml, application/xhtml+xml",
         "Referer": refererUrl
     }
-    
+
     conn = httplib.HTTPConnection(server)
     conn.request("POST", processPath, params, headers)
     response = conn.getresponse()
     results = response.read()
     conn.close()
-    
+
     xmlDoc = parseString(results)
     res = []
     resultsNodeList = xmlDoc.getElementsByTagName('results')
@@ -75,12 +77,13 @@ def magma_free_eval(code, strip=True, columns=0):
         for line in lines:
             for textNode in line.childNodes:
                 res.append(textNode.data)
-    
+
     res = "\n".join(res)
     if strip:
         res = res.strip()
-    
+
     return MagmaExpr(res)
+
 
 class MagmaFree:
     """
@@ -89,7 +92,7 @@ class MagmaFree:
     Evaluate MAGMA code without requiring MAGMA to be installed on your computer
     by using the free online MAGMA calculator.
 
-    LIMITATIONS: 
+    LIMITATIONS:
     - The code must evaluate in at most 120 seconds.
     - There is a limitation on the amount of RAM available.
 
@@ -114,5 +117,6 @@ class MagmaFree:
         See the class documentation for more details and examples.
         """
         return magma_free_eval(code, strip=strip, columns=columns)
+
 
 magma_free = MagmaFree()
