1ae3ecc8ead792483e0f2d4ed6bb761747eb5dc0
Newtech66 <contact.mainakroy@gmail.com>
2024-07-29 21:45:55 +0530

    Split species into new PR

--- src/sage/rings/all.py
+++ src/sage/rings/all.py
@@ -166,8 +166,7 @@ from sage.rings.continued_fraction import (continued_fraction,
 # asymptotic ring
 from sage.rings.asymptotic.all import *
 
-lazy_import('sage.rings.burnside', ['BurnsideRing', 'PolynomialMolecularDecomposition',
-                                    'MultivariateAtomicSpecies'])
+lazy_import('sage.rings.burnside', 'BurnsideRing')
 
 # Register classes in numbers abc
 from sage.rings import numbers_abc
