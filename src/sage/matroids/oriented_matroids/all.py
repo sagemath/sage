3a4c1fc45a685a6d9b2fb4b668beec4a79a35de8
Giorgos Mousa <gmousa@proton.me>
2024-09-07 15:01:46 +0000

    Delete catalog files
    Because they are empty

--- src/sage/matroids/oriented_matroids/all.py
+++ src/sage/matroids/oriented_matroids/all.py
@@ -7,4 +7,3 @@ install_doc(__package__, __doc__)
 from sage.misc.lazy_import import lazy_import
 lazy_import('sage.matroids.oriented_matroids.oriented_matroid', 'OrientedMatroid')
 lazy_import('sage.matroids.oriented_matroids.abstract_oriented_matroid', 'AbstractOrientedMatroid')
-lazy_import('sage.matroids.oriented_matroids', 'oriented_matroids_catalog', 'oriented_matroids')
