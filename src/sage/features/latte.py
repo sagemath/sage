5047161a7ceae829e3f48765a068073f226d84d7
Vincent Delecroix <20100.delecroix@gmail.com>
2024-09-19 11:03:47 +0200

    implement is_functional for LattE executable

--- src/sage/features/latte.py
+++ src/sage/features/latte.py
@@ -16,14 +16,41 @@ Features for testing the presence of ``latte_int``
 #                  https://www.gnu.org/licenses/
 # ****************************************************************************
 
-from . import Executable
+import subprocess
+from . import Executable, FeatureTestResult
 from .join_feature import JoinFeature
 
 
 LATTE_URL = "https://www.math.ucdavis.edu/~latte/software.php"
 
 
-class Latte_count(Executable):
+class Latte_executable(Executable):
+    def is_functional(self):
+        r"""
+        Check whether this LattE executable works on trivial input.
+
+        EXAMPLES::
+
+            sage: from sage.features.latte import Latte_count, Latte_integrate
+
+            sage: Latte_count().is_functional() # optional - latte_int
+            FeatureTestResult('count', True)
+            sage: Latte_integrate().is_functional() # optional - latte_int
+            FeatureTestResult('integrate', True)
+        """
+        try:
+            lines = subprocess.check_output(self.executable, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            return FeatureTestResult(self, False,
+                    reason="Call `{command}` failed with exit code {e.returncode}".format(command=self.executable, e=e))
+        if 'LattE' not in lines:
+            return FeatureTestResult(self, False,
+                    reason="Call `{command}` did not produce output which contains `{expected}`".format(command=self.executable, expected='LattE'))
+
+        return FeatureTestResult(self, True)
+
+
+class Latte_count(Latte_executable):
     r"""
     Feature for the executable ``count`` from :ref:`LattE integrale <spkg_latte_int>`.
     """
@@ -40,7 +67,7 @@ class Latte_count(Executable):
                             url=LATTE_URL)
 
 
-class Latte_integrate(Executable):
+class Latte_integrate(Latte_executable):
     r"""
     Feature for the executable ``integrate`` from :ref:`LattE integrale <spkg_latte_int>`.
     """
