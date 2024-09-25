2199d6188964f457ae04d6b1ec6a426be05fe56c
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-09-25 12:56:45 -0700

    src/pyproject.toml.m4: Merge all metadata from src/setup.cfg.m4, remove src/setup.cfg.m4, update docs

--- .ci/write-dockerfile.sh
+++ .ci/write-dockerfile.sh
@@ -275,7 +275,7 @@ $ADD Makefile VERSION.txt COPYING.txt condarc.yml README.md bootstrap bootstrap-
 $ADD config/config.rpath /new/config/config.rpath
 $ADD src/doc/bootstrap /new/src/doc/bootstrap
 $ADD src/bin /new/src/bin
-$ADD src/Pipfile.m4 src/pyproject.toml src/requirements.txt.m4 src/setup.cfg.m4 src/VERSION.txt /new/src/
+$ADD src/Pipfile.m4 src/pyproject.toml src/requirements.txt.m4 src/VERSION.txt /new/src/
 $ADD m4 /new/m4
 $ADD pkgs /new/pkgs
 $ADD build /new/build
