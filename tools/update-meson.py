46f490f7ceb785c341d37924805e5783cb6bdff8
Tobias Diez <code@tobiasdiez.com>
2024-10-15 04:23:58 +0000

    Improve handling of optional dependencies

--- tools/update-meson.py
+++ tools/update-meson.py
@@ -140,9 +140,6 @@ def update_python_sources(self: Rewriter, visitor: AstPython):
             "bliss.pyx",
             "mcqd.pyx",
             "tdlib.pyx",
-            "meataxe.pyx",
-            "sirocco.pyx",
-            "parametric_surface.pyx",
         }
         cython_files = [x for x in cython_files if x.name not in special_cython_files]
         # Add all cython files that are not in the source list
