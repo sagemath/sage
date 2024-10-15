71197264e74f9537d19fa5c1ae0e8fc1bb2f47f9
Kwankyu Lee <ekwankyu@gmail.com>
2024-10-16 06:54:48 +0900

    Add features

---
+++ src/sage/features/sloane_database.py
@@ -0,0 +1,56 @@
+# sage_setup: distribution = sagemath-environment
+r"""
+Feature for testing the presence of Sloane Online Encyclopedia of Integer Sequences
+"""
+
+# ****************************************************************************
+#       Copyright (C) 2024 Kwankyu Lee <ekwankyu@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#                  https://www.gnu.org/licenses/
+# ****************************************************************************
+
+from . import Feature
+
+
+class SloaneOEIS(Feature):
+    r"""
+    A :class:`~sage.features.Feature` which describes the presence of
+    the Sloane Online Encyclopedia of Integer Sequences.
+
+    EXAMPLES::
+
+        sage: from sage.features.sloane_database import SloaneOEIS
+        sage: bool(SloaneOEIS().is_present())  # optional - sloane_database
+        True
+    """
+    def __init__(self):
+        r"""
+        TESTS::
+
+            sage: from sage.features.sloane_database import SloaneOEIS
+            sage: isinstance(SloaneOEIS(), SloaneOEIS)
+            True
+        """
+        Feature.__init__(self, name='sloane_database',
+                         description='Sloane Online Encyclopedia of Integer Sequences')
+
+    def _is_present(self):
+        r"""
+        Return whether the database is installed.
+
+        EXAMPLES::
+
+            sage: from sage.features.sloane_database import SloaneOEIS
+            sage: bool(SloaneOEIS().is_present())  # optional - !sloane_database
+            False
+        """
+        from sage.databases.sloane import SloaneEncyclopedia
+        return SloaneEncyclopedia.is_installed()
+
+
+def all_features():
+    return [SloaneOEIS()]
