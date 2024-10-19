2bf9a090bcaaf1297e172b15469f1332905e5552
Tobias Diez <code@tobiasdiez.com>
2024-10-19 01:56:40 +0000

    add all pxd files

--- tools/update-meson.py
+++ tools/update-meson.py
@@ -80,7 +80,7 @@ def update_python_sources(self: Rewriter, visitor: AstPython):
 
         folder = Path(target.filename).parent
         python_files = sorted(
-            list(folder.glob("*.py"))
+            list(folder.glob("*.py")) + list(folder.glob('*.pxd'))
         )  # + list(folder.glob('*.pxd')) + list(folder.glob('*.h')))
 
         to_append: list[StringNode] = []
