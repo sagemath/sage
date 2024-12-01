0c375f56a1ff75002e86a1ff70fe617ecab5a0d5
Shriya M <25shriya@gmail.com>
2024-12-01 18:25:34 +0530

    Created file for Kahler Algebras category

---
+++ src/sage/categories/kahler_algebras.py
@@ -0,0 +1,14 @@
+r"""
+Category of Kahler Algebras.
+
+AUTHORS:
+
+- Shriya M
+"""
+
+from sage.categories.category_types import Category_over_base_ring
+class KahlerAlgebras(Category_over_base_ring):
+    def super_categories(self):
+        pass
+    
+    
