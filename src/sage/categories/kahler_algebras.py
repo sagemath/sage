6287bb8d80557cf42e2f14eed4e9e54249867e0a
Shriya M <25shriya@gmail.com>
2024-12-03 17:57:27 +0530

    Added poincare_pairing() method

--- src/sage/categories/kahler_algebras.py
+++ src/sage/categories/kahler_algebras.py
@@ -7,8 +7,20 @@ AUTHORS:
 """
 
 from sage.categories.category_types import Category_over_base_ring
+from sage.categories.graded_algebras_with_basis import GradedAlgebrasWithBasis
+
 class KahlerAlgebras(Category_over_base_ring):
-    def super_categories(self):
-        pass
+    class ParentMethods:
+        def super_categories(self):
+            return GradedAlgebrasWithBasis
+        def poincare_pairing(self, a, b, r):
+            if (a.homogeneous_degree() <= (r/2)) & (b.homogeneous_degree() == (r - a.homogeneous_degree())):
+                el = a*b
+                return el.degree()
+                
+
+
+
+            
     
     
