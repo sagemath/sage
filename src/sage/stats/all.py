d2655547eee5b9781cbac7d788109046d3cfadca
nataliedurgin <nataliedurgin@users.noreply.github.com>
2024-12-04 17:16:46 -0800

    Remove import

--- src/sage/stats/all.py
+++ src/sage/stats/all.py
@@ -3,7 +3,6 @@ import sage.stats.distributions.catalog as distributions
 from sage.stats.r import ttest
 from sage.stats.basic_stats import (mean, mode, std, variance, median, moving_average)
 from sage.stats.hmm import all as hmm
-from sage.stats.statistical_tests import sigmas_from_uniform
 
 # We lazy_import the following modules since they import numpy which
 # slows down sage startup
