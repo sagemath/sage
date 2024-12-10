ea541dc50c44978f9ff2c4b294f5d28dabba6431
Shriya M <25shriya@gmail.com>
2024-12-10 12:31:29 +0530

    Modified super_categories() method

--- src/sage/categories/kahler_algebras.py
+++ src/sage/categories/kahler_algebras.py
@@ -8,24 +8,26 @@ AUTHORS:
 
 from sage.categories.category_types import Category_over_base_ring
 from sage.categories.graded_algebras_with_basis import GradedAlgebrasWithBasis
-from abc import abstractmethod
+from sage.misc.abstract_method import abstract_method
 
 class KahlerAlgebras(Category_over_base_ring):
     class ParentMethods:
         def super_categories(self):
-            return GradedAlgebrasWithBasis
+            return [GradedAlgebrasWithBasis(self.base_ring())]
+
         def poincare_pairing(self, a, b, r):
             if (a.homogeneous_degree() <= (r/2)) & (b.homogeneous_degree() == (r - a.homogeneous_degree())):
                 el = a*b
                 return el.degree()
-        @abstractmethod
-        def lefschetz_element(self):
+
+        @abstract_method
+        def lefschetz_element():
             pass
         
-        def lefschetz_element(self, el, a, r):
+        def lefschetz_element_injection(self, el, a, r):
             if a.homogeneous_degree() < r/2:
                 return a*el
-        
+
         def hodge_riemann_relations(self, el, a, r):
             if a.homogeneous_degree() <= r/2:
                 element = a*(el**(r-(2*a.homogeneous_degree())))*a
