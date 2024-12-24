11d8b9346d8ff13165fd7ffd5657c48730a564df
Tobias Diez <code@tobiasdiez.de>
2024-12-24 15:42:53 +0800

    Annotate test in Galois group as random
    It is constantly failing, eg https://github.com/sagemath/sage/actions/runs/12461220078/job/34780487946?pr=38879. I think its because of this old issue https://github.com/sagemath/sage/issues/18308

--- src/sage/rings/number_field/galois_group.py
+++ src/sage/rings/number_field/galois_group.py
@@ -994,7 +994,7 @@ class GaloisGroup_v2(GaloisGroup_perm):
             sage: x = polygen(ZZ, 'x')
             sage: K.<b> = NumberField(x^4 - 2*x^2 + 2, 'a').galois_closure()
             sage: G = K.galois_group()
-            sage: [G.artin_symbol(P) for P in K.primes_above(7)]
+            sage: [G.artin_symbol(P) for P in K.primes_above(7)]  # random order, see Issue #18308
             [(1,4)(2,3)(5,8)(6,7), (1,4)(2,3)(5,8)(6,7),
              (1,5)(2,6)(3,7)(4,8), (1,5)(2,6)(3,7)(4,8)]
             sage: G.artin_symbol(17)
