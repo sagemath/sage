fff86ea0d76827401cb4b4092000e84865857c25
dcoudert <david.coudert@inria.fr>
2025-01-11 11:02:25 +0100

    fix issue in sage/combinat/designs/designs_pyx.pyx

--- src/sage/combinat/designs/designs_pyx.pyx
+++ src/sage/combinat/designs/designs_pyx.pyx
@@ -470,8 +470,6 @@ def is_group_divisible_design(groups, blocks, v, G=None, K=None, lambd=1, verbos
 
     cdef MemoryAllocator mem = MemoryAllocator()
     cdef unsigned short * matrix = <unsigned short *> mem.calloc(n*n, sizeof(unsigned short))
-    if matrix is NULL:
-        raise MemoryError(f"{n}")
 
     # Counts the number of occurrences of each pair of points
     for b in blocks:
