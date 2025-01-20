a92cb5db816e9252749b15ef46d057197fabc972
Frédéric Chapoton <chapoton@unistra.fr>
2025-01-20 13:52:19 +0100

    some fixes for PLR1716

--- src/sage/combinat/sloane_functions.py
+++ src/sage/combinat/sloane_functions.py
@@ -8083,11 +8083,10 @@ def perm_mh(m, h):
     - Jaap Spies (2006)
     """
     n = m + h
-    M = MatrixSpace(ZZ, m, n)
-    A = M(0)
+    A = MatrixSpace(ZZ, m, n).zero()
     for i in range(m):
-        for j in range(n):
-            if i <= j and j <= i + h:
+        for j in range(i, n):
+            if j <= i + h:
                 A[i, j] = 1
     return A.permanent()
 
