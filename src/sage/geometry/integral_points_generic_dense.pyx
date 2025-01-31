ea0877173f7cc832e4e74fc4cf19bd9a00fc77a6
user202729 <25191436+user202729@users.noreply.github.com>
2025-01-31 12:56:25 +0700

    Add a docstring for explanation

--- src/sage/geometry/integral_points_generic_dense.pyx
+++ src/sage/geometry/integral_points_generic_dense.pyx
@@ -1,4 +1,13 @@
 # cython: wraparound=False, boundscheck=False
+r"""
+Cython helper methods to compute integral points in polyhedra
+
+Note that while the URL of this documentation page ends with
+``integral_points_generic_dense``, this is merely to allow Sphinx to generate
+the documentation automatically. Imports should be from
+:mod:`sage.geometry.integral_points`, as can be seen in the examples below.
+Furthermore, not all functions are exported to the public interface.
+"""
 
 from sage.modules.vector_integer_dense cimport Vector_integer_dense as VectorClass
 from sage.matrix.matrix_dense cimport Matrix_dense as MatrixClass
