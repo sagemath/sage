5d0985d40f4c6cce3378d392960214a5a4233993
Noel Roemmele <noel.roemmele@uleth.ca>
2025-02-03 17:31:18 -0700

    Updated wording of doctest to make it more clear why the doctest was included.

--- src/sage/calculus/transforms/fft.pyx
+++ src/sage/calculus/transforms/fft.pyx
@@ -166,7 +166,7 @@ cdef class FastFourierTransform_complex(FastFourierTransform_base):
 
         TESTS:
 
-        Assigning a value to a pair no longer assumes that the values are uninitialized. ::
+        Verify that :issue:10758 is fixed. ::
 
             sage: F = FFT(1)
             sage: F[0] = (1,1)
