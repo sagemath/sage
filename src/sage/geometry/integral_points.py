0ddb79ae295c99c2990a4fc56763e5fb99b89ae8
user202729 <25191436+user202729@users.noreply.github.com>
2025-02-06 23:41:47 +0700

    Add __all__

--- src/sage/geometry/integral_points.py
+++ src/sage/geometry/integral_points.py
@@ -26,3 +26,9 @@ except ImportError:
         Inequality_int,
         InequalityCollection,
     )
+
+
+# __all__ is needed to generate Sphinx documentation
+__all__ = ['InequalityCollection', 'Inequality_generic', 'Inequality_int',
+           'loop_over_parallelotope_points', 'parallelotope_points', 'print_cache',
+           'ray_matrix_normal_form', 'rectangular_box_points', 'simplex_points']
