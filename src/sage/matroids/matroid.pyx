9ac07c700b0a531ccc13784bfd3b7b04ba7d9797
Shriya M <25shriya@gmail.com>
2025-02-20 16:16:34 +0530

    Corrected doctests

--- src/sage/matroids/matroid.pyx
+++ src/sage/matroids/matroid.pyx
@@ -8080,7 +8080,7 @@ cdef class Matroid(SageObject):
             sage: M = matroids.Wheel(2)
             sage: A = M.chow_ring(R=ZZ, augmented=False, presentation='fy'); A
             Chow ring of Wheel(2): Regular matroid of rank 2 on 4 elements with
-            5 bases over Integer Ring in Feitchner-Yuzvinsky presentation
+            5 bases in Feitchner-Yuzvinsky presentation over Integer Ring
             sage: A.defining_ideal()._gens_constructor(A.defining_ideal().ring())
             [A0*A1, A0*A23, A1*A23, A0 + A0123, A1 + A0123, A23 + A0123]
             sage: A23 = A.gen(0)
@@ -8091,8 +8091,8 @@ cdef class Matroid(SageObject):
 
             sage: M = matroids.catalog.Fano()
             sage: A = M.chow_ring(QQ, False, 'fy'); A
-            Chow ring of Fano: Binary matroid of rank 3 on 7 elements, type (3, 0)
-            over Rational Field in Feitchner-Yuzvinsky presentation
+            Chow ring of Fano: Binary matroid of rank 3 on 7 elements,
+            type (3, 0) in Feitchner-Yuzvinsky presentation over Rational Field
 
         Next we get the non-trivial generators and do some computations::
 
