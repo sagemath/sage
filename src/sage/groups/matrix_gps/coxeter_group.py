19d55256da35d5639cd783ad5b46e4562dff5deb
Eduardo Venturini <35201396+ventu06@users.noreply.github.com>
2025-03-02 00:42:34 +0100

    Fixed _positive_roots_reflections for index_set different from {1, 2, ..., n}

--- src/sage/groups/matrix_gps/coxeter_group.py
+++ src/sage/groups/matrix_gps/coxeter_group.py
@@ -557,13 +557,14 @@ class CoxeterMatrixGroup(UniqueRepresentation, FinitelyGeneratedMatrixGroup_gene
         simple_roots = FreeModule(self.base_ring(), self.ngens()).gens()
 
         refls = self.simple_reflections()
+        refls_index = dict((refl_i[1], refl_i[0]) for refl_i in enumerate(refls.keys()))
         resu = []
         d = {}
         for i in range(1, N + 1):
             segment = word[:i]
             last = segment.pop()
             ref = refls[last]
-            rt = simple_roots[last - 1]
+            rt = simple_roots[refls_index[last]]
             while segment:
                 last = segment.pop()
                 cr = refls[last]
