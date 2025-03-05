c63ca75d03fa8bb4524a03f92d8083f5b7d0f62a
Levente Bodnar <bodnalev@gmail.com>
2025-03-05 11:39:37 +0000

    revert eigenvalue calculation

--- src/sage/algebras/combinatorial_theory.py
+++ src/sage/algebras/combinatorial_theory.py
@@ -1008,7 +1008,7 @@ class _CombinatorialTheory(Parent, UniqueRepresentation):
         for X in iterator:
             Xr = _round_matrix(X, method=0, denom=denom)
             Xnp = np.array(Xr)
-            eigenvalues, eigenvectors = LA.eigh(Xnp)
+            eigenvalues, eigenvectors = LA.eig(Xnp)
             emin = min(eigenvalues)
             if emin<0:
                 eminr = ceil(-emin*denom)/denom
@@ -2795,7 +2795,6 @@ class BuiltTheory(_CombinatorialTheory):
     
     mpt = mul_project_table
     table = mul_project_table
-    
 
 class ExoticTheory(_CombinatorialTheory):
     
