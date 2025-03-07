a6608af7b7b1e708c453f66d0672c0f95c1c1926
Tobias Diez <code@tobiasdiez.de>
2025-03-07 15:33:19 +0100

    Restore setup.py

--- src/setup.py
+++ src/setup.py
@@ -47,7 +47,7 @@ Distribution._removed = staticmethod(lambda ep: True)
 
 import sage.env
 sage.env.SAGE_SRC = os.getcwd()
-from sage.env import cython_aliases, sage_include_directories
+from sage.env import *
 
 sys.excepthook = excepthook
 
@@ -102,6 +102,7 @@ else:
     log.info(f"Cythonizing with {nthreads} threads...")
     try:
         from Cython.Build import cythonize
+        from sage.env import cython_aliases, sage_include_directories
         from sage.misc.package_dir import cython_namespace_package_support
         with cython_namespace_package_support():
             extensions = cythonize(
