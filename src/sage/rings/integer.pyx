fba56030ad20bc3f1057ecb7a2e6ea1af7a78dfd
Noel Roemmele <noel.roemmele@uleth.ca>
2025-03-24 17:17:44 -0600

    Made sure solution was modulo lcm(m,n). Changed <> to !=. Change error message slightly.

--- src/sage/rings/integer.pyx
+++ src/sage/rings/integer.pyx
@@ -6975,7 +6975,7 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
 
     def crt(self, y, m, n):
         """
-        Return the unique integer between `0` and `mn` that is congruent to
+        Return the unique integer between `0` and `\\lcm(m,n)` that is congruent to
         the integer modulo `m` and to `y` modulo `n`.
 
         EXAMPLES::
@@ -6988,7 +6988,7 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
             sage: m%11
             5
 
-        `crt` also works for some non-coprime moduli::
+        ``crt`` also works for some non-coprime moduli::
 
             sage: 6.crt(0,10,4)
             16
@@ -6996,7 +6996,7 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
             Traceback (most recent call last):
             ...
             ValueError: no solution to crt problem since gcd(10,10) does not
-            divide 6-0
+            divide 6 - 0
         """
         cdef object g, s
         cdef Integer _y, _m, _n
@@ -7005,11 +7005,11 @@ cdef class Integer(sage.structure.element.EuclideanDomainElement):
         _n = Integer(n)
         g, s, _ = _m.xgcd(_n)
         if not g.is_one():
-            if (self % g) <> (_y % g):
-                raise ValueError("no solution to crt problem since gcd(%s,%s) does not divide %s-%s" % (_m, _n, self, _y))
-            return (self + g * Integer(0).crt((_y - self) // g, _m // g, _n // g)) % _n.lcm(_m)
+            if (self % g) != (_y % g):
+                raise ValueError(f"no solution to crt problem since gcd({_m},{_n}) does not divide {self} - {_y}")
+            return (self + g * Integer(0).crt((_y - self) // g, _m // g, _n // g)) % _m.lcm(_n)
         # Now s*m + t*n = 1, so the answer is x + (y-x)*s*m, where x=self.
-        return (self + (_y - self) * s * _m) % (_m * _n)
+        return (self + (_y - self) * s * _m) % _m.lcm(_n)
 
     def test_bit(self, long index):
         r"""
