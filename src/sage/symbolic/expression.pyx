632af5dd577c1a407c7d0daf4a7121d351d0dc42
user202729 <25191436+user202729@users.noreply.github.com>
2025-04-07 20:20:23 +0700

    Apply suggestions from code review
    Co-authored-by: Travis Scrimshaw <clfrngrown@aol.com>

--- src/sage/symbolic/expression.pyx
+++ src/sage/symbolic/expression.pyx
@@ -4757,7 +4757,7 @@ cdef class Expression(Expression_abc):
         return matrix([[g.derivative(x) for x in self.arguments()]
                        for g in self.gradient()])
 
-    def series(self, symbol, order=None, algorithm=None):
+    def series(self, symbol, order=None, algorithm='ginac'):
         r"""
         Return the power series expansion of ``self`` in terms of the
         given variable to the given order.
@@ -4771,8 +4771,10 @@ cdef class Expression(Expression_abc):
         - ``order`` -- integer; if nothing given, it is set
           to the global default (``20``), which can be changed
           using :func:`set_series_precision`
-        - ``algorithm`` -- string (default: ``None``);
-          if specified, ``'ginac'`` or ``'maxima'``
+        - ``algorithm`` -- string (default: ``'ginac'``); one of the following:
+
+          * ``'ginac'``
+          * ``'maxima'``
 
         OUTPUT: a power series
 
@@ -4879,14 +4881,12 @@ cdef class Expression(Expression_abc):
             sage: ((1 - x)^-x).series(x, 8, algorithm="ginac")
             1 + 1*x^2 + 1/2*x^3 + 5/6*x^4 + 3/4*x^5 + 33/40*x^6 + 5/6*x^7 + Order(x^8)
         """
-        if algorithm is None:
-            algorithm = "ginac"  # might be changed in the future
         if algorithm == "maxima":
             # call series() again to convert the result (a rational function in the symbol)
             # to a SymbolicSeries with the correct order
             return self.taylor(symbol, 0, order-1).series(symbol, order, algorithm="ginac")
         if algorithm != "ginac":
-            raise ValueError("algorithm must be 'maxima' or 'ginac' if specified")
+            raise ValueError("invalid algorithm")
         cdef Expression symbol0 = self.coerce_in(symbol)
         cdef GEx x
         cdef SymbolicSeries nex
