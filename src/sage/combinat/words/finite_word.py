8ad3823e336fbe97565525ce80bf957792bd819b
Frédéric Chapoton <chapoton@unistra.fr>
2025-05-01 13:01:04 +0200

    fix mistake

--- src/sage/combinat/words/finite_word.py
+++ src/sage/combinat/words/finite_word.py
@@ -1684,8 +1684,8 @@ class FiniteWord_class(Word_class):
             g.allow_loops(True)
             g.allow_multiple_edges(True)
             for v in l:
-                i = next(g.neighbors_in_iterator(v))
-                o = next(g.neighbors_out_iterator(v))
+                i = next(g.neighbor_in_iterator(v))
+                o = next(g.neighbor_out_iterator(v))
                 g.add_edge(i, o, g.edge_label(i, v)[0] * g.edge_label(v, o)[0])
                 g.delete_vertex(v)
         return g
