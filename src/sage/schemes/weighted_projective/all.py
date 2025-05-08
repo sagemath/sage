bb4b129364ed7f653bf1d30c86f858496b8844f2
Gareth Ma <grhkm21@gmail.com>
2025-05-08 10:47:22 +0100

    add basic weighted projective space functionality

--- src/sage/schemes/weighted_projective/all.py
+++ src/sage/schemes/weighted_projective/all.py
@@ -1,5 +1,5 @@
-"""
-all.py -- export of schemes to Sage
+"""nodoctest
+all.py -- export of projective schemes to Sage
 """
 
 # *****************************************************************************
@@ -20,30 +20,4 @@ all.py -- export of schemes to Sage
 #                  https://www.gnu.org/licenses/
 # *****************************************************************************
 
-from sage.schemes.jacobians.all import *
-
-from sage.schemes.hyperelliptic_curves.all import *
-
-from sage.schemes.curves.all import *
-
-from sage.schemes.plane_conics.all import *
-
-from sage.schemes.elliptic_curves.all import *
-
-from sage.schemes.plane_quartics.all import *
-
-from sage.schemes.generic.all import *
-
-from sage.schemes.toric.all import *
-
-from sage.schemes.affine.all import *
-
-from sage.schemes.projective.all import *
-
-from sage.schemes.product_projective.all import *
-
-from sage.schemes.weighted_projective.all import *
-
-from sage.schemes.cyclic_covers.all import *
-
-from sage.schemes.berkovich.all import *
+from sage.schemes.weighted_projective.weighted_projective_space import WeightedProjectiveSpace
---
+++ src/sage/schemes/weighted_projective/weighted_projective_curve.py
@@ -0,0 +1,25 @@
+from sage.schemes.curves.curve import Curve_generic
+from sage.schemes.weighted_projective.weighted_projective_space import WeightedProjectiveSpace_ring
+
+
+class WeightedProjectiveCurve(Curve_generic):
+    """
+    Curves in weighted projective spaces.
+
+    EXAMPLES:
+
+    We construct a hyperelliptic curve manually::
+
+        sage: P.<x, y, z> = WeightedProjectiveSpace([1, 3, 1], QQ)
+        sage: C = P.curve(y^2 - x^5 * z - 3 * x^2 * z^4 - 2 * z^6); C
+    """
+    def __init__(self, A, X, *kwargs):
+        # TODO ensure that A is the right type?
+        # Something like a `is_WeightProjectiveSpace` which means making a
+        # WeightProjectiveSpace class?
+        if not isinstance(A, WeightedProjectiveSpace_ring):
+            raise TypeError(f"A(={A}) is not a weighted projective space")
+        super().__init__(A, X, *kwargs)
+
+    def curve(self):
+        return
