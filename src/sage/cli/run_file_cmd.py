f949efc8ad76e654984947ac66b096f7bd1913a5
Antonio Rojas <arojas@archlinux.org>
2025-05-18 00:04:15 +0200

    cli: restore ability to run files from the command line

---
+++ src/sage/cli/run_file_cmd.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+
+from sage.cli.options import CliOptions
+from sage.repl.preparse import preparse_file_named
+from sage.all import sage_globals
+
+
+class RunFileCmd:
+    @staticmethod
+    def extend_parser(parser: argparse.ArgumentParser):
+        r"""
+        Extend the parser with the "run file" command.
+
+        INPUT:
+
+        - ``parsers`` -- the parsers to extend.
+
+        OUTPUT:
+
+        - the extended parser.
+        """
+        parser.add_argument(
+            "file",
+            nargs="?",
+            help="execute the given file as sage code",
+        )
+
+    def __init__(self, options: CliOptions):
+        r"""
+        Initialize the command.
+        """
+        self.options = options
+
+    def run(self) -> int:
+        r"""
+        Execute the given command.
+        """
+        input_file = preparse_file_named(self.options.file) if self.options.file.endswith('.sage') else self.options.file
+        try:
+            eval(compile(open(input_file, "rb").read(), input_file, "exec"), sage_globals())
+        except Exception as e:
+            print(f"An error occurred while executing the file: {e}")
+            return 1
+        return 0
