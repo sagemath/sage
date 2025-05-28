bec544c27242741fdec71ae5d757aedfcd3e676a
local-ring <lial0921.miu@gmail.com>
2025-05-28 00:16:27 -0400

    compute the result of s1*s2 directly to avoid the failure of test

--- src/sage/data_structures/blas_dict.pyx
+++ src/sage/data_structures/blas_dict.pyx
@@ -347,13 +347,13 @@ cpdef dict linear_combination(dict_factor_iter, bint factor_on_left=True):
         sage: s1 = SGA([2, 1, 3]) # (1 2)
         sage: s2 = SGA([3, 1, 2]) # (1 3)
         sage: D1 = {0: s1}
-        sage: blas.linear_combination([(D1, s2)], factor_on_left=False)
-        {0: s1 * s2}
+        sage: blas.linear_combination([(D1, s2)], factor_on_left=False) # s1 * s2
+        {0: [1, 3, 2]} 
 
     Check left multiplication with coefficients in a noncommutative ring::
     
-        sage: blas.linear_combination([(D1, s2)], factor_on_left=True)
-        {0: s2 * s1}
+        sage: blas.linear_combination([(D1, s2)], factor_on_left=True) # s2 * s1
+        {0: [3, 2, 1]} 
     """
     cdef dict result = {}
     cdef dict D
