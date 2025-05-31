ee21b0cb4982eb95c64078413fddf09e417dc29e
soon-haari <alstns.040422@gmail.com>
2025-05-31 12:33:20 +0900

    fix typo

--- src/sage/repl/preparse.py
+++ src/sage/repl/preparse.py
@@ -1190,7 +1190,7 @@ def preparse_numeric_literals(code, extract=False, quotes="'"):
         sage: preparse_numeric_literals('000042')
         'Integer(42)'
 
-    Cheeck that :issue:`40179` is fixed::
+    Check that :issue:`40179` is fixed::
 
         sage: preparse_numeric_literals("1" * 4300) == f"Integer({"1" * 4300})"
         True
