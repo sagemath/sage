f2ef3533ea943e4e543eb2502c6d83b3e0bf7d8b
kappybar <newton7233@outlook.jp>
2025-06-12 22:48:28 +0900

    change to shorter code

--- src/sage/graphs/path_enumeration.pyx
+++ src/sage/graphs/path_enumeration.pyx
@@ -735,7 +735,7 @@ def yen_k_shortest_simple_paths(self, source, target, weight_function=None,
          [1, 6, 9, 3, 4, 5],
          [1, 6, 9, 11, 10, 5]]
 
-    When s = t and report_edge=True and report_weight=True (issue 40247)::
+    When ``s == t`` and ``report_edge == True`` and ``report_weight == True`` (:issue:`40247`)::
 
         sage: g = DiGraph([(1, 2)])
         sage: list(yen_k_shortest_simple_paths(g, 1, 1, report_edges=True, report_weight=True))
@@ -747,16 +747,8 @@ def yen_k_shortest_simple_paths(self, source, target, weight_function=None,
         raise ValueError("vertex '{}' is not in the graph".format(target))
 
     if source == target:
-        if report_weight:
-            if report_edges:
-                yield (0, [])
-            else:
-                yield (0, [source])
-        else:
-            if report_edges:
-                yield []
-            else:
-                yield [source]
+        P = [] if report_edges else [source]
+        yield (0, P) if report_weight else P
         return
 
     if self.has_loops() or self.allows_multiple_edges():
