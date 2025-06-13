672959738b44a9e3dd13ca0f5ed83e25d9899b58
Antonio Rojas <arojas@archlinux.org>
2025-06-13 23:03:06 +0200

    Try to fix Windows CI

--- tools/update-meson.py
+++ tools/update-meson.py
@@ -79,7 +79,7 @@ def _symbol(val: str) -> SymbolNode:
 
 def update_python_sources(self: Rewriter, visitor: AstPython):
     for target in visitor.install_sources_calls:
-        ignored_files = {('src/sage/tests', 'cmdline.py')}
+        ignored_files = {'cmdline.py'}
         # Generate the current source list
         src_list: list[str] = []
         for arg in arg_list_from_node(target):
@@ -94,7 +94,7 @@ def update_python_sources(self: Rewriter, visitor: AstPython):
         to_append: list[StringNode] = []
         for file in python_files:
             file_name = file.name
-            if file_name in src_list or (str(folder), file_name) in ignored_files:
+            if file_name in src_list or file_name in ignored_files:
                 continue
             token = Token("string", target.filename, 0, 0, 0, None, file_name)
             to_append += [StringNode(token)]
