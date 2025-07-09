70d4ef1470257936bda5271f5360e935b8c762c0
Frédéric Chapoton <fchapoton2@gmail.com>
2025-07-09 10:31:47 +0200

    Update src/sage/combinat/posets/lattices.py
    Co-authored-by: Travis Scrimshaw <clfrngrown@aol.com>

--- src/sage/combinat/posets/lattices.py
+++ src/sage/combinat/posets/lattices.py
@@ -734,6 +734,7 @@ def LatticePoset(data=None, *args, **options):
                 error.x = P._vertex_to_element(error.x)
                 error.y = P._vertex_to_element(error.y)
                 raise
+    from sage.categories.posets import Posets
     cat = Posets().or_subcategory(options.get('category', None))
     cat = cat & FiniteLatticePosets()
     return FiniteLatticePoset(P, category=cat, facade=P._is_facade)
