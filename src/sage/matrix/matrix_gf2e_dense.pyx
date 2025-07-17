e5b8fd4bbe566bcd8f908944f9e9ffd880b16557
Nicholas Bell <Nicholas.Bell@etu.sorbonne-universite.fr>
2025-07-17 23:19:58 +0200

    add faster implementations of matrix_from_* constructors

--- src/sage/matrix/matrix_gf2e_dense.pyx
+++ src/sage/matrix/matrix_gf2e_dense.pyx
@@ -82,6 +82,7 @@ REFERENCES:
 #*****************************************************************************
 
 from cysignals.signals cimport sig_on, sig_off
+from cpython.sequence cimport PySequence_Fast
 
 cimport sage.matrix.matrix_dense as matrix_dense
 from sage.structure.element cimport Matrix
@@ -1346,6 +1347,101 @@ cdef class Matrix_gf2e_dense(matrix_dense.Matrix_dense):
         if ncols == 0 or nrows == 0:
             return A
         A._entries = mzed_submatrix(A._entries, self._entries, row, col, highr, highc)
+        return A
+
+    def matrix_from_rows(self, rows):
+        r"""
+        Return the matrix constructed from ``self`` using rows with indices in
+        the rows list.
+
+        EXAMPLES::
+
+            sage: M = matrix(GF(4),[[0,[0,1],0],[0,[1,1],1],[1,0,1]])
+            sage: M
+            [     0     z2      0]
+            [     0 z2 + 1      1]
+            [     1      0      1]
+            sage: M.matrix_from_rows([2,1])
+            [     1      0      1]
+            [     0 z2 + 1      1]
+        """
+        rows = PySequence_Fast(rows, "rows is not iterable")
+        if any(0 > row or row >= self._nrows for row in rows):
+            raise TypeError("Rows are not all in range")
+        
+        cdef Py_ssize_t nrows = len(rows)
+        
+        cdef Matrix_gf2e_dense A = self.new_matrix(nrows=nrows)
+        
+        cdef Py_ssize_t i, j, row
+        for i, row in enumerate(rows):
+            for j in range(self._ncols):
+                mzed_write_elem(A._entries, i, j, mzed_read_elem(self._entries, row, j))
+        return A
+
+    def matrix_from_columns(self, cols):
+        r"""
+        Return the matrix constructed from ``self`` using columns with indices
+        in the columns list.
+
+        EXAMPLES::
+
+            sage: M = matrix(GF(4),[[0,[0,1],0],[0,[1,1],1],[1,0,1]])
+            sage: M
+            [     0     z2      0]
+            [     0 z2 + 1      1]
+            [     1      0      1]
+            sage: M.matrix_from_columns([2,1])
+            [     0     z2]
+            [     1 z2 + 1]
+            [     1      0]
+        """
+        cols = PySequence_Fast(cols, "cols is not iterable")
+        if any(0 > col or col >= self._ncols for col in cols):
+            raise TypeError("Columns are not all in range")
+        
+        cdef Py_ssize_t ncols = len(cols)
+        
+        cdef Matrix_gf2e_dense A = self.new_matrix(ncols=ncols)
+        
+        cdef Py_ssize_t i, j, col
+        for i in range(self._nrows):
+            for j, col in enumerate(cols):
+                mzed_write_elem(A._entries, i, j, mzed_read_elem(self._entries, i, col))
+        return A
+
+    def matrix_from_rows_and_columns(self, rows, cols):
+        r"""
+        Return the matrix constructed from ``self`` from the given rows and
+        columns.
+
+        EXAMPLES::
+
+            sage: M = matrix(GF(4),[[0,[0,1],0],[0,[1,1],1],[1,0,1]])
+            sage: M
+            [     0     z2      0]
+            [     0 z2 + 1      1]
+            [     1      0      1]
+            sage: M.matrix_from_rows_and_columns([0,1],[2,1])
+            [     0     z2]
+            [     1 z2 + 1]
+        """
+        rows = PySequence_Fast(rows, "rows is not iterable")
+        cols = PySequence_Fast(cols, "cols is not iterable")
+        if any(0 > row or row >= self._nrows for row in rows):
+            raise TypeError("Rows are not all in range")
+        if any(0 > col or col >= self._ncols for col in cols):
+            raise TypeError("Columns are not all in range")
+        
+        cdef Py_ssize_t nrows = len(rows)
+        cdef Py_ssize_t ncols = len(cols)
+        
+        cdef Matrix_gf2e_dense A = self.new_matrix(nrows=nrows, ncols=ncols)
+        
+        cdef Py_ssize_t i, j, row, col
+        for i,row in enumerate(rows):
+            for j, col in enumerate(cols):
+                mzed_write_elem(A._entries, i, j, mzed_read_elem(self._entries, row, col))
         return A
 
     def rank(self):
