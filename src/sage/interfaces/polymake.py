34333638b380337e89a8c0570483c0785c8f7339
Chenxin Zhong <chenxin.zhong@outlook.com>
2025-07-21 20:59:23 +0800

    put the line "import polymake" before "polymake.cube(15)"

--- src/sage/interfaces/polymake.py
+++ src/sage/interfaces/polymake.py
@@ -446,8 +446,8 @@ class PolymakeAbstract(ExtraTabCompletion, Interface):
         TESTS::
 
             sage: # optional - jupymake
-            sage: c = polymake.cube(15)
             sage: from sage.interfaces.polymake import polymake
+            sage: c = polymake.cube(15)
             sage: polymake._available_vars = []
             sage: old = c._name
             sage: del c
