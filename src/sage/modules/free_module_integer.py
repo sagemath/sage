297a303f8f5a417e87a44f92525ee53067a1f463
Sahil Jain <jsahil730@gmail.com>
2025-08-13 16:21:50 +0530

    Some more changes

--- src/sage/modules/free_module_integer.py
+++ src/sage/modules/free_module_integer.py
@@ -907,12 +907,10 @@ class FreeModule_submodule_with_basis_integer(FreeModule_submodule_with_basis_pi
 
             \mathcal{H}(B) = \dfrac{det L}{\|v_1\| \|v_2\| \cdots \|v_n\|}^{\dfrac{1}{n}}
 
-
         The closer this ratio is to 1, the more orthogonal the basis is.
 
         INPUT:
-        - ``use_reduced_basis`` -- boolean (default: ``True``); uses reduced basis 
-          for computing the ratio
+        - ``use_reduced_basis`` -- boolean (default: ``True``); uses reduced basis for computing the ratio
 
         OUTPUT: the ratio described above.
 
