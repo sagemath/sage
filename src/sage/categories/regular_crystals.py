f64192b5c8ce941e4e8d4774452fa459b3cb97fc
Martin Rubey <martin.rubey@tuwien.ac.at>
2025-08-19 23:27:06 +0200

    adapt verbose output of TestSuite.run in categories

--- src/sage/categories/regular_crystals.py
+++ src/sage/categories/regular_crystals.py
@@ -82,6 +82,8 @@ class RegularCrystals(Category_singleton):
         running ._test_new() . . . pass
         running ._test_not_implemented_methods() . . . pass
         running ._test_pickling() . . . pass
+        running ._test_random() . . . pass
+        running ._test_rank() . . . pass
         running ._test_some_elements() . . . pass
         running ._test_stembridge_local_axioms() . . . pass
     """
