6dac5b0922c289ad18f209a30c4bb54c060fc6bc
Frédéric Chapoton <chapoton@unistra.fr>
2025-09-13 20:58:27 +0200

    various details in algebras, categories

--- src/sage/categories/highest_weight_crystals.py
+++ src/sage/categories/highest_weight_crystals.py
@@ -112,7 +112,7 @@ class HighestWeightCrystals(Category_singleton):
     class ParentMethods:
 
         @cached_method
-        def highest_weight_vectors(self):
+        def highest_weight_vectors(self) -> tuple:
             r"""
             Return the highest weight vectors of ``self``.
 
@@ -672,7 +672,7 @@ class HighestWeightCrystals(Category_singleton):
             Implement operations on tensor products of crystals.
             """
             @cached_method
-            def highest_weight_vectors(self):
+            def highest_weight_vectors(self) -> tuple:
                 r"""
                 Return the highest weight vectors of ``self``.
 
