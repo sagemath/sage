f37f15c3eeab8e743c3b1e60e7cc7be1dcf1c24b
Michael Orlitzky <michael@orlitzky.com>
2025-09-14 12:11:32 -0400

    src/sage/stats/distributions/discrete_gaussian_lattice.py: more long time
    Add "# long time" to two _normalisation_factor_zz() examples. These
    take ~30s on one of my machines, and ~50s on another. Although they
    run fast enough on the CI, I think it is reasonable to mark them as
    taking a long time -- they are the two slowest examples in this file
    for me.

--- src/sage/stats/distributions/discrete_gaussian_lattice.py
+++ src/sage/stats/distributions/discrete_gaussian_lattice.py
@@ -254,7 +254,7 @@ class DiscreteGaussianDistributionLatticeSampler(SageObject):
 
             sage: Sigma = Matrix(ZZ, [[5, -2, 4], [-2, 10, -5], [4, -5, 5]])
             sage: D = DGL(ZZ^3, Sigma, [7, 2, 5])
-            sage: D._normalisation_factor_zz()
+            sage: D._normalisation_factor_zz()  # long time
             78.6804...
 
             sage: M = Matrix(ZZ, [[1, 3, 0], [-2, 5, 1]])
@@ -489,14 +489,15 @@ class DiscreteGaussianDistributionLatticeSampler(SageObject):
             sage: c = vector(ZZ, [7, 2, 5])
             sage: D = distributions.DiscreteGaussianDistributionLatticeSampler(ZZ^n, Sigma, c)
             sage: f = D.f
-            sage: nf = D._normalisation_factor_zz(); nf # This has not been properly implemented
+            sage: # This has not been properly implemented...
+            sage: nf = D._normalisation_factor_zz(); nf  # long time
             78.6804...
 
         We can compute the expected number of samples before sampling a vector::
 
             sage: v = vector(ZZ, n, (11, 4, 8))
             sage: v.set_immutable()
-            sage: 1 / (f(v) / nf)
+            sage: 1 / (f(v) / nf)  # long time
             2553.9461...
 
             sage: counter = defaultdict(Integer); m = 0
