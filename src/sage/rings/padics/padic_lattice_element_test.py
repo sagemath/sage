0f420461b99fa1a44be5735099edbb645a313e6d
Michael Orlitzky <michael@orlitzky.com>
2025-10-04 09:23:25 -0400

    sage.rings.padics.padic_lattice_element_test: parameterize tests
    Use parameterized tests and fixtures instead of an internal "for" loop
    to improve the reporting a bit.

--- src/sage/rings/padics/padic_lattice_element_test.py
+++ src/sage/rings/padics/padic_lattice_element_test.py
@@ -1,27 +1,65 @@
 import pytest
-import warnings
 
 
-def test_padic_lattice_element():
+@pytest.fixture
+def R1():
+    from warnings import catch_warnings, filterwarnings
+    from sage.rings.padics.factory import ZpLC
+    with catch_warnings(category=FutureWarning):
+        filterwarnings("ignore", category=FutureWarning)
+        return ZpLC(2)
+
+
+@pytest.fixture
+def R2():
+    from warnings import catch_warnings, filterwarnings
+    from sage.rings.padics.factory import ZpLF
+    with catch_warnings(category=FutureWarning):
+        filterwarnings("ignore", category=FutureWarning)
+        return ZpLF(2)
+
+
+@pytest.fixture
+def R3():
+    from warnings import catch_warnings, filterwarnings
+    from sage.rings.padics.factory import QpLC
+    with catch_warnings(category=FutureWarning):
+        filterwarnings("ignore", category=FutureWarning)
+        return QpLC(2)
+
+
+@pytest.fixture
+def R4():
+    from warnings import catch_warnings, filterwarnings
+    from sage.rings.padics.factory import QpLF
+    with catch_warnings(category=FutureWarning):
+        filterwarnings("ignore", category=FutureWarning)
+        return QpLF(2)
+
+
+# Use strings for the fixture names here, and then later convert them
+# to the actual fixture objects using request.getfixturevalue(). This
+# is a workaround for being unable to pass fixtures directly as
+# parameters:
+#
+#   https://github.com/pytest-dev/pytest/issues/349
+#
+elements = ( "R1", "R2", "R3", "R4" )
+
+
+@pytest.mark.parametrize("e", elements)
+def test_padic_lattice_element(e, request):
     r"""
     Run the ``TestSuite()`` for some examples that previously
     lived in the TESTS:: block of the padic_lattice_element module.
     """
     from sage.misc.sage_unittest import TestSuite
-    from sage.rings.padics.factory import ZpLC, ZpLF, QpLC, QpLF
 
-    with warnings.catch_warnings(category=FutureWarning):
-        warnings.filterwarnings("ignore", category=FutureWarning)
-        # These all raise FutureWarnings
-        R1 = ZpLC(2)
-        R2 = ZpLF(2)
-        R3 = QpLC(2)
-        R4 = QpLF(2)
+    # Convert the string to a real fixture
+    e = request.getfixturevalue(e)
 
-    for R in (R1, R2, R3, R4):
-        # Only do a few runs, _test_matrix_smith() in particular is
-        # sloooooow.
-        TestSuite(R).run(verbose=True,
-                         raise_on_failure=True,
-                         skip="_test_teichmuller",
-                         max_runs=8)
+    # Only do a few runs, _test_matrix_smith() in particular is slow.
+    TestSuite(e).run(verbose=True,
+                     raise_on_failure=True,
+                     skip="_test_teichmuller",
+                     max_runs=8)
