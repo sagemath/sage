546c87fda220db0fccde597290ec566d354b4505
Michael Orlitzky <michael@orlitzky.com>
2025-10-04 09:31:17 -0400

    sage.rings.valuation.mapped_valuation_test: parameterize tests
    Use parameterized tests and fixtures to improve the reporting from
    these tests.

--- src/sage/rings/valuation/mapped_valuation_test.py
+++ src/sage/rings/valuation/mapped_valuation_test.py
@@ -1,12 +1,8 @@
 import pytest
 
 
-def test_finite_extension_from_limit_valuation():
-    r"""
-    Run the ``TestSuite()`` for two examples given in the
-    ``FiniteExtensionFromLimitValuation`` documentation.
-    """
-    from sage.misc.sage_unittest import TestSuite
+@pytest.fixture
+def w():
     from sage.rings.function_field.constructor import FunctionField
     from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
     from sage.rings.integer import Integer
@@ -18,10 +14,42 @@ def test_finite_extension_from_limit_valuation():
     y = R.gen()
     L = K.extension(y**2 - x)
     v = K.valuation(Integer(1))
-    w = v.extensions(L)
 
-    for ext in w:
-        # fewer max_runs, these are kind of slow
-        TestSuite(ext).run(verbose=True,
-                           raise_on_failure=True,
-                           max_runs=512)
+    return v.extensions(L)
+
+
+@pytest.fixture
+def w0(w):
+    return w[0]
+
+
+@pytest.fixture
+def w1(w):
+    return w[1]
+
+
+# Use strings for the fixture names here, and then later convert them
+# to the actual fixture objects using request.getfixturevalue(). This
+# is a workaround for being unable to pass fixtures directly as
+# parameters:
+#
+#   https://github.com/pytest-dev/pytest/issues/349
+#
+extensions = ( "w0", "w1" )
+
+
+@pytest.mark.parametrize("e", extensions)
+def test_finite_extension_from_limit_valuation(e, request):
+    r"""
+    Run the ``TestSuite()`` for two examples given in the
+    ``FiniteExtensionFromLimitValuation`` documentation.
+    """
+    from sage.misc.sage_unittest import TestSuite
+
+    # Convert the string to a real fixture
+    e = request.getfixturevalue(e)
+
+    # fewer max_runs, these are kind of slow
+    TestSuite(e).run(verbose=True,
+                     raise_on_failure=True,
+                     max_runs=512)
