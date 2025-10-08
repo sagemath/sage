a424b8e3ecc226480e417c1e357a5ca81a3107af
Michael Orlitzky <michael@orlitzky.com>
2025-10-08 12:53:56 -0400

    sage.rings.valuation.mapped_valuation_test: eliminate boilerplate
    The one test in this file is easy to parameterize based on the index
    (an int) of a fixture supplied to it. This allows us to eliminate a
    lot of the boilerplate associated with fixture parameterizations.

--- src/sage/rings/valuation/mapped_valuation_test.py
+++ src/sage/rings/valuation/mapped_valuation_test.py
@@ -18,38 +18,15 @@ def w():
     return v.extensions(L)
 
 
-@pytest.fixture
-def w0(w):
-    return w[0]
-
-
-@pytest.fixture
-def w1(w):
-    return w[1]
-
-
-# Use strings for the fixture names here, and then later convert them
-# to the actual fixture objects using request.getfixturevalue(). This
-# is a workaround for being unable to pass fixtures directly as
-# parameters:
-#
-#   https://github.com/pytest-dev/pytest/issues/349
-#
-extensions = ( "w0", "w1" )
-
-
-@pytest.mark.parametrize("e", extensions)
-def test_finite_extension_from_limit_valuation(e, request):
+@pytest.mark.parametrize("idx", [0,1])
+def test_finite_extension_from_limit_valuation_w(w, idx):
     r"""
     Run the ``TestSuite()`` for two examples given in the
     ``FiniteExtensionFromLimitValuation`` documentation.
     """
     from sage.misc.sage_unittest import TestSuite
 
-    # Convert the string to a real fixture
-    e = request.getfixturevalue(e)
-
     # fewer max_runs, these are kind of slow
-    TestSuite(e).run(verbose=True,
-                     raise_on_failure=True,
-                     max_runs=512)
+    TestSuite(w[idx]).run(verbose=True,
+                          raise_on_failure=True,
+                          max_runs=512)
