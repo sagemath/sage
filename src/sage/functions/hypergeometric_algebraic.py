139ddce87d8e0f2fb53f6243f725de3756eab15c
Xavier Caruso <xavier@caruso.ovh>
2025-10-27 14:44:11 +0100

    p-curvature and Dwork relations

--- src/sage/functions/hypergeometric_algebraic.py
+++ src/sage/functions/hypergeometric_algebraic.py
@@ -1,6 +1,8 @@
 r"""
 Algebraic properties of hypergeometric functions.
 
+[Tutorial]
+
 AUTHORS:
 
 - Xavier Caruso, Florian Fürnsinn (2025-10): initial version
@@ -24,6 +26,8 @@ from sage.rings.padics.factory import QpFP
 from sage.categories.pushout import pushout
 from sage.categories.finite_fields import FiniteFields
 
+from sage.matrix.constructor import matrix
+
 from sage.rings.polynomial.polynomial_ring_constructor import PolynomialRing
 from sage.rings.power_series_ring import PowerSeriesRing
 from sage.rings.polynomial.ore_polynomial_ring import OrePolynomialRing
@@ -32,12 +36,47 @@ from sage.rings.polynomial.ore_polynomial_ring import OrePolynomialRing
 def mod2(x, d):
     return d - (-x) % d
 
-
 class Parameters():
-    def __init__(self, alpha, beta, is_one_included=False):
+    r"""
+    Class for parameters of hypergeometric functions.
+    """
+    def __init__(self, alpha, beta, add_one=True):
+        r"""
+        Initialize this set of parameters.
+
+        INPUT:
+
+        - ``alpha`` -- list of top parameters
+
+        - ``beta`` -- list of bottom parameters
+
+        - ``add_one`` -- boolean (default: ``True``),
+          if ``True``, add an additional one to the bottom
+          parameters.
+
+        EXAMPLES::
+
+            sage: from sage.functions.hypergeometric_algebraic import Parameters
+            sage: p = Parameters([1/4, 1/3, 1/2], [2/5, 3/5])
+            sage: p
+            ([1/4, 1/3, 1/2], [2/5, 3/5, 1])
+            sage: type(p)
+            <class 'sage.functions.hypergeometric_algebraic.Parameters'>
+
+        By default, parameters are sorted, duplicates are removed and
+        a trailing `1` is added to the bottom parameters::
+
+            sage: Parameters([1/2, 1/3, 2/3], [2/3])
+            ([1/3, 1/2], [1])
+
+        We can avoid adding the trailing `1` by passing ``add_one=False``)::
+
+            sage: Parameters([1/2, 1/3, 2/3], [2/3], add_one=False)
+            ([1/3, 1/2], [])
+        """
         try:
-            alpha = sorted([QQ(a) for a in alpha])
-            beta = sorted([QQ(b) for b in beta])
+            alpha = sorted([QQ(a) for a in alpha if a is not None])
+            beta = sorted([QQ(b) for b in beta if b is not None])
         except TypeError:
             raise NotImplementedError("parameters must be rational numbers")
         i = j = 0
@@ -49,7 +88,7 @@ class Parameters():
                 j += 1
             else:
                 i += 1
-        if not is_one_included:
+        if add_one:
             beta.append(QQ(1))
         self.alpha = alpha
         self.beta = beta
@@ -61,15 +100,56 @@ class Parameters():
                         + [ b.denominator() for b in beta ])
             self.bound = 2 * self.d * max(alpha + beta) + 1
 
+    def __repr__(self):
+        r"""
+        Return a string representation of these parameters.
+
+        EXAMPLES::
+
+            sage: from sage.functions.hypergeometric_algebraic import Parameters
+            sage: p = Parameters([1/4, 1/3, 1/2], [2/5, 3/5])
+            sage: p  # indirect doctest
+            ([1/4, 1/3, 1/2], [2/5, 3/5, 1])
+        """
+        return "(%s, %s)" % (self.alpha, self.beta)
+
     def is_balanced(self):
+        r"""
+        Return ``True`` if there are as many top parameters as bottom
+        parameters; ``False`` otherwise.
+
+        EXAMPLES::
+
+            sage: from sage.functions.hypergeometric_algebraic import Parameters
+            sage: p = Parameters([1/4, 1/3, 1/2], [2/5, 3/5])
+            sage: p
+            ([1/4, 1/3, 1/2], [2/5, 3/5, 1])
+            sage: p.is_balanced()
+            True
+
+        ::
+
+            sage: p = Parameters([1/4, 1/3, 1/2], [2/5, 3/5], add_one=False)
+            sage: p
+            ([1/4, 1/3, 1/2], [2/5, 3/5])
+            sage: p.is_balanced()
+            False
+        """
         return len(self.alpha) == len(self.beta)
 
     @cached_method
-    def parenthesis_criterion(self, c):
+    def christol_sorting(self, c=1):
+        r"""
+        """
         d = self.d
-        A = [(mod2(d*c*a, d), -a, 1) for a in self.alpha]
-        B = [(mod2(d*c*b, d), -b, -1) for b in self.beta]
-        AB = sorted(A + B)
+        def mod2(x):
+            return d - (-x) % d
+        A = [mod2(d*c*a), -a, 1) for a in self.alpha]
+        B = [mod2(d*c*b), -b, -1) for b in self.beta]
+        return sorted(A + B)
+
+    def parenthesis_criterion(self, c):
+        AB = self.christol_sorting(c)
         parenthesis = 0
         previous_paren = -1
         for _, _, paren in AB:
@@ -79,12 +159,8 @@ class Parameters():
             previous_paren = paren
         return parenthesis >= 0
 
-    @cached_method
     def interlacing_criterion(self, c):
-        d = self.d
-        A = [(mod2(d*c*a, d), 1) for a in self.alpha]
-        B = [(mod2(d*c*b, d), -1) for b in self.beta]
-        AB = sorted(A + B)
+        AB = self.christol_sorting(c)
         previous_paren = -1
         for _, paren in AB:
             if paren == previous_paren:
@@ -105,9 +181,7 @@ class Parameters():
             if alpha[i] is not None and beta[j] is not None:
                 alpha[i] = None
                 beta[j] = None
-        alpha = [a for a in alpha if a is not None]
-        beta = [b for b in beta if b is not None]
-        return Parameters(alpha, beta, is_one_included=True)
+        return Parameters(alpha, beta, add_one=False)
 
     def has_negative_integer_differences(self):
         return any(a - b in ZZ and a < b for a in self.alpha for b in self.beta)
@@ -133,7 +207,7 @@ class HypergeometricAlgebraic(SageObject, metaclass=ClasscallMetaclass):
                 return HypergeometricAlgebraic_QQ(parameters, x, scalar)
             return HypergeometricAlgebraic_charzero(parameters, x, scalar)
         else:
-            raise NotImplementedError
+            raise NotImplementedError("the base must have characteristic zero or be a finite field")
 
     def __init__(self, parameters, x, scalar=None):
         self._base = base = x.base_ring()
@@ -148,6 +222,7 @@ class HypergeometricAlgebraic(SageObject, metaclass=ClasscallMetaclass):
             self._parameters = None
         else:
             self._parameters = parameters
+        self._S = S
         self._x = S.gen()
         self._variable_name = S.variable_name()
 
@@ -207,10 +282,12 @@ class HypergeometricAlgebraic_charzero(HypergeometricAlgebraic):
 
 
 class HypergeometricAlgebraic_QQ(HypergeometricAlgebraic_charzero):
-    def reduce(self, p):
+    def mod(self, p):
         F = FiniteField(p)
         x = F['x'].gen()
         return HypergeometricAlgebraic(self._parameters, x, self._scalar)
+
+    __mod__ = mod
 
     def has_good_reduction(self, p):
         h = self.reduce(p)
@@ -240,8 +317,8 @@ class HypergeometricAlgebraic_QQ(HypergeometricAlgebraic_charzero):
 
 
 class HypergeometricAlgebraic_GFp(HypergeometricAlgebraic):
-    def __init__(self, alpha, beta, x, scalar=None):
-        HypergeometricAlgebraic.__init__(self, alpha, beta, x, scalar)
+    def __init__(self, parameters, x, scalar=None):
+        HypergeometricAlgebraic.__init__(self, parameters, x, scalar)
         self._p = self._base.cardinality()
 
     def is_defined(self):
@@ -260,7 +337,7 @@ class HypergeometricAlgebraic_GFp(HypergeometricAlgebraic):
         bound = self._parameters.bound
         if bound < p:
             return True
-        prec = 1 + p ** ceil(log(self._bound(), p))
+        prec = 1 + p ** ceil(log(self._parameters.bound, p))
         try:
             self.series(prec)
         except ValueError:
@@ -270,14 +347,14 @@ class HypergeometricAlgebraic_GFp(HypergeometricAlgebraic):
     def series(self, prec):
         S = PowerSeriesRing(self._base, name=self._variable_name)
         p = self._p
-        pprec = max(1, (len(self._beta) + 1) * ceil(log(prec, p)))
+        pprec = max(1, (len(self.beta()) + 1) * ceil(log(prec, p)))
         K = QpFP(p, pprec)
         c = K(self._scalar)
         coeffs = [c]
         for i in range(prec-1):
             for a in self.alpha():
                 c *= a + i
-            for b in self._beta():
+            for b in self.beta():
                 c /= b + i
             c /= i + 1
             if c.valuation() < 0:
@@ -287,3 +364,67 @@ class HypergeometricAlgebraic_GFp(HypergeometricAlgebraic):
 
     def is_algebraic(self):
         return self.is_defined()
+
+    def p_curvature(self):
+        L = self.differential_operator()
+        K = L.base_ring().fraction_field()
+        S = OrePolynomialRing(K, L.parent().twisting_derivation().extend_to_fraction_field(), names='d')
+        L = S(L.list())
+        d = S.gen()
+        p = self._p
+        rows = [ ]
+        n = L.degree()
+        for i in range(p, p + n):
+            Li = d**i % L
+            rows.append([Li[j] for j in range(n)])
+        return matrix(rows)
+
+    def dwork_relation(self):
+        r"""
+        Return (P1, g1), ..., (Ps, gs) such that
+
+            h = P1*g1^p + ... + Ps*gs^p
+        """
+        if not self._parameters.is_balanced():
+            raise ValueError("the hypergeometric function must be a pFq with q = p-1")
+        if not self.is_defined():
+            raise ValueError("this hypergeometric function is not defined")
+
+        # We compute the series expansion up to x^p
+        p = self._p
+        S = self._S
+        cs = self.series(p).list()
+
+        # We compute the relevant exponents
+        exponents = sorted([(1-b) % p for b in self._parameters.beta])
+        exponents.append(p)
+        Ps = []
+        for i in range(len(exponents) - 1):
+            e = exponents[i]
+            if e < len(cs) and cs[e]:
+                P = S(cs[e:exponents[i+1]]) << e
+                Ps.append((e, P))
+
+        # We compute the hypergeometric series
+        Hs = [ ]
+        for r, P in Ps:
+            alpha = [ ]
+            for a in self.alpha():
+                ap = (a + (-a) % p) / p  # Dwork map
+                ar = prod(a + i for i in range(r))
+                if ar % p == 0:
+                    alpha.append(ap + 1)
+                else:
+                    alpha.append(ap)
+            beta = [ ]
+            for b in self.beta():
+                bp = (b + (-b) % p) / p  # Dwork map
+                br = prod(b + i for i in range(r))
+                if br % p == 0:
+                    beta.append(bp + 1)
+                else:
+                    beta.append(bp)
+            parameters = Parameters(alpha, beta)
+            Hs.append((P, HypergeometricAlgebraic_GFp(parameters, self._x)))
+
+        return Hs
