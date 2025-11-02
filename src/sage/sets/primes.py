fd9b973f9480cf1d8b650b0e8c2116e1c5bb58d0
Xavier Caruso <xcaruso@users.noreply.github.com>
2025-11-02 19:38:39 +0100

    Update src/sage/sets/primes.py
    Co-authored-by: Martin Rubey <axiomize@yahoo.de>

--- src/sage/sets/primes.py
+++ src/sage/sets/primes.py
@@ -1016,8 +1016,7 @@ class Primes(Set_generic, UniqueRepresentation):
 
     def is_supset(self, other, almost=False):
         r"""
-        Return ``True`` if this set of is supset of ``other``;
-        ``False`` otherwise.
+        Return whether this set contains the set ``other`` as a subset.
 
         INPUT:
 
