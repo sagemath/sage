3f9ea6f2068d34bc5ca438921dfa5cc0c9e49e12
mjl036 <94404566+mjl036@users.noreply.github.com>
2025-11-12 10:58:34 -0600

    Final changes for Set_Theory_Learning

--- gui/SetTheory/SetTheoryQuestions.py
+++ gui/SetTheory/SetTheoryQuestions.py
@@ -8,6 +8,6 @@ def randomQuestion():
              4:"Given A = <setA>, B = <setB>, C = <setC>, find\na.) A Δ B\nb.) A ∪ (B ∩ C)\nc.) A ∩ (B ∪ C).",
              5:"Given the set A = <setA>, determine if the family of sets F = {<setB>, <setC>, <setD>} form a partition of A.",
              6:"Given A = <setA> and B = <setB>, find\na.) A\\B\nb.)B\\A.",
-             7:"Given A = <setA> and B ⊆ A and C ⊆ A where B = <setB> and C = <setC>, find\na.) B'\nb.) C'\n Answer with two sets separated by a semicolon (;)."}
+             7:"Given A = <setA> and B ⊆ A and C ⊆ A where B = <setB> and C = <setC>, find\na.) B'\nb.) C'."}
     q_num = random.choice(list(Questions.keys()))
     return q_num, Questions[q_num]
