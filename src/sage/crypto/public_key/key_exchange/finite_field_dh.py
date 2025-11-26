69d31c07a81f11fa1a6e9c4cdbb3ca06dc379eb1
Vincent Macri <vincent.macri@ucalgary.ca>
2025-11-26 13:10:12 -0700

    Docstring cleanup

--- src/sage/crypto/public_key/key_exchange/finite_field_dh.py
+++ src/sage/crypto/public_key/key_exchange/finite_field_dh.py
@@ -1,4 +1,6 @@
-from sage.crypto.public_key.key_exchange.key_exchange_base import CommutativeKeyExchangeBase
+from sage.crypto.public_key.key_exchange.key_exchange_base import (
+    CommutativeKeyExchangeBase,
+)
 from sage.misc.prandom import randint
 from sage.rings.finite_rings.finite_field_constructor import GF
 
--- src/sage/crypto/public_key/key_exchange/key_exchange_base.py
+++ src/sage/crypto/public_key/key_exchange/key_exchange_base.py
@@ -5,7 +5,7 @@ from sage.structure.sage_object import SageObject
 
 
 class KeyExchangeBase(SageObject, ABC):
-    """
+    r"""
     An Base class for Public Key Exchange Schemes.
 
     Implementors of this class must give implementations
@@ -22,20 +22,20 @@ class KeyExchangeBase(SageObject, ABC):
 
     @abstractmethod
     def alice_secret_key(self):
-        """
-        Generate a valid secret key for Alice
+        r"""
+        Generate a valid secret key for Alice.
         """
         raise NotImplementedError
 
     @abstractmethod
     def alice_public_key(self, alice_secret_key):
-        """
-        Generate a valid public key for Alice
+        r"""
+        Generate a valid public key for Alice.
 
         INPUT:
 
         - ``alice_secret_key``: Alice's secret key that will be used to generate
-            the public key.
+            the public key
 
         OUTPUT:
 
@@ -45,20 +45,20 @@ class KeyExchangeBase(SageObject, ABC):
 
     @abstractmethod
     def bob_secret_key(self):
-        """
-        Generate a valid secret key for Bob
+        r"""
+        Generate a valid secret key for Bob.
         """
         raise NotImplementedError
 
     @abstractmethod
     def bob_public_key(self, bob_secret_key):
-        """
-        Generate a valid public key for Bob
+        r"""
+        Generate a valid public key for Bob.
 
         INPUT:
 
         - ``bob_secret_key``: Bob's secret key that will be used to generate
-            the public key.
+            the public key
 
         OUTPUT:
 
@@ -68,14 +68,14 @@ class KeyExchangeBase(SageObject, ABC):
 
     @abstractmethod
     def alice_compute_shared_secret(self, alice_secret_key, bob_public_key):
-        """
-        Compute Alice's shared secret
+        r"""
+        Compute Alice's shared secret.
 
         INPUT:
 
-        - ``alice_secret_key``: Alice's secret key that is kept secret from all parties.
+        - ``alice_secret_key``: Alice's secret key that is kept secret from all parties
 
-        - ``bob_public_key``: Bob's public key that has been sent to Alice.
+        - ``bob_public_key``: Bob's public key that has been sent to Alice
 
         OUTPUT:
 
@@ -86,14 +86,14 @@ class KeyExchangeBase(SageObject, ABC):
 
     @abstractmethod
     def bob_compute_shared_secret(self, bob_secret_key, alice_public_key) -> Any:
-        """
-        Compute Bob's shared secret
+        r"""
+        Compute Bob's shared secret.
 
         INPUT:
 
-        - ``bob_secret_key``: Bob's secret key that is kept secret from all parties.
+        - ``bob_secret_key``: Bob's secret key that is kept secret from all parties
 
-        - ``alice_public_key``: Alice's public key that has been sent to Bob.
+        - ``alice_public_key``: Alice's public key that has been sent to Bob
 
         OUTPUT:
 
@@ -103,9 +103,9 @@ class KeyExchangeBase(SageObject, ABC):
         raise NotImplementedError
 
     def alice_key_generate(self) -> tuple[Any, Any]:
-        """
+        r"""
         Generate a valid (secret key, public key) pair for Alice's
-        key exchange
+        key exchange.
 
         OUTPUT:
             A two tuple (secret_key, public_key) which is Alice's
@@ -116,9 +116,9 @@ class KeyExchangeBase(SageObject, ABC):
         return (alice_sk, alice_pk)
 
     def bob_key_generate(self) -> tuple[Any, Any]:
-        """
+        r"""
         Generate a valid (secret key, public key) pair for Bob's
-        key exchange
+        key exchange.
 
         OUTPUT:
             A 2-tuple (secret_key, public_key) which is Bob's
@@ -129,14 +129,14 @@ class KeyExchangeBase(SageObject, ABC):
         return (bob_sk, bob_pk)
 
     def do_key_exchange(self) -> tuple[Any, Any, Any, Any, Any]:
-        """
+        r"""
         Do a full key exchange and returns all public keys, secret keys,
         and the computed shared secret between Alice and Bob. Raises
         an AssertException if the computed shared secret between Alice
         and Bob are not the same.
 
         OUTPUT:
-            A 5-tuple (alice_secret_key, alice_public_key, bob_secret_key, bob_public_key, shared_secret).
+            A 5-tuple (alice_secret_key, alice_public_key, bob_secret_key, bob_public_key, shared_secret)
         """
         alice_sk, alice_pk = self.alice_key_generate()
         bob_sk, bob_pk = self.bob_key_generate()
@@ -145,36 +145,35 @@ class KeyExchangeBase(SageObject, ABC):
         return (alice_sk, alice_pk, bob_sk, bob_pk, alice_shared_secret)
 
     def _test_key_exchange(self, **options):
-        """
-        Tests the key exchange generates the same shared secrets for both parties
+        r"""
+        Tests the key exchange generates the same shared secrets for both parties.
         """
         tester = self._tester(**options)
-        for _ in range(5):
-            alice_sk, alice_pk = self.alice_key_generate()
-            bob_sk, bob_pk = self.bob_key_generate()
-            alice_shared_secret = self.alice_compute_shared_secret(alice_sk, bob_pk)
-            bob_shared_secret = self.bob_compute_shared_secret(bob_sk, alice_pk)
-            tester.assertEqual(alice_shared_secret, bob_shared_secret)
+        alice_sk, alice_pk = self.alice_key_generate()
+        bob_sk, bob_pk = self.bob_key_generate()
+        alice_shared_secret = self.alice_compute_shared_secret(alice_sk, bob_pk)
+        bob_shared_secret = self.bob_compute_shared_secret(bob_sk, alice_pk)
+        tester.assertEqual(alice_shared_secret, bob_shared_secret)
 
 
 class CommutativeKeyExchangeBase(KeyExchangeBase):
-    """
-    A base class for Key Exchange schemes where alice and bob perform
-    the same computations for generating public/secret keys and the
-    shared secret key
+    r"""
+    A base class for key exchange schemes such as Diffie-Hellman where Alice
+    and Bob perform the same computations for generating public/secret keys and
+    the shared secret key.
     """
 
     @abstractmethod
     def secret_key(self) -> Any:
-        """
-        Generate a secret key for the key exchange
+        r"""
+        Generate a secret key for the key exchange.
         """
         raise NotImplementedError
 
     @abstractmethod
     def public_key(self, secret_key) -> Any:
-        """
-        Generate a public key for the secret key that you have chosen
+        r"""
+        Generate a public key for the secret key that you have chosen.
 
         INPUT:
             - ``secret_key``: A secret key that has been chosen beforehand
@@ -183,8 +182,8 @@ class CommutativeKeyExchangeBase(KeyExchangeBase):
 
     @abstractmethod
     def compute_shared_secret(self, secret_key, public_key) -> Any:
-        """
-        Generate the computed shared secret
+        r"""
+        Generate the computed shared secret.
 
         INPUT:
             - ``secret_key``: A secret key that has been chosen beforehand
@@ -196,19 +195,19 @@ class CommutativeKeyExchangeBase(KeyExchangeBase):
         raise NotImplementedError
 
     def alice_secret_key(self) -> Any:
-        """
-        Generate a valid secret key for Alice
+        r"""
+        Generate a valid secret key for Alice.
         """
         return self.secret_key()
 
     def alice_public_key(self, alice_secret_key) -> Any:
-        """
-        Generate a valid public key for Alice
+        r"""
+        Generate a valid public key for Alice.
 
         INPUT:
 
         - ``alice_secret_key``: Alice's secret key that will be used to generate
-            the public key.
+            the public key
 
         OUTPUT:
 
@@ -217,19 +216,19 @@ class CommutativeKeyExchangeBase(KeyExchangeBase):
         return self.public_key(alice_secret_key)
 
     def bob_secret_key(self) -> Any:
-        """
-        Generate a valid secret key for Bob
+        r"""
+        Generate a valid secret key for Bob.
         """
         return self.secret_key()
 
     def bob_public_key(self, bob_secret_key) -> Any:
-        """
-        Generate a valid public key for Bob
+        r"""
+        Generate a valid public key for Bob.
 
         INPUT:
 
         - ``bob_secret_key``: Bob's secret key that will be used to generate
-            the public key.
+            the public key
 
         OUTPUT:
 
@@ -239,13 +238,13 @@ class CommutativeKeyExchangeBase(KeyExchangeBase):
 
     def alice_compute_shared_secret(self, alice_sk, bob_pk) -> Any:
         """
-        Compute Alice's shared secret
+        Compute Alice's shared secret.
 
         INPUT:
 
-        - ``alice_secret_key``: Alice's secret key that is kept secret from all parties.
+        - ``alice_secret_key``: Alice's secret key that is kept secret from all parties
 
-        - ``bob_public_key``: Bob's public key that has been sent to Alice.
+        - ``bob_public_key``: Bob's public key that has been sent to Alice
 
         OUTPUT:
 
@@ -255,20 +254,17 @@ class CommutativeKeyExchangeBase(KeyExchangeBase):
         return self.compute_shared_secret(alice_sk, bob_pk)
 
     def bob_compute_shared_secret(self, bob_sk, alice_pk) -> Any:
-        """
-        Compute Bob's shared secret
+        r"""
+        Compute Bob's shared secret.
 
         INPUT:
 
-        - ``bob_secret_key``: Bob's secret key that is kept secret from all parties.
+        - ``bob_secret_key``: Bob's secret key that is kept secret from all parties
 
-        - ``alice_public_key``: Alice's public key that has been sent to Bob.
+        - ``alice_public_key``: Alice's public key that has been sent to Bob
 
         OUTPUT:
 
         - The secret key that is shared between Alice and Bob
         """
         return self.compute_shared_secret(bob_sk, alice_pk)
-
-    def __dir__(self):
-        return ['secret_key', 'public_key', 'compute_shared_secret']
