d245ceb7d34e37383ec5b6965d3e7d22fdf8a291
user202729 <25191436+user202729@users.noreply.github.com>
2025-12-20 11:31:50 +0700

    Fix a compile error

--- src/sage/libs/singular/ring.pyx
+++ src/sage/libs/singular/ring.pyx
@@ -27,7 +27,7 @@ from sage.libs.singular.decl cimport omAlloc0, omStrDup, omAlloc
 from sage.libs.singular.decl cimport ringorder_dp, ringorder_Dp, ringorder_lp, ringorder_ip, ringorder_ds, ringorder_Ds, ringorder_ls, ringorder_M, ringorder_c, ringorder_C, ringorder_wp, ringorder_Wp, ringorder_ws, ringorder_Ws, ringorder_a, rRingOrder_t
 from sage.libs.singular.decl cimport prCopyR
 from sage.libs.singular.decl cimport n_unknown, n_R, n_algExt, n_transExt, n_long_C, n_Z, n_Zn, n_Znm, n_Z2m
-from sage.libs.singular.decl cimport n_coeffType
+from sage.libs.singular.decl cimport n_coeffType, LongComplexInfo
 from sage.libs.singular.decl cimport rDefault, GFInfo, ZnmInfo, nInitChar, AlgExtInfo, TransExtInfo
 
 from sage.rings.integer cimport Integer
