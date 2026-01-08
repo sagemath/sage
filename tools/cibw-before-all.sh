201485443cc527d9fa0a9c86e38aa254158e711b
Tobias Diez <code@tobiasdiez.de>
2026-01-08 12:56:46 +0100

    Disable Python building

--- tools/cibw-before-all.sh
+++ tools/cibw-before-all.sh
@@ -11,6 +11,6 @@ eval $(build/bin/sage-print-system-package-command $SYSTEM "$@" update)
 eval $(build/bin/sage-print-system-package-command $SYSTEM --yes --no-install-recommends --spkg install _bootstrap _prereq)
 ./bootstrap
 eval $(build/bin/sage-print-system-package-command $SYSTEM --yes --no-install-recommends --ignore-missing install $(build/bin/sage-get-system-packages $SYSTEM $(build/bin/sage-package list :standard:)))
-./configure --enable-build-as-root --with-system-python3=force
+./configure --enable-build-as-root --without-system-python3
 export MAKE="make -j8"
 make sagelib-build-deps
