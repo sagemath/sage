286499bc86ce630382f40310cc472f9c7a0fa4dd
Tobias Diez <code@tobiasdiez.de>
2026-01-08 12:30:56 +0100

    Add cibuildwheel configuration

---
+++ tools/cibw-before-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script is run from the root of the Sage source tree before cibuildwheel starts building any wheels.
+
+# Exit on error
+set -e
+
+export PATH=$(pwd)/build/bin:$PATH
+SYSTEM=$(sage-guess-package-system)
+eval $(build/bin/sage-print-system-package-command $SYSTEM "$@" update)
+eval $(build/bin/sage-print-system-package-command $SYSTEM --yes --no-install-recommends --spkg install _bootstrap _prereq)
+./bootstrap
+eval $(build/bin/sage-print-system-package-command $SYSTEM --yes --no-install-recommends --ignore-missing install $(build/bin/sage-get-system-packages $SYSTEM $(build/bin/sage-package list :standard:)))
+./configure --enable-build-as-root --with-system-python3=force
+export MAKE="make -j8"
+make sagelib-build-deps
