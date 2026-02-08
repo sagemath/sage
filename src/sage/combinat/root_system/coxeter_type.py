10b8c08fa7da490c7f7e755d32adb199904608fa
SamyMekkati <samy.mekkati.1@ens.etsmtl.ca>
2026-02-08 15:56:40 -0500

    removing redundant import

--- src/sage/combinat/root_system/coxeter_type.py
+++ src/sage/combinat/root_system/coxeter_type.py
@@ -26,7 +26,6 @@ from sage.misc.cachefunc import cached_method
 from sage.misc.classcall_metaclass import ClasscallMetaclass
 from sage.matrix.args import SparseEntry
 from sage.matrix.constructor import Matrix
-from sage.misc.lazy_import import lazy_import
 from sage.structure.unique_representation import UniqueRepresentation
 from sage.structure.sage_object import SageObject
 from sage.misc.lazy_import import lazy_import
