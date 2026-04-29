463366806898d94cc9b15dead26e3bf71ac7c90c
Sebastian <seb.oehms@gmail.com>
2026-04-29 19:01:06 +0200

    F507 fixes

--- src/sage/combinat/permutation.py
+++ src/sage/combinat/permutation.py
@@ -9509,7 +9509,7 @@ class CyclicPermutationsOfPartition(Permutations):
                 sage: elt.check()                                                       # needs sage.combinat
             """
             if [sorted(_) for _ in self] != [sorted(_) for _ in self.parent().partition]:
-                raise ValueError("Invalid cyclic permutation of the partition" % self.parent().partition)
+                raise ValueError("Invalid cyclic permutation of the partition")
 
     def _repr_(self):
         """
