cc462e9c7114393c333b526598749a2d927ae801
Antonio Rojas <arojas@archlinux.org>
2026-06-13 10:23:52 +0200

    Fix MatrixSpace(implementation='meataxe') constructor
    And make it consistent with all other implementations

--- src/sage/matrix/matrix_space.py
+++ src/sage/matrix/matrix_space.py
@@ -57,8 +57,6 @@ from sage.categories.enumerated_sets import EnumeratedSets
 
 from sage.misc.lazy_import import lazy_import
 from sage.features.meataxe import Meataxe
-lazy_import('sage.matrix.matrix_gfpn_dense', ['Matrix_gfpn_dense'],
-            feature=Meataxe())
 lazy_import('sage.groups.matrix_gps.matrix_group', ['MatrixGroup_base'])
 
 _Semirings = Semirings()
@@ -396,7 +394,8 @@ def get_matrix_class(R, nrows, ncols, sparse, implementation):
 
         if implementation == 'meataxe':
             if R.is_field() and R.order() < 256:
-                return Matrix_gfpn_dense
+                from . import matrix_gfpn_dense
+                return matrix_gfpn_dense.Matrix_gfpn_dense
             raise ValueError("'meataxe' matrix can only deal with finite fields of order < 256")
 
         if implementation == 'numpy':
