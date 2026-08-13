0ae66fcdd5446df7e564ade61a4e452740c1d394
Michael Orlitzky <michael@orlitzky.com>
2026-08-13 12:34:34 -0400

    src/sage/matrix/matrix_space.py: remove duplicate test
    We have one test for the type of a matrix over GF(64,'z') that is
    repeated. The first copy is missing "# needs m4rie" while the second
    one has it, so we delete the first.

--- src/sage/matrix/matrix_space.py
+++ src/sage/matrix/matrix_space.py
@@ -139,8 +139,6 @@ def get_matrix_class(R, nrows, ncols, sparse, implementation):
         <class 'sage.matrix.matrix_complex_ball_dense.Matrix_complex_ball_dense'>
         sage: type(matrix(GF(2), 2, range(4)))
         <class 'sage.matrix.matrix_mod2_dense.Matrix_mod2_dense'>
-        sage: type(matrix(GF(64,'z'), 2, range(4)))
-        <class 'sage.matrix.matrix_gf2e_dense.Matrix_gf2e_dense'>
         sage: type(matrix(GF(125,'z'), 2, range(4)))     # optional: meataxe
         <class 'sage.matrix.matrix_gfpn_dense.Matrix_gfpn_dense'>
         sage: type(matrix(SR, 2, 2, 0))                                                 # needs sage.symbolic
