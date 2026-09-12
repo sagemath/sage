198f912e880aabdb26ccafbaf17c31be7ea56246
cxzhong <chenxin.zhong@outlook.com>
2026-09-12 12:54:29 +0800

    gh-42712: mark variable limit regression doctests as long

--- src/sage/rings/polynomial/multi_polynomial_libsingular.pyx
+++ src/sage/rings/polynomial/multi_polynomial_libsingular.pyx
@@ -384,6 +384,7 @@ cdef class MPolynomialRing_libsingular(MPolynomialRing_base):
         Check that the number of variables fits into Singular's signed short
         (:issue:`42712`)::
 
+            sage: # long time
             sage: R = PolynomialRing(QQ, "x", 2**15)
             sage: type(R)
             <class 'sage.rings.polynomial.multi_polynomial_ring.MPolynomialRing_polydict_domain_with_category'>
